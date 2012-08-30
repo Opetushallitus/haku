0392563322acedfc730e9e26c9478bd239df21f0
jhirvonen <jhirvonen@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-08-30 14:04:09 +0000

    [OVT-1710] testit

---
+++ src/main/java/fi/vm/sade/oppija/haku/Hello.java
@@ -0,0 +1,12 @@
+package fi.vm.sade.oppija.haku;
+
+/**
+ * @author jukka
+ * @version 8/30/124:56 PM}
+ * @since 1.1
+ */
+public class Hello {
+    public String doHello() {
+        return "world";
+    }
+}
