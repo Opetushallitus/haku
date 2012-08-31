79a361ce40056eeb79963e46e89b8eb8541a46fd
jhirvonen <jhirvonen@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-08-31 08:48:27 +0000

    [OVT-1710] main structure

---
+++ src/main/java/fi/vm/sade/oppija/haku/controller/MainController.java
@@ -0,0 +1,12 @@
+package fi.vm.sade.oppija.haku.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author jukka
+ * @version 8/31/1211:47 AM}
+ * @since 1.1
+ */
+@Controller
+public class MainController {
+}
