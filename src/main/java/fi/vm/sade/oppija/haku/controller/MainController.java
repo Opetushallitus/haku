e1719d8ce30a7f9f96165e334c4e65785f03db60
jhirvonen <jhirvonen@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-08-31 10:45:51 +0000

    [OVT-1710] sample controller and view

--- src/main/java/fi/vm/sade/oppija/haku/controller/MainController.java
+++ src/main/java/fi/vm/sade/oppija/haku/controller/MainController.java
@@ -1,12 +1,29 @@
 package fi.vm.sade.oppija.haku.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @author jukka
  * @version 8/31/1211:47 AM}
  * @since 1.1
  */
+@RequestMapping("/")
 @Controller
 public class MainController {
+    @PostConstruct
+    public void init() {
+        System.out.println("imit");
+    }
+
+    @RequestMapping("/foo")
+    public ModelAndView helloWorld() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("helloWorld");
+        mav.addObject("message", "Hello World!");
+        return mav;
+    }
 }
