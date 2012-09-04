0b904c25f576369beede2ef77a2533d2b7067c99
jhirvonen <jhirvonen@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-09-04 07:37:59 +0000

    [OVT-1712] cleanups

--- src/test/java/fi/vm/sade/oppija/haku/dao/ApplicationPeriodDAOTest.java
+++ src/test/java/fi/vm/sade/oppija/haku/dao/ApplicationPeriodDAOTest.java
@@ -22,20 +22,20 @@ public class ApplicationPeriodDAOTest {
     public void testInsertApplicationPeriod() {
 
         // input question
-        Map<Object, Object> question1 = new HashMap<Object, Object>();
+        Map<String, Object> question1 = new HashMap<String, Object>();
         question1.put("id", "question1");
         question1.put("type", "INPUT");
         question1.put("label", "Etunimet");
 
         // help text question
-        Map<Object, Object> question2 = new HashMap<Object, Object>();
+        Map<String, Object> question2 = new HashMap<String, Object>();
         question2.put("id", "question2");
         question2.put("type", "HELP_TEXT");
         question2.put("related_question", "question1");
         question2.put("text", "Syötä etunimet kuten ne ovat passissa.");
 
         // checkbox question
-        Map<Object, Object> question3 = new HashMap<Object, Object>();
+        Map<String, Object> question3 = new HashMap<String, Object>();
         question3.put("id", "question3");
         question3.put("type", "CHECKBOX");
         question3.put("label", "Pohjakoulutus");
@@ -43,21 +43,21 @@ public class ApplicationPeriodDAOTest {
         question3.put("option2", "IB-tutkinto");
         question3.put("option3", "EB-tutkinto");
 
-        Map<Object, Object> category1 = new HashMap<Object, Object>();
+        Map<String, Object> category1 = new HashMap<String, Object>();
         category1.put("id", "OMATTIEDOT");
-        List<Map<Object, Object>> questions = new ArrayList<Map<Object, Object>>();
+        List<Map<String, Object>> questions = new ArrayList<Map<String, Object>>();
         questions.add(question1);
         questions.add(question2);
         questions.add(question3);
         category1.put("questions", questions);
 
-        Map<Object, Object> application = new HashMap<Object, Object>();
+        Map<String, Object> application = new HashMap<String, Object>();
         application.put("id", "1234");
-        List<Map<Object, Object>> categories = new ArrayList<Map<Object, Object>>();
+        List<Map<String, Object>> categories = new ArrayList<Map<String, Object>>();
         categories.add(category1);
         application.put("categories", categories);
 
-        Map<Object, Object> applicationPeriod = new HashMap<Object, Object>();
+        Map<String, Object> applicationPeriod = new HashMap<String, Object>();
         applicationPeriod.put("id", "YHTEISHAKU");
         applicationPeriod.put("application", application);
 
