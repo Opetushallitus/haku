2796b13003bc64ac859dcfcb2d3aa892d3cb29b6
jhirvonen <jhirvonen@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-09-04 11:21:18 +0000

    [OVT-1731] type safety

--- src/main/java/fi/vm/sade/oppija/haku/dao/ApplicationPeriodDAO.java
+++ src/main/java/fi/vm/sade/oppija/haku/dao/ApplicationPeriodDAO.java
@@ -1,14 +1,12 @@
 package fi.vm.sade.oppija.haku.dao;
 
 
-import com.mongodb.DBObject;
-
 import java.util.Map;
 
 public interface ApplicationPeriodDAO {
 
     void insert(Map<String, Object> map);
 
-    Map find(String applicationPeriodId);
+    Map<String, Object> find(String applicationPeriodId);
 
 }
