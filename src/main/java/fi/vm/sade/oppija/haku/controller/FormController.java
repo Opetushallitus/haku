163a3efb0f2aedea0c683a8afeb70077ebdd3f2a
vrajapaju <vrajapaju@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-09-04 11:29:39 +0000

    Lisätty lokitus

--- src/main/java/fi/vm/sade/oppija/haku/controller/FormController.java
+++ src/main/java/fi/vm/sade/oppija/haku/controller/FormController.java
@@ -1,6 +1,8 @@
 package fi.vm.sade.oppija.haku.controller;
 
 import fi.vm.sade.oppija.haku.service.FormService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +18,8 @@ import java.util.Map;
 @Controller
 public class FormController {
 
+    private final static Logger logger = LoggerFactory.getLogger(FormController.class);
+
     final FormService formService;
 
     @Autowired
@@ -24,8 +28,10 @@ public class FormController {
     }
 
     @RequestMapping(value = "/{applicationPeriodId}/{formId}", method = RequestMethod.GET)
-    public ModelAndView getFormsAsHtml(@PathVariable final String applicationPeriodId, @PathVariable final String formId) {
+    public ModelAndView getFormAsHtml(@PathVariable final String applicationPeriodId, @PathVariable final String formId) {
+        logger.debug("getFormsAsHtml ", applicationPeriodId, formId);
         final Map<String, Object> data = formService.getForm(applicationPeriodId, formId);
+        logger.debug("form ", data);
         final ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("template");
         modelAndView.addObject("data", data);
@@ -35,12 +41,14 @@ public class FormController {
     @RequestMapping(value = "/{applicationPeriodId}/", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     public List<Map<String, Object>> getForms(@PathVariable final String applicationPeriodId) {
+        logger.debug("getForms ", applicationPeriodId);
         return formService.getForms(applicationPeriodId);
     }
 
     @RequestMapping(value = "/{applicationPeriodId}/{formId}", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public Map getStudyApplicationProcessAsJson(@PathVariable final String applicationPeriodId, @PathVariable final String formId) {
+    public Map getFormAsJson(@PathVariable final String applicationPeriodId, @PathVariable final String formId) {
+        logger.debug("getFormAsJson ", applicationPeriodId, formId);
         return formService.getForm(applicationPeriodId, formId);
     }
 }
