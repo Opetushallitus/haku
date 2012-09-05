3afcfee80580715d3dcba2ef6ae62dd085cee4aa
vrajapaju <vrajapaju@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-09-05 09:42:14 +0000

    Kontrolloinnin yhdistäminen.

--- src/main/java/fi/vm/sade/oppija/haku/controller/FormController.java
+++ src/main/java/fi/vm/sade/oppija/haku/controller/FormController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
 import java.util.Map;
 
 
@@ -25,16 +26,57 @@ public class FormController {
         this.applicationPeriodService = applicationPeriodService;
     }
 
+    @RequestMapping(value = "/{applicationPeriodId}", method = RequestMethod.GET)
+    public ModelAndView getApplicationPeriod(@PathVariable final String applicationPeriodId) {
+        logger.debug("getApplicationPeriod {}", applicationPeriodId);
+        final Map<String, Object> data = applicationPeriodService.getApplicationPeriod(applicationPeriodId);
+        final ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("form");
+        modelAndView.addObject("data", data);
+        final Map<String, Object> form = (Map<String, Object>) data.get("form");
+        List<Map<String, Object>> categories = (List<Map<String, Object>>) form.get("categories");
+        modelAndView.addObject("categories", categories);
+        modelAndView.addObject("questions", categories.get(0).get("questions"));
+        return modelAndView;
+    }
+
     @RequestMapping(value = "/{applicationPeriodId}/{formId}", method = RequestMethod.GET)
-    public ModelAndView getFormAsHtml(@PathVariable final String applicationPeriodId, @PathVariable final String formId) {
-        logger.debug("getFormsAsHtml ", applicationPeriodId, formId);
+    public ModelAndView getForm(@PathVariable final String applicationPeriodId, @PathVariable final String formId) {
+        logger.debug("getForm {}, {}", new Object[]{applicationPeriodId, formId});
         final Map<String, Object> data = applicationPeriodService.findForm(applicationPeriodId, formId);
-        logger.debug("form ", data);
-        System.out.println(data);
         final ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("forms");
         modelAndView.addObject("data", data);
         return modelAndView;
     }
 
+    @RequestMapping(value = "/{applicationPeriodId}/{formId}/{categoryId}", method = RequestMethod.GET)
+    public ModelAndView getCategory(@PathVariable final String applicationPeriodId,
+                                    @PathVariable final String formId,
+                                    @PathVariable final String categoryId) {
+        logger.debug("getCategory {}, {}, {}", new Object[]{applicationPeriodId, formId, categoryId});
+        final Map<String, Object> data = applicationPeriodService.findForm(applicationPeriodId, formId);
+        final List<Map<String, Object>> categories = (List<Map<String, Object>>) data.get("categories");
+
+        final ModelAndView modelAndView = new ModelAndView();
+
+        String prev = null;
+        String next = null;
+
+        for (int i = 0; i < categories.size(); i++) {
+            if (categories.get(i).get("id").equals(categoryId)) {
+                modelAndView.addObject("category", categories.get(i));
+                if (i > 0) {
+                    prev = (String) categories.get(i - 1).get("id");
+                }
+                if (i < categories.size() - 1) {
+                    next = (String) categories.get(i + 1).get("id");
+                }
+            }
+        }
+        modelAndView.setViewName("category");
+        modelAndView.addObject("prev", prev);
+        modelAndView.addObject("next", next);
+        return modelAndView;
+    }
 }
