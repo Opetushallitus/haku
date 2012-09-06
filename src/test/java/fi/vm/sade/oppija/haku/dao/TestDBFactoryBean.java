9b6c9d4f2d2007c402a0bb5a0cf3e22b1de2df19
hlyytika <hlyytika@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-09-06 07:13:35 +0000

    OVT-1735: Add: Added test specific properties and DBFactoryBean

---
+++ src/test/java/fi/vm/sade/oppija/haku/dao/TestDBFactoryBean.java
@@ -0,0 +1,22 @@
+package fi.vm.sade.oppija.haku.dao;
+
+import com.mongodb.WriteConcern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestDBFactoryBean extends DBFactoryBean {
+
+    private final static Logger logger = LoggerFactory.getLogger(TestDBFactoryBean.class);
+
+    @Override
+    public void shutDown() {
+//        try {
+//            getObject().dropDatabase();
+//        } catch (Exception e) {
+//            logger.warn("Could not drop test database.");
+//        }
+
+        mongo.close();
+    }
+
+}
