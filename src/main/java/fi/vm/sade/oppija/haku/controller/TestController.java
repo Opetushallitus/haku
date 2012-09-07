72fe4560539be12939bd50ee1c1b834649c499d6
vrajapaju <vrajapaju@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-09-07 07:18:59 +0000

    Lisätty testi kontrolleri

---
+++ src/main/java/fi/vm/sade/oppija/haku/controller/TestController.java
@@ -0,0 +1,42 @@
+package fi.vm.sade.oppija.haku.controller;
+
+import fi.vm.sade.oppija.haku.service.FormService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Map;
+
+
+@Controller
+public class TestController {
+
+    private final static Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    final FormService formService;
+
+    @Autowired
+    public TestController(final FormService formService) {
+        this.formService = formService;
+    }
+
+    @RequestMapping(value = "/{applicationPeriodId}", method = RequestMethod.GET)
+    public ModelAndView getApplicationPeriod(@PathVariable final String applicationPeriodId) {
+        logger.debug("getApplicationPeriod {}", applicationPeriodId);
+        final Map<String, Object> data = formService.getApplicationPeriod(applicationPeriodId);
+        final ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("root");
+        modelAndView.addObject("data", data);
+        final Map<String, Object> form = (Map<String, Object>) data.get("form");
+        List<Map<String, Object>> categories = (List<Map<String, Object>>) form.get("categories");
+        modelAndView.addObject("categories", categories);
+        modelAndView.addObject("questions", categories.get(0).get("questions"));
+        return modelAndView;
+    }
+}
