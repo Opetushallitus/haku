bf423350d96c07c346035755bb189fbd4c499393
jhirvonen <jhirvonen@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-09-10 10:44:26 +0000

    [OVT-1824] navigation

---
+++ src/main/java/fi/vm/sade/oppija/haku/domain/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package fi.vm.sade.oppija.haku.domain.exception;
+
+/**
+ * @author jukka
+ * @version 9/10/1210:29 AM}
+ * @since 1.1
+ */
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
