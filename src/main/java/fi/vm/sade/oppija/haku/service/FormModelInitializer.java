efa2ff2afd3d2f9e135aa10eca3596e07f44e3bc
jhirvonen <jhirvonen@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-09-14 11:49:02 +0000

    [OVT-1824] added: Factory

---
+++ src/main/java/fi/vm/sade/oppija/haku/service/FormModelInitializer.java
@@ -0,0 +1,27 @@
+package fi.vm.sade.oppija.haku.service;
+
+import fi.vm.sade.oppija.haku.domain.ApplicationPeriod;
+import fi.vm.sade.oppija.haku.domain.FormModel;
+import fi.vm.sade.oppija.haku.domain.elements.Form;
+
+import java.util.Map;
+import java.util.Set;
+
+public class FormModelInitializer {
+    private final FormModel model;
+
+    public FormModelInitializer(FormModel model) {
+        this.model = model;
+    }
+
+    public void initModel() {
+
+        //init forms
+        for (Map.Entry<String, ApplicationPeriod> stringApplicationPeriodEntry : model.getApplicationPerioidMap().entrySet()) {
+            final Set<Map.Entry<String, Form>> entries = stringApplicationPeriodEntry.getValue().getForms().entrySet();
+            for (Map.Entry<String, Form> entry : entries) {
+                entry.getValue().init();
+            }
+        }
+    }
+}
