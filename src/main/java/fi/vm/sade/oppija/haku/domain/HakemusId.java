72c4a22efe30d8c05be0b97e52bf728eb150eb84
jhirvonen <jhirvonen@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-09-28 13:59:58 +0000

    [OVT-1936] refactored: removed dead code

--- src/main/java/fi/vm/sade/oppija/haku/domain/HakemusId.java
+++ src/main/java/fi/vm/sade/oppija/haku/domain/HakemusId.java
@@ -1,11 +1,15 @@
 package fi.vm.sade.oppija.haku.domain;
 
+import java.io.Serializable;
+
 /**
  * @author jukka
  * @version 9/26/123:02 PM}
  * @since 1.1
  */
-public class HakemusId {
+public class HakemusId implements Serializable {
+
+    private static final long serialVersionUID = -6584775919268318934L;
     private final String applicationPeriodId;
     private final String formId;
     private final String categoryId;
