21c8fac76bccaa2186fe2c92507efbb8d47f2939
tjaakkola <tjaakkola@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-10-02 10:48:01 +0000

    ovt-2140: vapaasanahaku-lomakkeen hakukenttä lisätty

--- src/main/webapp/static-html/layout/javascript/master.js
+++ src/main/webapp/static-html/layout/javascript/master.js
@@ -197,6 +197,65 @@ var formReplacements = {
 	}
 }
 
+var tabsMenu = {
+		load:function()
+		{
+			
+			if(window.location.hash)
+			{
+				hash = window.location.hash.substr(1);
+				key = "tabsheet";
+				hashparams = hash.split(':');
+				id = hashparams[1];
+
+				if(hash.indexOf(key) != -1)
+				{
+
+					$('.tabs .tab[data-tabs-id="'+hashparams[1]+'"]').each(function(){
+					
+						group = $(this).attr('data-tabs-group');
+						
+						$('.tabsheet[data-tabs-group="'+group+'"]').hide();
+						$('.tabs .tab[data-tabs-group="'+group+'"]').removeClass('current');
+						$('.tabsheet[data-tabs-group="'+group+'"][data-tabs-id="'+id+'"]').show();
+						$('.tabs .tab[data-tabs-group="'+group+'"][data-tabs-id="'+id+'"]').addClass('current');
+					});
+				}
+			}
+		
+			$('.tabs .tab').click(function(event){
+				event.preventDefault();
+				group = $(this).attr('data-tabs-group');
+				id = $(this).attr('data-tabs-id');
+				
+				$('.tabs .tab[data-tabs-group="'+group+'"]').removeClass('current');
+				$(this).addClass('current');
+				
+				$('.tabsheet[data-tabs-group="'+group+'"]').hide();
+				
+				$('.tabsheet[data-tabs-group="'+group+'"][data-tabs-id="'+id+'"]').show();
+			});
+		},
+		build:function(){
+			tabsMenu.load();
+			tabsMenu.setTriggers();
+		},
+		setTriggers:function(){
+			$('.tabs .tab').click(function(event){
+				event.preventDefault();
+				group = $(this).attr('data-tabs-group');
+				id = $(this).attr('data-tabs-id');
+				
+				$('.tabs .tab[data-tabs-group="'+group+'"]').removeClass('current');
+				$(this).addClass('current');
+				
+				$('.tabsheet[data-tabs-group="'+group+'"]').hide();
+				
+				$('.tabsheet[data-tabs-group="'+group+'"][data-tabs-id="'+id+'"]').show();
+			});
+		}
+	}
+
 applicationBasket.build();
 formReplacements.build();
 
