9934dc8c9dd6162ad2746ecd453443a21665d45b
tjaakkola <tjaakkola@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-10-02 11:16:43 +0000

    ovt-2140: tab-rakenne kuntoon vapaahakulomakkeessa

--- src/main/webapp/static-html/layout/javascript/master.js
+++ src/main/webapp/static-html/layout/javascript/master.js
@@ -198,6 +198,10 @@ var formReplacements = {
 }
 
 var tabsMenu = {
+		build:function(){
+			tabsMenu.load();
+			tabsMenu.setTriggers();
+		},
 		load:function()
 		{
 			
@@ -236,10 +240,6 @@ var tabsMenu = {
 				$('.tabsheet[data-tabs-group="'+group+'"][data-tabs-id="'+id+'"]').show();
 			});
 		},
-		build:function(){
-			tabsMenu.load();
-			tabsMenu.setTriggers();
-		},
 		setTriggers:function(){
 			$('.tabs .tab').click(function(event){
 				event.preventDefault();
@@ -258,73 +258,73 @@ var tabsMenu = {
 
 applicationBasket.build();
 formReplacements.build();
-
+tabsMenu.build();
 
 
 var protoFunctions = {
-build:function(){
-	
-var formStep = {
-	stepUrls : {
-		1 : 'index.html',
-		2 : 'koulutustausta.html',
-		3 : 'hakutoiveet.html',
-		4 : 'arvosanat.html',
-		5 : 'lisatiedot.html',
-		6 : 'esikatselu.html',
-		7 : 'valmis.html'
-	},
 	build:function(){
-		formStep.setTriggers();
-	},
-	change:function(param){
-		// Next and Previous buttons
-		if (param == 'next' || param == 'previous')
-		{
-			index = parseInt($('form[data-form-step-id]').attr('data-form-step-id'));
-			
-			if (param == 'next')
-			{
-				window.location.href = formStep.stepUrls[index+1];
-			}
-			else
-			{
-				window.location.href = formStep.stepUrls[index-1];
-			}
-		}
-		else if(param.indexOf('goto') != -1)
-		{
-			params = param.split(':');
-			index = params[1];
-			
-			window.location.href = formStep.stepUrls[index];
-		}
-
-	},
-	setTriggers:function(){
-		$('button[data-form-step-action]').click(function(event){
-			event.preventDefault();
-			params = $(this).attr('data-form-step-action');
-			formStep.change(params);
-		});
 		
-		$('a[data-form-step-action]').click(function(event){
-			event.preventDefault();
-			params = $(this).attr('data-form-step-action');
-			formStep.change(params);
-		});
-	}
-}
-	
-	
-$('button').click(function(event){
-	event.preventDefault();
-});
-	
-	
-formStep.build();
+	var formStep = {
+		stepUrls : {
+			1 : 'index.html',
+			2 : 'koulutustausta.html',
+			3 : 'hakutoiveet.html',
+			4 : 'arvosanat.html',
+			5 : 'lisatiedot.html',
+			6 : 'esikatselu.html',
+			7 : 'valmis.html'
+		},
+		build:function(){
+			formStep.setTriggers();
+		},
+		change:function(param){
+			// Next and Previous buttons
+			if (param == 'next' || param == 'previous')
+			{
+				index = parseInt($('form[data-form-step-id]').attr('data-form-step-id'));
+				
+				if (param == 'next')
+				{
+					window.location.href = formStep.stepUrls[index+1];
+				}
+				else
+				{
+					window.location.href = formStep.stepUrls[index-1];
+				}
+			}
+			else if(param.indexOf('goto') != -1)
+			{
+				params = param.split(':');
+				index = params[1];
+				
+				window.location.href = formStep.stepUrls[index];
+			}
 
-}
+		},
+		setTriggers:function(){
+			$('button[data-form-step-action]').click(function(event){
+				event.preventDefault();
+				params = $(this).attr('data-form-step-action');
+				formStep.change(params);
+			});
+			
+			$('a[data-form-step-action]').click(function(event){
+				event.preventDefault();
+				params = $(this).attr('data-form-step-action');
+				formStep.change(params);
+			});
+		}
+	}
+		
+		
+	$('button').click(function(event){
+		event.preventDefault();
+	});
+		
+		
+	formStep.build();
+
+	}
 }
 
 protoFunctions.build();
