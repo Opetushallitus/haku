7259793691050236331bd21efa12f18260e04a37
hlyytika <hlyytika@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-10-10 08:42:00 +0000

    OVT-1945: Change: Closure implementation for gradegrid.js add language functionality

--- src/main/webapp/resources/javascript/gradegrid.js
+++ src/main/webapp/resources/javascript/gradegrid.js
@@ -1,22 +1,27 @@
+/**
+    Handles adding new language rows to the GradeGrid component.
+*/
 
+var gradegrid = (function() {
 
-var gradegrid = {
+    return {
 
-    addLanguage: function() {
+        addLanguage: function() {
 
-        var customLanguageCount = $(".gradegrid-custom-language-row").length
+            var customLanguageCount = $(".gradegrid-custom-language-row").length
 
-        var newLang = $('#gradegrid-table tr:last').clone().css('display', '')
-            .addClass('gradegrid-language-row').addClass('gradegrid-custom-language-row');
+            var newLang = $('#gradegrid-table tr:last').clone().css('display', '')
+                .addClass('gradegrid-language-row').addClass('gradegrid-custom-language-row');
 
-        $(newLang.find('select').get(0)).attr('id', 'custom-scope_' + customLanguageCount).attr('name', 'custom-scope_' + customLanguageCount);
-        $(newLang.find('select').get(1)).attr('id', 'custom-language_' + customLanguageCount).attr('name', 'custom-language_' + customLanguageCount);
-        $(newLang.find('select').get(2)).attr('id', 'custom-commongrade_' + customLanguageCount).attr('name', 'custom-commongrade_' + customLanguageCount);
-        $(newLang.find('select').get(3)).attr('id', 'custom-optionalgrade_' + customLanguageCount).attr('name', 'custom-optionalgrade_' + customLanguageCount);
+            $(newLang.find('select').get(0)).attr('id', 'custom-scope_' + customLanguageCount).attr('name', 'custom-scope_' + customLanguageCount);
+            $(newLang.find('select').get(1)).attr('id', 'custom-language_' + customLanguageCount).attr('name', 'custom-language_' + customLanguageCount);
+            $(newLang.find('select').get(2)).attr('id', 'custom-commongrade_' + customLanguageCount).attr('name', 'custom-commongrade_' + customLanguageCount);
+            $(newLang.find('select').get(3)).attr('id', 'custom-optionalgrade_' + customLanguageCount).attr('name', 'custom-optionalgrade_' + customLanguageCount);
 
-        $("#gradegrid-table tr.gradegrid-language-row:last").after(newLang);
-    }
-}
+            $("#gradegrid-table tr.gradegrid-language-row:last").after(newLang);
+        }
+    };
+}());
 
 $(document).ready(function() {
    $("#add_language_button").click(gradegrid.addLanguage);
