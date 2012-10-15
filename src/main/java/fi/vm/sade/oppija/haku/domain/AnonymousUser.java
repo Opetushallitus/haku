76295af9ce952efdcf725d691a088db7486c7912
jhirvonen <jhirvonen@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-10-15 08:39:15 +0000

    [OVT-2350] missing class

---
+++ src/main/java/fi/vm/sade/oppija/haku/domain/AnonymousUser.java
@@ -0,0 +1,13 @@
+package fi.vm.sade.oppija.haku.domain;
+
+/**
+ * @author jukka
+ * @version 10/12/123:58 PM}
+ * @since 1.1
+ */
+public class AnonymousUser extends User {
+
+    public AnonymousUser() {
+        super("anonymous");
+    }
+}
