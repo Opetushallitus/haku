14fb5a9b2bf0f3681b8b077c3c053f9118bbcdd2
hlyytika <hlyytika@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-10-17 13:13:24 +0000

    OVT-2345: Change: Using mocked services in AdditionalQuestionsService. Enabled from ignore.

--- src/test/java/fi/vm/sade/oppija/haku/service/AdditionalQuestionsServiceTest.java
+++ src/test/java/fi/vm/sade/oppija/haku/service/AdditionalQuestionsServiceTest.java
@@ -1,90 +1,54 @@
 package fi.vm.sade.oppija.haku.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
-import com.mongodb.util.JSON;
-import fi.vm.sade.oppija.haku.dao.FormModelDAO;
 import fi.vm.sade.oppija.haku.dao.impl.FormModelDummyMemoryDaoImpl;
+import fi.vm.sade.oppija.haku.domain.Hakemus;
 import fi.vm.sade.oppija.haku.domain.HakemusId;
 import fi.vm.sade.oppija.haku.domain.User;
 import fi.vm.sade.oppija.haku.domain.questions.Question;
-import fi.vm.sade.oppija.haku.service.AbstractServiceTest;
-import fi.vm.sade.oppija.haku.tools.FileHandling;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Ignore;
+import fi.vm.sade.oppija.haku.service.impl.AdditionalQuestionServiceImpl;
+import fi.vm.sade.oppija.haku.service.impl.HakemusServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.io.IOException;
 import java.util.*;
 
-import static java.lang.ClassLoader.getSystemResourceAsStream;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 /**
  * @author Hannu Lyytikainen
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:spring/test-context.xml")
-@Ignore
-public class AdditionalQuestionsServiceTest extends AbstractServiceTest {
 
-    private final String LOMAKE_COLLECTION = "haku";
-    private final String HAKEMUS_COLLECTION = "hakemus";
+public class AdditionalQuestionsServiceTest {
 
-    protected static DBObject lomakeTestDataObject;
-    protected static List<DBObject> hakemusTestDataObjects = new ArrayList<DBObject>();
-
-    @Autowired
-    FormModelHolder formModelHolder;
-
-    @Autowired
     private AdditionalQuestionService additionalQuestionService;
 
-    FormModelDAO formModelDAO;
-
     public AdditionalQuestionsServiceTest() {
-        this.formModelDAO = new FormModelDummyMemoryDaoImpl();
-    }
+        FormService formService = new FormModelDummyMemoryDaoImpl();
+        HakemusService hakemusService = createHakemusServiceMock();
 
-    @BeforeClass
-    public static void readTestData() {
-        // read test data into objects
-
-        //String lomakeContent = new FileHandling().readFile(getSystemResourceAsStream("test-data.json"));
-        //lomakeTestDataObject = (DBObject) JSON.parse(lomakeContent);
-
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-
-            List hakemusTestObjects = mapper.readValue(getSystemResourceAsStream("hakemus-test-data.json"), List.class);
-            for (Object testObject : hakemusTestObjects) {
-                hakemusTestDataObjects.add(new BasicDBObject((Map)testObject));
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        additionalQuestionService = new AdditionalQuestionServiceImpl(formService, hakemusService);
 
     }
 
-    @Before
-    public void insertTestData() {
+    private HakemusService createHakemusServiceMock() {
+        HakemusService hakemusService = mock(HakemusServiceImpl.class);
 
-        formModelHolder.updateModel(formModelDAO.find());
+        HakemusId hakemusId = new HakemusId("test", "yhteishaku", "arvosanat");
 
-        try {
-            dbFactory.getObject().getCollection(HAKEMUS_COLLECTION).insert(hakemusTestDataObjects);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Map<String, String> values = new HashMap<String, String>();
+        values.put("preference1-Koulutus-id", "0_0");
 
+        Hakemus hakemus = new Hakemus(hakemusId, values, new User("testuser"));
+
+        when(hakemusService.getHakemus(hakemusId)).thenReturn(hakemus);
+
+        return hakemusService;
     }
 
     @Test
@@ -111,11 +75,4 @@ public class AdditionalQuestionsServiceTest extends AbstractServiceTest {
 
     }
 
-    @Override
-    protected List<String> getCollectionNames() {
-        List<String> names = new ArrayList<String>();
-        names.add(LOMAKE_COLLECTION);
-        names.add(HAKEMUS_COLLECTION);
-        return names;
-    }
 }
