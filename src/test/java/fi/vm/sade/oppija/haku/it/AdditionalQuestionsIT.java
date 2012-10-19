25e004c31c61983bcb521e442d2070417f523221
mmajapuro <mmajapuro@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-10-19 07:48:42 +0000

    Test fix.

--- src/test/java/fi/vm/sade/oppija/haku/it/AdditionalQuestionsIT.java
+++ src/test/java/fi/vm/sade/oppija/haku/it/AdditionalQuestionsIT.java
@@ -26,15 +26,17 @@ public class AdditionalQuestionsIT extends AbstractRemoteTest {
     public void testAdditionalQuestion() {
         beginAt("education/additionalquestion/test/yhteishaku/hakutoiveet/hakutoiveetGrp/2_2");
         assertElementPresent("2_2_additional_question_1");
-        assertElementPresent("2_2_additional_question_2_2_2_q2_option_1");
-        assertElementPresent("2_2_additional_question_2_2_2_q2_option_2");
+        assertElementPresentByXPath("//input[@value='q2_option_1']");
+        assertElementPresentByXPath("//input[@value='q2_option_2']");
+        assertFormElementPresent("2_2_additional_question_2");
     }
 
     @Test
     public void testNoAdditionalQuestion() {
         beginAt("education/additionalquestion/test/yhteishaku/hakutoiveet/hakutoiveetGrp/6_1");
         assertElementNotPresent("6_1_additional_question_1");
-        assertElementNotPresent("6_1_additional_question_2_2_2_q2_option_1");
-        assertElementNotPresent("6_1_additional_question_2_2_2_q2_option_2");
+        assertElementNotPresentByXPath("//input[@value='q2_option_1']");
+        assertElementNotPresentByXPath("//input[@value='q2_option_2']");
+        assertFormElementNotPresent("6_1_additional_question_2");
     }
 }
