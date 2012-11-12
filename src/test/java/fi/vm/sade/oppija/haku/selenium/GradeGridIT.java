896b769f3ce168262d2fd22b265cf7c8bc3d5b64
hlyytika <hlyytika@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-11-12 13:04:05 +0000

    Changed into selenium test.

---
+++ src/test/java/fi/vm/sade/oppija/haku/selenium/GradeGridIT.java
@@ -0,0 +1,49 @@
+package fi.vm.sade.oppija.haku.selenium;
+
+import fi.vm.sade.oppija.haku.FormModelHelper;
+import fi.vm.sade.oppija.haku.dao.impl.FormModelDummyMemoryDaoImpl;
+import fi.vm.sade.oppija.haku.domain.FormModel;
+import fi.vm.sade.oppija.haku.domain.builders.FormModelBuilder;
+import fi.vm.sade.oppija.haku.it.AbstractRemoteTest;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author Hannu Lyytikainen
+ */
+public class GradeGridIT extends AbstractSeleniumBase {
+
+    private FormModelHelper formModelHelper;
+
+    @Before
+    public void init() {
+        FormModelDummyMemoryDaoImpl dummyMem = new FormModelDummyMemoryDaoImpl();
+        initModel(dummyMem.getModel());
+    }
+
+    @Test
+    public void testTableExists() {
+        final String url = getBaseUrl() + "/" + "lomake/test/yhteishaku/arvosanat";
+        final WebDriver driver = seleniumHelper.getDriver();
+        driver.get(url);
+
+        assertNotNull(driver.findElement(By.id("gradegrid-table")));
+
+    }
+
+    @Test
+    public void testAddLanguage() {
+        final String url = getBaseUrl() + "/" + "lomake/test/yhteishaku/arvosanat";
+        final WebDriver driver = seleniumHelper.getDriver();
+        driver.get(url);
+
+        driver.findElement(By.id("add_language_button")).click();
+
+        assertNotNull(driver.findElement(By.className("gradegrid-custom-language-row")));
+    }
+}
