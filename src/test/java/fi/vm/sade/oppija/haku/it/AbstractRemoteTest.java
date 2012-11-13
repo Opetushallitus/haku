adc983129eb530ba9ee8c79777516d3faa6073fb
mmajapuro <mmajapuro@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-11-13 13:12:46 +0000

    removed system property

--- src/test/java/fi/vm/sade/oppija/haku/it/AbstractRemoteTest.java
+++ src/test/java/fi/vm/sade/oppija/haku/it/AbstractRemoteTest.java
@@ -15,7 +15,7 @@ import static net.sourceforge.jwebunit.junit.JWebUnit.*;
 public abstract class AbstractRemoteTest extends TomcatContainerBase {
 
     public AbstractRemoteTest() {
-        System.setProperty("webdriver.firefox.bin", "/home/majapuro/Downloads/firefox/firefox");
+        //System.setProperty("webdriver.firefox.bin", "/home/majapuro/Downloads/firefox/firefox");
     }
 
     protected FormModelHelper initModel(FormModel formModel1) {
