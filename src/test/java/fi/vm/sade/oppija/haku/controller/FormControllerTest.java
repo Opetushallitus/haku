0fcc30ad046b3eae8e22563f371a53d9b219b539
vrajapaju <vrajapaju@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-11-16 13:03:38 +0000

    Rakentajasta poistettu ylimääräinen parametri.

--- src/test/java/fi/vm/sade/oppija/haku/controller/FormControllerTest.java
+++ src/test/java/fi/vm/sade/oppija/haku/controller/FormControllerTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2012 The Finnish Board of Education - Opetushallitus
+ *
+ * This program is free software:  Licensed under the EUPL, Version 1.1 or - as
+ * soon as they will be approved by the European Commission - subsequent versions
+ * of the EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://www.osor.eu/eupl/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * European Union Public Licence for more details.
+ */
+
 package fi.vm.sade.oppija.haku.controller;
 
 import fi.vm.sade.oppija.haku.dao.impl.ApplicationDAOMongoImpl;
@@ -7,7 +23,6 @@ import fi.vm.sade.oppija.haku.domain.exception.ResourceNotFoundException;
 import fi.vm.sade.oppija.haku.event.EventHandler;
 import fi.vm.sade.oppija.haku.service.SessionDataHolder;
 import fi.vm.sade.oppija.haku.service.UserHolder;
-import fi.vm.sade.oppija.haku.service.impl.AdditionalQuestionServiceImpl;
 import fi.vm.sade.oppija.haku.service.impl.HakemusServiceImpl;
 import fi.vm.sade.oppija.haku.service.impl.UserDataStorage;
 import fi.vm.sade.oppija.haku.service.impl.UserPrefillDataServiceImpl;
@@ -34,7 +49,7 @@ public class FormControllerTest {
         UserHolder userHolder = new UserHolder();
         final HakemusServiceImpl hakemusService = new HakemusServiceImpl(new UserDataStorage(new SessionDataHolder(), new ApplicationDAOMongoImpl(), userHolder), eventHandler, formService);
         final UserPrefillDataServiceImpl userPrefillDataService = new UserPrefillDataServiceImpl(userHolder);
-        this.formController = new FormController(formService, new AdditionalQuestionServiceImpl(formService, hakemusService), hakemusService, userPrefillDataService);
+        this.formController = new FormController(formService, hakemusService, userPrefillDataService);
     }
 
     @Test
