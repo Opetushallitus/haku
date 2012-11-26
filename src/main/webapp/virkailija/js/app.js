9da5ca5955563763380e4528eb3c54949228fa8a
mmajapuro <mmajapuro@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-11-26 07:14:36 +0000

    deleted duplicate impl

--- src/main/webapp/virkailija/js/app.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-
-// Declare app level module which depends on filters, and services
-angular.module('virkailija', ['virkailija.filters', 'hakemusServices', 'virkailija.directives']).
-  config(['$routeProvider', function($routeProvider) {
-  }]);
