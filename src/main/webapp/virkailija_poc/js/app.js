2fdfc45a9f82632d8298aad59ef906491330f8e2
mmajapuro <mmajapuro@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-11-26 07:45:56 +0000

    OVT-2873 : Add: officer ui poc implementation

---
+++ src/main/webapp/virkailija_poc/js/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('virkailija', ['virkailija.filters', 'hakemusServices', 'virkailija.directives']).
+  config(['$routeProvider', function($routeProvider) {
+  }]);
