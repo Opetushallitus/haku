ec6905918d63f8e5a8b9341d8f4a0d5fd96267e9
mmajapuro <mmajapuro@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-12-07 14:08:45 +0000

    OVT-3229: Add: officer ui rich client implementations

--- src/main/webapp/virkailija/app/js/app.js
+++ src/main/webapp/virkailija/app/js/app.js
@@ -3,8 +3,8 @@
 // Declare app level module which depends on filters, and services
 angular.module('virkailija', ['virkailija.filters', 'virkailija.services', 'virkailija.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: MyCtrl1});
+    $routeProvider.when('/search', {templateUrl: 'partials/search.html', controller: MyCtrl1});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: MyCtrl2});
-    $routeProvider.otherwise({redirectTo: '/view1'});
+    $routeProvider.otherwise({redirectTo: '/search'});
   }]);
 
