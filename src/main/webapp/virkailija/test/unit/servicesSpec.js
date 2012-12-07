c73ee4107aacab3b417d2a1ebab8811670b1df45
mmajapuro <mmajapuro@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-12-07 14:04:15 +0000

    OVT-3229: Add: officer ui rich client implementations

--- src/main/webapp/virkailija/test/unit/servicesSpec.js
+++ src/main/webapp/virkailija/test/unit/servicesSpec.js
@@ -3,7 +3,7 @@
 /* jasmine specs for services go here */
 
 describe('service', function() {
-  beforeEach(module('myApp.services'));
+  beforeEach(module('virkailija.services'));
 
 
   describe('version', function() {
