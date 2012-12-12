72cb450640db74f408f69e1946d8590c58f246f1
mmajapuro <mmajapuro@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2012-12-12 14:34:29 +0000

    OVT-3229: Add: Officer UI for managing sent applications.

--- src/main/webapp/virkailija/test/unit/controllersSpec.js
+++ src/main/webapp/virkailija/test/unit/controllersSpec.js
@@ -13,28 +13,35 @@ describe('Controllers', function(){
     });
 
     beforeEach(module('virkailija.services'));
+    beforeEach(module('virkailija'));
 
     describe('SearchCtrl', function(){
         var scope, ctrl, $httpBackend;
 
-        beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+        beforeEach(inject(function(_$httpBackend_, $rootScope, $controller, Config) {
+
             $httpBackend = _$httpBackend_;
+            $httpBackend.expectGET("/context.html/hakemukset?term=1.2.3.4.5.0").
+                respond([{vastaukset:{henkilotiedot:{Sukunimi: "Testaaja", Etunimet: "Teppo Topias", Henkilotunnus: "120187-1234"}}, oid : "1.2.3.4.5.1", state: "Voimassa"}]);
             scope = $rootScope.$new();
-            ctrl = $controller(SearchCtrl, {$scope: scope});
+            ctrl = $controller(SearchCtrl, {$scope: scope, Config: Config});
         }));
 
 
         it('should search "applications" model with 2 application fetched', function() {
             expect(scope.applications).toEqual([]);
+            scope.term = "1.2.3.4.5.0";
             scope.search();
-            expect(scope.applications.length).toBe(2);
-            expect(scope.applications).toEqualData([{vastaukset:{henkilotiedot:{Sukunimi: "Testaaja", Etunimet: "Teppo Topias", Henkilotunnus: "120187-1234"}}, oid : "1.2.3.4.5.1", state: "Voimassa"},
-                {vastaukset:{henkilotiedot:{Sukunimi: "Virtanen", Etunimet: "Vesa Matti", Henkilotunnus: "130382-1112"}}, oid : "1.2.3.4.5.2", state: "Voimassa"}]);
+            $httpBackend.flush();
+            expect(scope.applications.length).toBe(1);
+            expect(scope.applications).toEqualData([{vastaukset:{henkilotiedot:{Sukunimi: "Testaaja", Etunimet: "Teppo Topias", Henkilotunnus: "120187-1234"}}, oid : "1.2.3.4.5.1", state: "Voimassa"}]);
         });
 
 
         it('should clear all the fetched applications when reset called', function() {
+            scope.term = "1.2.3.4.5.0";
             scope.search();
+            $httpBackend.flush();
             scope.reset();
             expect(scope.applications).toEqual([]);
         });
