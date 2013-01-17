512a8988e2843406ab23890b6da7b7b124a5057b
jteuho <jteuho@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2013-01-17 17:24:28 +0000

    OVT-3527 : Change, Add : Pretty print pois muilta kuin TestModelExporterilta

--- src/test/java/fi/vm/sade/oppija/lomake/tools/TestModelExporter.java
+++ src/test/java/fi/vm/sade/oppija/lomake/tools/TestModelExporter.java
@@ -1,5 +1,7 @@
 package fi.vm.sade.oppija.lomake.tools;
 
+import fi.vm.sade.oppija.lomake.converter.FormModelToJsonString;
+import fi.vm.sade.oppija.lomake.converter.FormModelToMap;
 import fi.vm.sade.oppija.lomake.dao.FormModelDAO;
 import fi.vm.sade.oppija.lomake.dao.impl.FormModelDummyMemoryDaoImpl;
 import fi.vm.sade.oppija.lomake.domain.FormModel;
@@ -13,15 +15,23 @@ import java.io.File;
  */
 public class TestModelExporter {
 
-
     public static void main(String[] args) {
-        new TestCommandLineTooling(new String[]{"export", "."}).execute();
+        new TestCommandLineTooling(new String[] { "export", "." }).execute();
     }
 
     private static class TestCommandLineTooling extends CommandExecutor {
 
         private TestCommandLineTooling(String[] args) {
             super(args);
+        }
+
+        protected void export(File file) {
+            System.out.println(new FormModelToMap().apply(new FormModel()));
+            final FormModel model = getService().find(new FormModel()).get(0);
+            final String contentAsString = new FormModelToJsonString().applyPretty(model);
+            final String filename = createFilename(file, model);
+            LOG.info("writing file: " + filename);
+            write(contentAsString, filename);
         }
 
         @Override
@@ -34,6 +44,5 @@ public class TestModelExporter {
             return folder.getAbsolutePath() + "/src/test/resources/test-data.json";
         }
     }
-
 
 }
