d6f10e6198fa5f881697223d4ee6f9590eee687e
hlyytika <hlyytika@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2013-02-01 08:54:07 +0000

    HAK-29: High-school degree year rule fixed.

---
+++ src/test/java/fi/vm/sade/oppija/ui/selenium/EducationBackgroundTest.java
@@ -0,0 +1,44 @@
+package fi.vm.sade.oppija.ui.selenium;
+
+import clover.com.lowagie.text.pdf.BadPdfFormatException;
+import fi.vm.sade.oppija.common.selenium.AbstractSeleniumBase;
+import fi.vm.sade.oppija.lomake.FormModelHelper;
+import fi.vm.sade.oppija.lomake.dao.impl.FormModelDummyMemoryDaoImpl;
+import fi.vm.sade.oppija.lomake.domain.FormModel;
+import fi.vm.sade.oppija.lomake.domain.builders.FormModelBuilder;
+import fi.vm.sade.oppija.lomake.domain.elements.questions.Radio;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * @author Hannu Lyytikainen
+ */
+public class EducationBackgroundTest extends AbstractSeleniumBase {
+
+    private FormModelHelper formModelHelper;
+
+    @Before
+    public void init() {
+        FormModelDummyMemoryDaoImpl formModel = new FormModelDummyMemoryDaoImpl();
+        this.formModelHelper = initModel(formModel.getModel());
+
+    }
+
+    @Test
+    public void testRule() {
+        final String startUrl = formModelHelper.getFormUrl(formModelHelper.getFirstForm().getPhase("koulutustausta"));
+
+        WebDriver driver = seleniumHelper.getDriver();
+        driver.get(getBaseUrl() + "/" + startUrl); //  lomake/Yhteishaku/yhteishaku/henkilotiedot
+
+        driver.findElement(new By.ById("millatutkinnolla_tutkinto1")).click();
+
+
+        System.out.println("dd");
+
+    // millatutkinnolla_tutkinto1
+    }
+
+}
