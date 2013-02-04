c56521e78aca7bc6669fac0c4a812f90b0930bf4
vrajapaju <vrajapaju@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2013-02-04 11:36:01 +0000

    Fix:  Unused Imports.

--- src/main/java/fi/vm/sade/oppija/hakemus/domain/dto/ApplicationDTO.java
+++ src/main/java/fi/vm/sade/oppija/hakemus/domain/dto/ApplicationDTO.java
@@ -1,8 +1,22 @@
+/*
+ * Copyright (c) 2012 The Finnish Board of Education - Opetushallitus
+ *
+ * This program is free software:  Licensed under the EUPL, Version 1.1 or - as
+ * soon as they will be approved by the European Commission - subsequent versions
+ * of the EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://www.osor.eu/eupl/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * European Union Public Licence for more details.
+ */
+
 package fi.vm.sade.oppija.hakemus.domain.dto;
 
 import fi.vm.sade.oppija.hakemus.domain.Application;
-import fi.vm.sade.oppija.lomake.domain.FormId;
-import fi.vm.sade.oppija.lomake.domain.User;
 import org.codehaus.jackson.annotate.JsonCreator;
 import org.codehaus.jackson.annotate.JsonProperty;
 import org.codehaus.jackson.annotate.JsonTypeInfo;
@@ -24,9 +38,9 @@ public class ApplicationDTO {
 
     @JsonCreator
     public ApplicationDTO(@JsonProperty(value = "oid") final String oid,
-                       @JsonProperty(value = "asId") final String asId,
-                       @JsonProperty(value = "answers") Map<String, String> answers,
-                       @JsonProperty(value = "additionalInfo") Map<String, String> additionalInfo) {
+                          @JsonProperty(value = "asId") final String asId,
+                          @JsonProperty(value = "answers") Map<String, String> answers,
+                          @JsonProperty(value = "additionalInfo") Map<String, String> additionalInfo) {
         this.oid = oid;
         this.asId = asId;
         this.answers = answers;
