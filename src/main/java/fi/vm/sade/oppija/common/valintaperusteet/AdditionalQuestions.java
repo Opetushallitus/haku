5c45ff88049756ba9f56b4034dae325485b88d0f
samis <samis@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2013-02-12 08:25:08 +0000

    HAK-46 initial dump

---
+++ src/main/java/fi/vm/sade/oppija/common/valintaperusteet/AdditionalQuestions.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2012 The Finnish Board of Education - Opetushallitus
+ *
+ * This program is free software:  Licensed under the EUPL, Version 1.1 or - as
+ * soon as they will be approved by the European Commission - subsequent versions
+ * of the EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://www.osor.eu/eupl/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * European Union Public Licence for more details.
+ */
+package fi.vm.sade.oppija.common.valintaperusteet;
+
+import java.util.List;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Predicate;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.LinkedListMultimap;
+
+public class AdditionalQuestions {
+
+    private final LinkedListMultimap<String /* oid */, InputParameter> questions = LinkedListMultimap.create();
+
+    public void addParameter(final String oid, final InputParameter param) {
+        questions.put(oid, param);
+    }
+
+    /**
+     * Get questions for hakukohde.
+     */
+    public List<InputParameter> getQuestistionsForHakukohde(final String oid) {
+        Preconditions.checkNotNull(oid, "Oid cannot be null");
+        return ImmutableList.copyOf(questions.get(oid));
+    }
+
+    /**
+     * Get questions for hakukohde, phase
+     */
+    public List<InputParameter> getQuestistionsForHakukohdePhase(final String oid, final String phase) {
+        Preconditions.checkNotNull(oid, "Oid cannot be null");
+        Preconditions.checkNotNull(phase, "Phase cannot be null");
+        return ImmutableList.copyOf(Iterables.filter(getQuestistionsForHakukohde(oid), new Predicate<InputParameter>() {
+            public boolean apply(InputParameter input) {
+                return phase.equals(input.getPhase());
+            }
+        }));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s, questions (%d) %s", this.getClass().getName(), this.questions.entries().size(), questions);
+    }
+}
---
+++ src/main/java/fi/vm/sade/oppija/common/valintaperusteet/impl/ValintaperusteetServiceMockImpl.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2012 The Finnish Board of Education - Opetushallitus
+ *
+ * This program is free software:  Licensed under the EUPL, Version 1.1 or - as
+ * soon as they will be approved by the European Commission - subsequent versions
+ * of the EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at: http://www.osor.eu/eupl/
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * European Union Public Licence for more details.
+ */
+package fi.vm.sade.oppija.common.valintaperusteet.impl;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.google.common.collect.LinkedListMultimap;
+
+import fi.vm.sade.oppija.common.valintaperusteet.AdditionalQuestions;
+import fi.vm.sade.oppija.common.valintaperusteet.InputParameter;
+import fi.vm.sade.oppija.common.valintaperusteet.ValintaperusteetService;
+
+@Service
+@Profile("dev")
+public class ValintaperusteetServiceMockImpl implements ValintaperusteetService {
+
+    private final LinkedListMultimap<String, InputParameter> questions = LinkedListMultimap.create();
+
+    public ValintaperusteetServiceMockImpl() {
+        //TODO add reasonable mock data
+        addQ("1234567", "1", "aidinkieli_yo", "DESIMAALILUKU");
+        addQ("1234567", "2", "matematiikka_yo", "DESIMAALILUKU");
+    }
+
+    private void addQ(String oid, String phase, String key, String type) {
+        final InputParameter param = new InputParameter(key, type, phase);
+        questions.put(oid, param);
+    }
+
+    @Override
+    public AdditionalQuestions retrieveAdditionalQuestions(List<String> oids) throws IOException {
+        AdditionalQuestions aq = new AdditionalQuestions();
+        for (String oid : oids) {
+            for (InputParameter param : questions.get(oid)) {
+                aq.addParameter(oid, param);
+            }
+        }
+        return aq;
+    }
+}
