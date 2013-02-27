860c7337ce8fbda8b57452cb688571ad713b443b
mmajapuro <mmajapuro@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2013-02-27 08:59:57 +0000

    HAK-89 Hakemus-api

--- haku-app/src/main/java/fi/vm/sade/oppija/hakemus/service/impl/HakemusServiceImpl.java
+++ haku-app/src/main/java/fi/vm/sade/oppija/hakemus/service/impl/HakemusServiceImpl.java
@@ -7,6 +7,7 @@ import fi.vm.sade.oppija.lomake.domain.exception.ResourceNotFoundException;
 import fi.vm.sade.service.hakemus.HakemusService;
 import fi.vm.sade.service.hakemus.schema.HakemusTyyppi;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import javax.jws.WebParam;
@@ -18,26 +19,24 @@ import java.util.List;
  */
 @Service
 public class HakemusServiceImpl implements HakemusService {
+    private ApplicationService applicationService;
 
     @Autowired
-    private ApplicationService applicationService;
+    public HakemusServiceImpl(ApplicationService applicationService) {
+       this.applicationService = applicationService;
+    }
 
     @Override
     public java.util.List<fi.vm.sade.service.hakemus.schema.HakemusTyyppi> haeHakemukset(
             @WebParam(name = "hakukohdeOid", targetNamespace = "")
             java.util.List<java.lang.String> hakukohdeOid) {
-        try {
-            applicationService.getApplication("123");
-        } catch (ResourceNotFoundException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-        //List<Application> applications = applicationService.findApplications("", new ApplicationQueryParameters(hakukohdeOid));
+        List<Application> applications = applicationService.findApplications("", new ApplicationQueryParameters(hakukohdeOid));
         List<fi.vm.sade.service.hakemus.schema.HakemusTyyppi> hakemukset = new ArrayList<HakemusTyyppi>();
-        /*for (Application app : applications) {
+        for (Application app : applications) {
             HakemusTyyppi ht = new HakemusTyyppi();
             ht.setHakemusOid(app.getOid());
             hakemukset.add(ht);
-        } */
+        }
         return hakemukset;
     }
 }
