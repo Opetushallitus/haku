6f14aa6ebb07ee81f4944776b83954379085060e
mmajapuro <mmajapuro@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2013-02-28 10:08:12 +0000

    HAK-89 Hakemus-api

---
+++ haku-app/src/test/java/fi/vm/sade/oppija/hakemus/service/impl/HakemusServiceImplTest.java
@@ -0,0 +1,53 @@
+package fi.vm.sade.oppija.hakemus.service.impl;
+
+import fi.vm.sade.oppija.hakemus.dao.ApplicationQueryParameters;
+import fi.vm.sade.oppija.hakemus.domain.Application;
+import fi.vm.sade.oppija.hakemus.service.ApplicationService;
+import fi.vm.sade.service.hakemus.HakemusService;
+import fi.vm.sade.service.hakemus.schema.HakemusTyyppi;
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.core.convert.ConversionService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Mikko Majapuro
+ */
+public class HakemusServiceImplTest {
+
+    private HakemusService hakemusService;
+    private ApplicationService applicationService;
+    private ConversionService conversionService;
+
+    @Before
+    public void setUp() {
+        applicationService = mock(ApplicationService.class);
+        Application app = new Application("1.2.3.4.5.1");
+        List<Application> applications = new ArrayList<Application>();
+        applications.add(app);
+        when(applicationService.findApplications(eq(""), any(ApplicationQueryParameters.class))).thenReturn(applications);
+        conversionService = mock(ConversionService.class);
+        HakemusTyyppi hakemus = new HakemusTyyppi();
+        hakemus.setHakemusOid(app.getOid());
+        when(conversionService.convert(any(Object.class), eq(HakemusTyyppi.class))).thenReturn(hakemus);
+        hakemusService = new HakemusServiceImpl(applicationService, conversionService);
+    }
+
+    @Test
+    public void testHaeHakemukset() {
+        List<String> oids = new ArrayList<String>();
+        oids.add("1.2.3.4.3.999");
+        List<HakemusTyyppi> hakemukset = hakemusService.haeHakemukset(oids);
+        Assert.assertNotNull(hakemukset);
+        Assert.assertEquals(1, hakemukset.size());
+        Assert.assertEquals("1.2.3.4.5.1", hakemukset.get(0).getHakemusOid());
+        verify(applicationService, times(1)).findApplications(eq(""), any(ApplicationQueryParameters.class));
+        verify(conversionService, times(1)).convert(any(Object.class), eq(HakemusTyyppi.class));
+    }
+}
