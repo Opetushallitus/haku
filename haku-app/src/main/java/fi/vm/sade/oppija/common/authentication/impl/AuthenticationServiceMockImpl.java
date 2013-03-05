ebb6cf7572a16a7c3ea6aeaf88b95ddfbabfa00c
vrajapaju <vrajapaju@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2013-03-05 16:08:30 +0000

    Fix: Final Field Could Be Static.

--- haku-app/src/main/java/fi/vm/sade/oppija/common/authentication/impl/AuthenticationServiceMockImpl.java
+++ haku-app/src/main/java/fi/vm/sade/oppija/common/authentication/impl/AuthenticationServiceMockImpl.java
@@ -27,7 +27,7 @@ import org.springframework.stereotype.Service;
 @Profile("dev")
 public class AuthenticationServiceMockImpl implements AuthenticationService {
 
-    private final String oidPrefix = "1.2.246.562.24.";
+    private static final String oidPrefix = "1.2.246.562.24.";
 
     public String addPerson(Person person) {
         return oidPrefix + String.format("%011d", Math.round(Math.random() * 1000000000));
