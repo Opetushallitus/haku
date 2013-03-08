1d80e8df1736030329b837d6896aad68badbc2b1
hlyytika <hlyytika@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2013-03-08 09:02:20 +0000

    KSH-16: Tarjonta event listener configuration

--- koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/event/TarjontaEventHandler.java
+++ koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/event/TarjontaEventHandler.java
@@ -2,14 +2,28 @@ package fi.vm.sade.koulutusinformaatio.event;
 
 import fi.vm.sade.events.Event;
 import fi.vm.sade.events.EventHandler;
+import fi.vm.sade.events.impl.EventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  * @author Hannu Lyytikainen
  */
+//@Component
 public class TarjontaEventHandler implements EventHandler {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(TarjontaEventHandler.class);
+
+    @Autowired
+    public TarjontaEventHandler(EventListener eventListener) {
+        eventListener.addEventHandler(this);
+
+    }
 
     @Override
     public void handleEvent(Event event) {
-        //To change body of implemented methods use File | Settings | File Templates.
+        // update education data...
     }
 }
---
+++ koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/event/mock/TarjontaEventHandlerMock.java
@@ -0,0 +1,16 @@
+package fi.vm.sade.koulutusinformaatio.event.mock;
+
+import fi.vm.sade.events.Event;
+import fi.vm.sade.events.EventHandler;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Hannu Lyytikainen
+ */
+@Component
+public class TarjontaEventHandlerMock implements EventHandler {
+    @Override
+    public void handleEvent(Event event) {
+        // mock impl
+    }
+}
