a14f4521a487db8a44573db6ca3bd0985303f980
mmajapuro <mmajapuro@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2013-03-22 11:21:19 +0000

    KSH-63 : Add : Model mapper

--- koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/service/impl/EducationDataServiceImpl.java
+++ koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/service/impl/EducationDataServiceImpl.java
@@ -3,6 +3,7 @@ package fi.vm.sade.koulutusinformaatio.service.impl;
 import fi.vm.sade.koulutusinformaatio.dao.ApplicationOptionDAO;
 import fi.vm.sade.koulutusinformaatio.dao.LearningOpportunityParentDAO;
 import fi.vm.sade.koulutusinformaatio.service.EducationDataService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,11 +15,13 @@ public class EducationDataServiceImpl implements EducationDataService {
 
     private LearningOpportunityParentDAO learningOpportunityParentDAO;
     private ApplicationOptionDAO applicationOptionDAO;
+    private ModelMapper modelMapper;
 
     @Autowired
     public EducationDataServiceImpl(LearningOpportunityParentDAO learningOpportunityParentDAO,
-                                    ApplicationOptionDAO applicationOptionDAO) {
+                                    ApplicationOptionDAO applicationOptionDAO, ModelMapper modelMapper) {
         this.learningOpportunityParentDAO = learningOpportunityParentDAO;
         this.applicationOptionDAO = applicationOptionDAO;
+        this.modelMapper = modelMapper;
     }
 }
