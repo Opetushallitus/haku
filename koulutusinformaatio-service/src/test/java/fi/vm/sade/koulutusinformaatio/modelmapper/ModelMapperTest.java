7e7ebb160a8693b1c4a264fdeb819efab4e22ebd
mmajapuro <mmajapuro@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2013-03-25 12:27:09 +0000

    KSH-63 : Change: Object mapper test update

--- koulutusinformaatio-service/src/test/java/fi/vm/sade/koulutusinformaatio/modelmapper/ModelMapperTest.java
+++ koulutusinformaatio-service/src/test/java/fi/vm/sade/koulutusinformaatio/modelmapper/ModelMapperTest.java
@@ -16,8 +16,10 @@
 
 package fi.vm.sade.koulutusinformaatio.modelmapper;
 
+import fi.vm.sade.koulutusinformaatio.dao.entity.ApplicationOptionEntity;
 import fi.vm.sade.koulutusinformaatio.dao.entity.ChildLearningOpportunityEntity;
 import fi.vm.sade.koulutusinformaatio.dao.entity.ParentLearningOpportunityEntity;
+import fi.vm.sade.koulutusinformaatio.domain.ApplicationOption;
 import fi.vm.sade.koulutusinformaatio.domain.ChildLearningOpportunity;
 import fi.vm.sade.koulutusinformaatio.domain.ParentLearningOpportunity;
 import org.junit.Test;
@@ -41,9 +43,21 @@ public class ModelMapperTest {
         ParentLearningOpportunity parent = new ParentLearningOpportunity();
         parent.setId("123");
         parent.setName("parentName");
+        parent.setEducationDegree("degree");
+
+        List<ApplicationOption> applicationOptions = new ArrayList<ApplicationOption>();
+        ApplicationOption ao = new ApplicationOption();
+        ao.setId("8.8.8");
+        ao.setApplicationSystemId("3.4.3");
+        ao.setEducationDegree("e degree");
+        ao.setName("ao name");
+        applicationOptions.add(ao);
+        parent.setApplicationOptions(applicationOptions);
 
         ChildLearningOpportunity child = new ChildLearningOpportunity("111", "child1Name");
+        child.setApplicationOptions(applicationOptions);
         ChildLearningOpportunity child2 = new ChildLearningOpportunity("222", "child2Name");
+        child2.setApplicationOptions(applicationOptions);
 
         List<ChildLearningOpportunity> children = new ArrayList<ChildLearningOpportunity>();
         children.add(child);
@@ -61,6 +75,11 @@ public class ModelMapperTest {
         assertEquals(child.getName(), entity.getChildren().get(0).getName());
         assertEquals(child2.getId(), entity.getChildren().get(1).getId());
         assertEquals(child2.getName(), entity.getChildren().get(1).getName());
+        assertNotNull(entity.getApplicationOptions());
+        assertEquals(1, entity.getApplicationOptions().size());
+        assertEquals(ao.getId(), entity.getApplicationOptions().get(0).getId());
+        assertEquals(ao.getId(), entity.getChildren().get(0).getApplicationOptions().get(0).getId());
+        assertEquals(ao.getId(), entity.getChildren().get(1).getApplicationOptions().get(0).getId());
     }
 
     @Test
@@ -69,14 +88,24 @@ public class ModelMapperTest {
         entity.setId("999");
         entity.setName("entityName");
 
+        List<ApplicationOptionEntity> aos = new ArrayList<ApplicationOptionEntity>();
+        ApplicationOptionEntity ao = new ApplicationOptionEntity();
+        ao.setId("900");
+        ao.setName("ao name");
+        ao.setApplicationSystemId("54543");
+        ao.setEducationDegree("degree");
+        aos.add(ao);
+        entity.setApplicationOptions(aos);
         List<ChildLearningOpportunityEntity> children = new ArrayList<ChildLearningOpportunityEntity>();
         ChildLearningOpportunityEntity child = new ChildLearningOpportunityEntity();
         child.setId("444");
         child.setName("child1EntityName");
+        child.setApplicationOptions(aos);
         children.add(child);
         ChildLearningOpportunityEntity child2 = new ChildLearningOpportunityEntity();
         child2.setId("555");
         child2.setName("child2EntityName");
+        child2.setApplicationOptions(aos);
         children.add(child2);
 
         entity.setChildren(children);
@@ -91,5 +120,12 @@ public class ModelMapperTest {
         assertEquals(child.getName(), domain.getChildren().get(0).getName());
         assertEquals(child2.getId(), domain.getChildren().get(1).getId());
         assertEquals(child2.getName(), domain.getChildren().get(1).getName());
+        assertNotNull(domain.getApplicationOptions());
+        assertEquals(1, domain.getApplicationOptions().size());
+        assertEquals(ao.getId(), domain.getApplicationOptions().get(0).getId());
+        assertEquals(1, domain.getChildren().get(0).getApplicationOptions().size());
+        assertEquals(1, domain.getChildren().get(1).getApplicationOptions().size());
+        assertEquals(ao.getId(), domain.getChildren().get(0).getApplicationOptions().get(0).getId());
+        assertEquals(ao.getId(), domain.getChildren().get(1).getApplicationOptions().get(0).getId());
     }
 }
