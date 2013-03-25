be6d7474fb41c11e78d94cd1d0c3b8cc00208dbf
vrajapaju <vrajapaju@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2013-03-25 16:19:13 +0000

    Fix:  Unused Imports

--- haku-app/src/main/java/fi/vm/sade/oppija/tarjonta/controller/IndexController.java
+++ haku-app/src/main/java/fi/vm/sade/oppija/tarjonta/controller/IndexController.java
@@ -22,7 +22,6 @@ import fi.vm.sade.koulutusinformaatio.service.IndexerService;
 import fi.vm.sade.koulutusinformaatio.service.UpdateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 
@@ -31,7 +30,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
 
 @Path("admin/index")
