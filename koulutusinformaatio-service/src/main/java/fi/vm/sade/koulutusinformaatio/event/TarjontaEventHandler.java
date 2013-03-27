11ba376f40c9f0fd9284319186980ccc0ed356d0
hlyytika <hlyytika@15ac3db4-bff1-4aba-a278-c1783e67ca60>
2013-03-27 09:03:44 +0000

    Enabled tarjonta update event handling.

--- koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/event/TarjontaEventHandler.java
+++ koulutusinformaatio-service/src/main/java/fi/vm/sade/koulutusinformaatio/event/TarjontaEventHandler.java
@@ -1,17 +1,17 @@
 package fi.vm.sade.koulutusinformaatio.event;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import fi.vm.sade.events.Event;
 import fi.vm.sade.events.EventHandler;
 import fi.vm.sade.events.impl.EventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  * @author Hannu Lyytikainen
  */
-//@Component
+@Component
 public class TarjontaEventHandler implements EventHandler {
 
     public static final Logger LOGGER = LoggerFactory.getLogger(TarjontaEventHandler.class);
@@ -25,5 +25,8 @@ public class TarjontaEventHandler implements EventHandler {
     @Override
     public void handleEvent(Event event) {
         // update education data...
+        LOGGER.debug("Received event: " + "\n" +
+        event.getCategory() + "\n" +
+        event.toString());
     }
 }
