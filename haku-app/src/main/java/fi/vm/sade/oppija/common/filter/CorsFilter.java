a8efd3e0f489d8425f21019fbb94453225904cc3
vpaju <ville.rajapaju@proactum.fi>
2013-04-09 14:50:17 +0300

    Fix padding of parentheses

--- haku-app/src/main/java/fi/vm/sade/oppija/common/filter/CorsFilter.java
+++ haku-app/src/main/java/fi/vm/sade/oppija/common/filter/CorsFilter.java
@@ -11,14 +11,14 @@ public class CorsFilter implements ContainerResponseFilter {
     @Override
     public ContainerResponse filter(ContainerRequest containerRequest, ContainerResponse containerResponse) {
 
-        if ( containerRequest.getRequestHeaders().containsKey("access-control-request-method") ) {
-            for ( String value : containerRequest.getRequestHeaders().get("access-control-request-method") ) {
-                containerResponse.getHttpHeaders().add("Access-Control-Allow-Methods", value );
+        if (containerRequest.getRequestHeaders().containsKey("access-control-request-method")) {
+            for (String value : containerRequest.getRequestHeaders().get("access-control-request-method")) {
+                containerResponse.getHttpHeaders().add("Access-Control-Allow-Methods", value);
             }
         }
-        if ( containerRequest.getRequestHeaders().containsKey("access-control-request-headers") ) {
-            for ( String value : containerRequest.getRequestHeaders().get("access-control-request-headers") ) {
-                containerResponse.getHttpHeaders().add("Access-Control-Allow-Headers", value );
+        if (containerRequest.getRequestHeaders().containsKey("access-control-request-headers")) {
+            for (String value : containerRequest.getRequestHeaders().get("access-control-request-headers")) {
+                containerResponse.getHttpHeaders().add("Access-Control-Allow-Headers", value);
             }
         }
         containerResponse.getHttpHeaders().add("Access-Control-Allow-Origin", "*");
