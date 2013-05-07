e7a05592ffea5612bcbb0d6c6ca8eb093c6764fb
vpaju <ville.rajapaju@proactum.fi>
2013-05-07 18:29:35 +0300

    Change tabs to spaces

--- haku-app/src/main/java/fi/vm/sade/oppija/common/filter/NoCacheFilter.java
+++ haku-app/src/main/java/fi/vm/sade/oppija/common/filter/NoCacheFilter.java
@@ -6,11 +6,11 @@ import com.sun.jersey.spi.container.ContainerResponseFilter;
 
 public class NoCacheFilter implements ContainerResponseFilter {
 
-	@Override
-	public ContainerResponse filter(ContainerRequest request, ContainerResponse response) {
-		response.getHttpHeaders().add("Cache-Control", "no-cache, no-store, must-revalidate");
-		response.getHttpHeaders().add("Pragma", "no-cache");
-		return response;
-	}
+    @Override
+    public ContainerResponse filter(ContainerRequest request, ContainerResponse response) {
+        response.getHttpHeaders().add("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.getHttpHeaders().add("Pragma", "no-cache");
+        return response;
+    }
 
 }
