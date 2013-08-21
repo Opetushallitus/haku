d24b070e5c24a7a747f6d0e1c80b3c37a4096561
majapuro <mikko.majapuro@gofore.com>
2013-08-21 12:29:22 +0300

    Merge branch 'devel' of github.com:Opetushallitus/haku into devel
    Conflicts:
    	haku-app/src/main/java/fi/vm/sade/oppija/lomake/service/AdditionalQuestionService.java
    	haku-app/src/main/java/fi/vm/sade/oppija/lomakkeenhallinta/FormGenerator.java

--- haku-app/src/main/java/fi/vm/sade/oppija/lomakkeenhallinta/FormGenerator.java
+++ haku-app/src/main/java/fi/vm/sade/oppija/lomakkeenhallinta/FormGenerator.java
@@ -2,7 +2,7 @@ package fi.vm.sade.oppija.lomakkeenhallinta;
 
 import com.google.common.collect.ImmutableList;
 import fi.vm.sade.oppija.common.koodisto.KoodistoService;
-import fi.vm.sade.oppija.lomake.domain.ApplicationPeriod;
+import fi.vm.sade.oppija.lomake.domain.ApplicationSystem;
 import fi.vm.sade.oppija.lomake.domain.I18nText;
 import fi.vm.sade.oppija.lomakkeenhallinta.service.tarjonta.TarjontaService;
 import fi.vm.sade.oppija.lomakkeenhallinta.yhteishaku2013.Yhteishaku2013;
@@ -29,13 +29,13 @@ public class FormGenerator {
         this.aoid = aoid;
     }
 
-    public List<ApplicationPeriod> generate() {
-        List<ApplicationPeriod> forms = new ArrayList<ApplicationPeriod>();
+    public List<ApplicationSystem> generate() {
+        List<ApplicationSystem> forms = new ArrayList<ApplicationSystem>();
         Map<String, Map<String, String>> applicationSystems = tarjontaService.getApplicationSystemOidsAndNames();
         for (Map.Entry<String, Map<String, String>> applicationSystem : applicationSystems.entrySet()) {
             Yhteishaku2013 e = new Yhteishaku2013(koodistoService,
                     applicationSystem.getKey(), aoid, new I18nText(applicationSystems.get(applicationSystem)));
-            forms.add(e.getApplicationPeriod());
+            forms.add(e.getApplicationSystem());
         }
         return ImmutableList.copyOf(forms);
     }
