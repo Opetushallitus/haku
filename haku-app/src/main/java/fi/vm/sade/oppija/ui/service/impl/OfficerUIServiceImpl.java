2dacda3e18d43e460d3d944b2caa14ff9561ccb3
jteuho <juho.teuho@gofore.com>
2013-08-28 16:53:51 +0300

    HAK-148 Hide edit buttons, when editing is not allowed

--- haku-app/src/main/java/fi/vm/sade/oppija/ui/service/impl/OfficerUIServiceImpl.java
+++ haku-app/src/main/java/fi/vm/sade/oppija/ui/service/impl/OfficerUIServiceImpl.java
@@ -94,6 +94,7 @@ public class OfficerUIServiceImpl implements OfficerUIService {
         officerApplicationResponse.setErrorMessages(validationResult.getErrorMessages());
         officerApplicationResponse.setAdditionalQuestions(getAdditionalQuestions(application));
         officerApplicationResponse.addObjectToModel("koulutusinformaatioBaseUrl", koulutusinformaatioBaseUrl);
+        officerApplicationResponse.addObjectToModel("virkailijaEditAllowed", hakuPermissionService.userCanUpdateApplication(application));
         return officerApplicationResponse;
     }
 
