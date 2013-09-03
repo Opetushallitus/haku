6a5eabe5c16fe3dd9a271ff435494cef5a5605a3
vpaju <ville.rajapaju@proactum.fi>
2013-09-03 14:23:23 +0300

    fix kielitaitoteema rules

--- haku-app/src/main/java/fi/vm/sade/oppija/lomakkeenhallinta/yhteishaku2013/phase/osaaminen/KielitaitokysymyksetTheme.java
+++ haku-app/src/main/java/fi/vm/sade/oppija/lomakkeenhallinta/yhteishaku2013/phase/osaaminen/KielitaitokysymyksetTheme.java
@@ -25,27 +25,19 @@ public final class KielitaitokysymyksetTheme {
             "aidinkieli",
             "perusopetuksen_kieli",
             "lukion_kieli",
-            "PK_AI_OPPIAINE",
-            "LK_AI_OPPIAINE"
     };
 
     public static Element createKielitaitokysymyksetTheme() {
         Expr suomiOnAidinkieliTaiKouluSuomeksi = atLeastOneVariableEqualsToValue("FI", languageQuestions);
         Expr ruotsiOnAidinkieliTaiKouluRuotsiksi = atLeastOneVariableEqualsToValue("SV", languageQuestions);
 
-        Expr suomenArvosanatRiittaa = arvosanatarkastus("FI");
-        Expr ruotsinArvosanatRiittaa = arvosanatarkastus("SV");
-
 
         Expr haettuSuomenkieliseenKoulutukseen = atLeastOneVariableEqualsToValue("FI", preferenceIds);
         Expr haettuRuotsinkieliseenKoulutukseen = atLeastOneVariableEqualsToValue("SV", preferenceIds);
 
-        Expr riittavaSuomenkielenTaito = new Or(suomiOnAidinkieliTaiKouluSuomeksi, suomenArvosanatRiittaa);
-        Expr riittavaRuotsinkielenTaito = new Or(ruotsiOnAidinkieliTaiKouluRuotsiksi, ruotsinArvosanatRiittaa);
 
-
-        Expr kysytaankoSuomi = new And(haettuSuomenkieliseenKoulutukseen, new Not(riittavaSuomenkielenTaito));
-        Expr kysytaankoRuotsi = new And(haettuRuotsinkieliseenKoulutukseen, new Not(riittavaRuotsinkielenTaito));
+        Expr kysytaankoSuomi = new And(haettuSuomenkieliseenKoulutukseen, new Not(suomiOnAidinkieliTaiKouluSuomeksi));
+        Expr kysytaankoRuotsi = new And(haettuRuotsinkieliseenKoulutukseen, new Not(ruotsiOnAidinkieliTaiKouluRuotsiksi));
 
         Expr naytetaankoKielitaitoteema = new Or(kysytaankoSuomi, kysytaankoRuotsi);
 
@@ -70,21 +62,5 @@ public final class KielitaitokysymyksetTheme {
         addDefaultTrueFalseOptions(radio);
         addRequiredValidator(radio);
         return radio;
-    }
-
-    private static Expr arvosanatarkastus(final String language) {
-        Expr pkA1OnSuomi = atLeastOneVariableEqualsToValue(language, "PK_A1_OPPIAINE");
-        Expr lkA1OnSuomi = atLeastOneVariableEqualsToValue(language, "LK_A1_OPPIAINE");
-        Expr pkA2OnSuomi = atLeastOneVariableEqualsToValue(language, "PK_A2_OPPIAINE");
-        Expr lkA2OnSuomi = atLeastOneVariableEqualsToValue(language, "LK_A2_OPPIAINE");
-        Expr pkA1ArvosanaOnHyva = atLeastOneValueEqualsToVariable("PK_A1", "7", "8", "9", "10");
-        Expr lkA1ArvosanaOnHyva = atLeastOneValueEqualsToVariable("LK_A1", "7", "8", "9", "10");
-        Expr pkA2ArvosanaOnHyva = atLeastOneValueEqualsToVariable("PK_A2", "7", "8", "9", "10");
-        Expr lkA2ArvosanaOnHyva = atLeastOneValueEqualsToVariable("LK_A2", "7", "8", "9", "10");
-        Expr pkA1Riittaa = new And(pkA1OnSuomi, pkA1ArvosanaOnHyva);
-        Expr lkA1Riittaa = new And(lkA1OnSuomi, lkA1ArvosanaOnHyva);
-        Expr pkA2Riittaa = new And(pkA2OnSuomi, pkA2ArvosanaOnHyva);
-        Expr lkA2Riittaa = new And(lkA2OnSuomi, lkA2ArvosanaOnHyva);
-        return new Or(new Or(pkA1Riittaa, pkA2Riittaa), new Or(lkA1Riittaa, lkA2Riittaa));
     }
 }
