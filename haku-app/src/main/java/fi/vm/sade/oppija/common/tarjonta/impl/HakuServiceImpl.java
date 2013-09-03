ca1912e92ae73d1277b8c4a5c8bff246d9206dd7
majapuro <mikko.majapuro@gofore.com>
2013-09-03 12:40:47 +0300

    HAK-313 : CHANGE : Application System and form generating refactored.

--- haku-app/src/main/java/fi/vm/sade/oppija/common/tarjonta/impl/HakuServiceImpl.java
+++ haku-app/src/main/java/fi/vm/sade/oppija/common/tarjonta/impl/HakuServiceImpl.java
@@ -31,7 +31,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import javax.ws.rs.core.MediaType;
@@ -41,7 +40,6 @@ import java.util.List;
  * @author Mikko Majapuro
  */
 @Service
-@Profile("default")
 public class HakuServiceImpl implements HakuService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HakuServiceImpl.class);
