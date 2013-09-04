3bd6a00b7e5f4541c75507cef3d875f5e94ded58
majapuro <mikko.majapuro@gofore.com>
2013-09-04 16:27:08 +0300

    OVT-5300 OVT-5183 OVT-5182 : BUG FIX : Preference table sorting fixes.

--- haku-app/src/main/webapp/resources/javascript/preferencetable.js
+++ haku-app/src/main/webapp/resources/javascript/preferencetable.js
@@ -1,107 +1,17 @@
-(function($) {
-    $.fn.getAttributes = function () {
-        var elem = this,
-            attr = {};
-
-        if(elem.length) $.each(elem.get(0).attributes, function(v,n) {
-            n = n.nodeName||n.name;
-            v = elem.attr(n);
-            if(v != undefined && v !== false) attr[n] = v
-        })
-
-        return attr
-    }
-})(jQuery);
-
 (function(){
     var sortabletable = {
+        sortUrl : document.URL.split("?")[0] + "/" + sortabletable_settings.elementId,
         moveRow : function(id, targetId) {
+            var fData = $("form").serialize(), tmpPrefix = "preferencex";
+            fData = fData.split(id).join(tmpPrefix);
+            fData = fData.split(targetId).join(id);
+            fData = fData.split(tmpPrefix).join(targetId);
 
-            var $clone, $targetClone, $source, $target;
-
-            $source = $('#' + id + "-row-content");
-            $clone = $source.clone();
-            $target = $('#' + targetId + "-row-content");
-            $targetClone = $target.clone();
-
-            var checkedTargetIds = sortabletable.getChecked($source, id, targetId);
-            var checkedSourceIds = sortabletable.getChecked($target, targetId, id);
-
-            $source.empty().append($targetClone.children());
-            $target.empty().append($clone.children());
-
-            sortabletable.replaceAllAttrSubString($source, targetId, id);
-            sortabletable.replaceAllAttrSubString($target, id, targetId);
-
-            sortabletable.replaceRuleSettings($source, targetId, id);
-            sortabletable.replaceRuleSettings($target, id, targetId);
-
-            sortabletable.updateChecked(checkedTargetIds);
-            sortabletable.updateChecked(checkedSourceIds);
-
-            preferenceRow.init();
-        },
-        replaceAllAttrSubString : function($elem, findString, replaceWith) {
-            $elem.find("*").each(function() {
-                var attrs = $(this).getAttributes();
-                for (attrName in attrs) {
-                    if (attrName) {
-                        if (!($(this).hasClass("related-question-rule-class") &&
-                            attrName === "id")) {
-                            var oldVal =  $(this).attr(attrName);
-                            if (oldVal && jQuery.type(oldVal) === "string") {
-                                var newVal = oldVal.replace(findString, replaceWith);
-                                $(this).prop(attrName, newVal);
-                            }
-                        }
-                    }
-                }
-                var dataAttrs = $(this).data();
-                for (dataName in dataAttrs) {
-                    if (dataName) {
-                        var oldVal =  $(this).data(dataName);
-                        if (oldVal && jQuery.type(oldVal) === "string") {
-                            var newVal = oldVal.replace(findString, replaceWith);
-                            $(this).data(dataName, newVal);
-                        }
-                    }
-                }
+            $.post(sortabletable.sortUrl, fData).done(function(data) {
+                $("#" + sortabletable_settings.elementId).replaceWith(data);
             });
-        },
-
-        replaceRuleSettings : function($elem, findString, replaceWith) {
-            $elem.find(".related-question-rule-class").each(function() {
-                var ruleId = this.id;
-                var settings = window[ruleId.replace(new RegExp('-', 'g'), '_')];
-                for (var index in settings.childIds) {
-                    var cId = settings.childIds[index];
-                    settings.childIds[index] = cId.replace(new RegExp(findString, 'g'), replaceWith);
-                }
-                settings.expression = settings.expression.replace(new RegExp(findString, 'g'), replaceWith);
-                settings.relatedSelector = settings.relatedSelector.replace(new RegExp(findString, 'g'), replaceWith);
-                $(settings.relatedSelector).unbind();
-                $(settings.relatedSelector).change(window[ruleId.replace(new RegExp('-', 'g'), '_') + "_func"]);
-            });
-        },
-
-        getChecked : function($elem, findIdString, replaceIdWith) {
-            var cc  = new Array();
-            $elem.find("input:checked").each(function(i) {
-                cc[i] = this.id.replace(new RegExp(findIdString, 'g'), replaceIdWith);
-            });
-            return cc;
-        },
-
-        updateChecked: function(checkedElemIds) {
-            if (checkedElemIds) {
-                $.each(checkedElemIds, function(index, value) {
-                    $("#" + value).prop("checked", true);
-                });
-            }
         }
     };
-
-
 
 	$('button.sort').click(function(event){
 	    var id = $(this).data('id'),
