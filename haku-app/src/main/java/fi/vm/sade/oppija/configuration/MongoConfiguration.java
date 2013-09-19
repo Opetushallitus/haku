fad5bc648596130470e0590c9fddb225614c38ea
vpaju <ville.rajapaju@proactum.fi>
2013-09-19 13:52:04 +0300

    OVT-5850 MongoDB Clientilta loppuu yhteydet kesken

--- haku-app/src/main/java/fi/vm/sade/oppija/configuration/MongoConfiguration.java
+++ haku-app/src/main/java/fi/vm/sade/oppija/configuration/MongoConfiguration.java
@@ -1,6 +1,7 @@
 package fi.vm.sade.oppija.configuration;
 
 import com.mongodb.Mongo;
+import com.mongodb.MongoOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,7 +21,11 @@ public class MongoConfiguration {
     }
 
     @Bean
-    public MongoFactoryBean mongo(@Value("${mongodb.url}") final String mongoUrl) throws URISyntaxException {
+    public MongoFactoryBean mongo(@Value("${mongodb.url}") final String mongoUrl,
+                                  @Value("${mongodb.connectionsPerHost:50}") final String connectionsPerHost,
+                                  @Value("${mongodb.threadsAllowedToBlockForConnectionMultiplier:5}")
+                                  final String threadsAllowedToBlockForConnectionMultiplier) throws URISyntaxException {
+
         MongoFactoryBean mongo = new MongoFactoryBean();
         URI url1 = new URI(mongoUrl);
         mongo.setHost(url1.getHost());
@@ -28,6 +33,12 @@ public class MongoConfiguration {
         if (port > 0) {
             mongo.setPort(port);
         }
+        MongoOptions options = new MongoOptions();
+        options.connectionsPerHost = Integer.valueOf(connectionsPerHost);
+        options.threadsAllowedToBlockForConnectionMultiplier = Integer.valueOf(threadsAllowedToBlockForConnectionMultiplier);
+        mongo.setMongoOptions(options);
         return mongo;
     }
+
+
 }
