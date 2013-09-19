7b2a85d4989cff2aa6bff121af66c080c04441b4
vpaju <ville.rajapaju@proactum.fi>
2013-09-19 15:32:42 +0300

    OVT-5850 MongoDB Clientilta loppuu yhteydet kesken

--- haku-app/src/main/java/fi/vm/sade/oppija/configuration/MongoConfiguration.java
+++ haku-app/src/main/java/fi/vm/sade/oppija/configuration/MongoConfiguration.java
@@ -1,28 +1,29 @@
 package fi.vm.sade.oppija.configuration;
 
 import com.mongodb.Mongo;
-import com.mongodb.MongoURI;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.MongoFactoryBean;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.net.UnknownHostException;
 
 @Configuration
 public class MongoConfiguration {
 
     @Bean
-    public MongoOperations mongoTemplate(final Mongo mongo, @Value("${mongo.db.name}") final String collection) {
-        return new MongoTemplate(mongo, collection);
+    public MongoOperations mongoTemplate(final Mongo mongo, @Value("${mongo.db.name}") final String databaseName) {
+        return new MongoTemplate(mongo, databaseName);
     }
 
     @Bean
-    public MongoFactoryBean mongo(@Value("${mongodb.url}") final String mongoUrl) {
-        MongoURI mongoURI = new MongoURI(mongoUrl);
-        MongoFactoryBean mongo = new MongoFactoryBean();
-        mongo.setMongoOptions(mongoURI.getOptions());
-        return mongo;
+    public MongoClient mongo(@Value("${mongodb.url}") final String mongoUri) throws UnknownHostException {
+        MongoClientURI mongoClientURI = new MongoClientURI(mongoUri);
+        MongoClient mongoClient = new MongoClient(mongoClientURI);
+        return mongoClient;
     }
 
 
