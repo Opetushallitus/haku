54dcead9b6506d6b1e9dfaf524db51e0b8f2fc59
vpaju <ville.rajapaju@proactum.fi>
2013-09-19 14:25:15 +0300

    OVT-5850 MongoDB Clientilta loppuu yhteydet kesken

--- haku-app/src/main/java/fi/vm/sade/oppija/configuration/MongoConfiguration.java
+++ haku-app/src/main/java/fi/vm/sade/oppija/configuration/MongoConfiguration.java
@@ -1,16 +1,13 @@
 package fi.vm.sade.oppija.configuration;
 
 import com.mongodb.Mongo;
-import com.mongodb.MongoOptions;
+import com.mongodb.MongoURI;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoFactoryBean;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
-
-import java.net.URI;
-import java.net.URISyntaxException;
 
 @Configuration
 public class MongoConfiguration {
@@ -21,22 +18,10 @@ public class MongoConfiguration {
     }
 
     @Bean
-    public MongoFactoryBean mongo(@Value("${mongodb.url}") final String mongoUrl,
-                                  @Value("${mongodb.connectionsPerHost:50}") final String connectionsPerHost,
-                                  @Value("${mongodb.threadsAllowedToBlockForConnectionMultiplier:5}")
-                                  final String threadsAllowedToBlockForConnectionMultiplier) throws URISyntaxException {
-
+    public MongoFactoryBean mongo(@Value("${mongodb.url}") final String mongoUrl) {
+        MongoURI mongoURI = new MongoURI(mongoUrl);
         MongoFactoryBean mongo = new MongoFactoryBean();
-        URI url1 = new URI(mongoUrl);
-        mongo.setHost(url1.getHost());
-        int port = url1.getPort();
-        if (port > 0) {
-            mongo.setPort(port);
-        }
-        MongoOptions options = new MongoOptions();
-        options.connectionsPerHost = Integer.valueOf(connectionsPerHost);
-        options.threadsAllowedToBlockForConnectionMultiplier = Integer.valueOf(threadsAllowedToBlockForConnectionMultiplier);
-        mongo.setMongoOptions(options);
+        mongo.setMongoOptions(mongoURI.getOptions());
         return mongo;
     }
 
