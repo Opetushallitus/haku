200b888ee8a396ec4df2ce694dcfd70b73a7a926
Antti Salonen <antti.salonen@proactum.fi>
2013-09-25 16:14:18 +0300

    ARK-252 Healtch check url poc haku-appilla

--- haku-app/src/main/java/fi/vm/sade/healthcheck/AbstractHealthCheckServlet.java
+++ haku-app/src/main/java/fi/vm/sade/healthcheck/AbstractHealthCheckServlet.java
@@ -23,6 +23,7 @@ import java.util.Map;
  *
  * @author Antti Salonen
  */
+@WebServlet(urlPatterns = {"/healthcheck"}, loadOnStartup = 1)
 public abstract class AbstractHealthCheckServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(HealthCheckServlet.class);
 
