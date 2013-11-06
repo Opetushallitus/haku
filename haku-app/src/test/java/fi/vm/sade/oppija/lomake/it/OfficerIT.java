5e574f131980c46192ad20a0d965ad0669b7e3cb
jteuho <juho.teuho@gofore.com>
2013-11-06 12:07:04 +0200

    HAK-430 testit kuntoon

--- haku-app/src/test/java/fi/vm/sade/oppija/lomake/it/OfficerIT.java
+++ haku-app/src/test/java/fi/vm/sade/oppija/lomake/it/OfficerIT.java
@@ -51,6 +51,7 @@ public class OfficerIT extends DummyModelBaseItTest {
 
     @Test
     public void testSearchByPreference() {
+        clearSearch();
         List<WebElement> applicationLinks = findByClassName("application-link");
         assertTrue("Applications found", applicationLinks.isEmpty());
 
