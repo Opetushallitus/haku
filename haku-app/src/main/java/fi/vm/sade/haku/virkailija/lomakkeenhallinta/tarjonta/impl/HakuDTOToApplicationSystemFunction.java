e8c9c2713856316f4246ec8f4a2dafb7b5e6ea29
majapuro <mikko.majapuro@gofore.com>
2013-12-11 17:42:55 +0200

    Revert test code...

--- haku-app/src/main/java/fi/vm/sade/haku/virkailija/lomakkeenhallinta/tarjonta/impl/HakuDTOToApplicationSystemFunction.java
+++ haku-app/src/main/java/fi/vm/sade/haku/virkailija/lomakkeenhallinta/tarjonta/impl/HakuDTOToApplicationSystemFunction.java
@@ -27,7 +27,6 @@ import fi.vm.sade.haku.oppija.lomake.domain.I18nText;
 import fi.vm.sade.tarjonta.service.resources.dto.HakuDTO;
 import fi.vm.sade.tarjonta.service.resources.dto.HakuaikaRDTO;
 
-import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -56,7 +55,7 @@ public class HakuDTOToApplicationSystemFunction implements Function<HakuDTO, App
         List<HakuaikaRDTO> hakuaikas = hakuDTO.getHakuaikas();
         if (hakuaikas != null) {
             for (HakuaikaRDTO hakuaika : hakuaikas) {
-                applicationPeriods.add(new ApplicationPeriod(new Date(), hakuaika.getLoppuPvm()));
+                applicationPeriods.add(new ApplicationPeriod(hakuaika.getAlkuPvm(), hakuaika.getLoppuPvm()));
             }
         }
 
