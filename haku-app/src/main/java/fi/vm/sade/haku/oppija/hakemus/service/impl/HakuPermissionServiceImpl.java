5f4e8455e208bd8df4244e373eaf51f0b4b8cefd
jteuho <juho.teuho@gofore.com>
2014-01-23 09:55:06 +0200

    HAK-437 Virkailija ei voi käsitellä omaa hakemustaan

--- haku-app/src/main/java/fi/vm/sade/haku/oppija/hakemus/service/impl/HakuPermissionServiceImpl.java
+++ haku-app/src/main/java/fi/vm/sade/haku/oppija/hakemus/service/impl/HakuPermissionServiceImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -61,6 +62,10 @@ public class HakuPermissionServiceImpl extends AbstractPermissionService impleme
 
     @Override
     public boolean userCanUpdateApplication(Application application) {
+        String userOid = SecurityContextHolder.getContext().getAuthentication().getName();
+        if (userOid == null || userOid.equals(application.getPersonOid())) {
+            return false;
+        }
         return userCanAccessApplication(application, getReadUpdateRole(), getCreateReadUpdateDeleteRole());
     }
 
