356b79bdf5082e9aa51e00e08fa47bab2226644c
jteuho <juho.teuho@gofore.com>
2014-04-23 10:18:36 +0300

    HAK-520 fiksummat null-checkit
    Conflicts:
    	haku-app/src/main/java/fi/vm/sade/haku/oppija/ui/service/impl/OfficerUIServiceImpl.java

--- haku-app/src/main/java/fi/vm/sade/haku/oppija/ui/service/impl/OfficerUIServiceImpl.java
+++ haku-app/src/main/java/fi/vm/sade/haku/oppija/ui/service/impl/OfficerUIServiceImpl.java
@@ -34,6 +34,7 @@ import fi.vm.sade.haku.virkailija.authentication.AuthenticationService;
 import fi.vm.sade.haku.virkailija.lomakkeenhallinta.koodisto.KoodistoService;
 import fi.vm.sade.haku.virkailija.lomakkeenhallinta.util.ElementUtil;
 import fi.vm.sade.haku.virkailija.lomakkeenhallinta.util.OppijaConstants;
+<<<<<<< HEAD
 import fi.vm.sade.haku.virkailija.valinta.ValintaService;
 import fi.vm.sade.organisaatio.api.model.types.OrganisaatioTyyppi;
 import fi.vm.sade.organisaatio.api.search.OrganisaatioSearchCriteria;
@@ -44,6 +45,10 @@ import fi.vm.sade.sijoittelu.tulos.dto.raportointi.HakutoiveenValintatapajonoDTO
 import fi.vm.sade.valintalaskenta.domain.dto.*;
 import fi.vm.sade.valintalaskenta.domain.dto.valintakoe.ValintakoeDTO;
 import fi.vm.sade.valintalaskenta.domain.valintakoe.Osallistuminen;
+import fi.vm.sade.koulutusinformaatio.domain.dto.ApplicationOptionDTO;
+import fi.vm.sade.organisaatio.api.model.types.OrganisaatioTyyppi;
+import fi.vm.sade.organisaatio.api.search.OrganisaatioSearchCriteria;
+import fi.vm.sade.tarjonta.service.resources.dto.HakukohdeDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -293,7 +298,6 @@ public class OfficerUIServiceImpl implements OfficerUIService {
         funktioNimet.put("en", nimiEn);
         return new I18nText(funktioNimet);
     }
-
 
     @Override
     public ModelResponse getAdditionalInfo(String oid) {
