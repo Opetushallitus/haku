d7f0fb44816311371db67f6139d943e93d631f18
vpaju <ville.rajapaju@proactum.fi>
2014-06-04 14:36:32 +0300

    OVT-5128 lisätty vielä arvo 'ei suoritusta'

--- haku-app/src/main/java/fi/vm/sade/haku/virkailija/lomakkeenhallinta/util/OppijaConstants.java
+++ haku-app/src/main/java/fi/vm/sade/haku/virkailija/lomakkeenhallinta/util/OppijaConstants.java
@@ -83,6 +83,7 @@ public final class OppijaConstants {
     public static final String EDUCATION_LANGUAGE_SE = "SE";
     public static final String EDUCATION_LANGUAGE_VE = "VE";
     public static final String EDUCATION_LANGUAGE_OTHER = "XX";
+    public static final String EDUCATION_LANGUAGE_EI_SUORITUSTA = "97";
 
     public static final String PREFERENCE_DISCRETIONARY = "preference%d-discretionary";
 
