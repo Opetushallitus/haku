7d5961000712c3082c5c9bf6aa95589c44cafd9e
jteuho <juho.teuho@gofore.com>
2014-06-13 20:22:38 +0300

    OVT-8032 pakollisuustarkistukset groupatuille kysymyksille

--- haku-app/src/main/java/fi/vm/sade/haku/oppija/lomake/validation/validators/RequiredFieldValidator.java
+++ haku-app/src/main/java/fi/vm/sade/haku/oppija/lomake/validation/validators/RequiredFieldValidator.java
@@ -17,6 +17,8 @@
 package fi.vm.sade.haku.oppija.lomake.validation.validators;
 
 import fi.vm.sade.haku.oppija.lomake.domain.I18nText;
+import fi.vm.sade.haku.oppija.lomake.domain.elements.Element;
+import fi.vm.sade.haku.oppija.lomake.domain.elements.TitledGroup;
 import fi.vm.sade.haku.oppija.lomake.validation.FieldValidator;
 import fi.vm.sade.haku.oppija.lomake.validation.ValidationInput;
 import fi.vm.sade.haku.oppija.lomake.validation.ValidationResult;
@@ -24,7 +26,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.annotation.PersistenceConstructor;
 
 public class RequiredFieldValidator extends FieldValidator {
-
 
     private final String id;
 
@@ -41,15 +42,25 @@ public class RequiredFieldValidator extends FieldValidator {
 
     @Override
     public ValidationResult validate(final ValidationInput validationInput) {
+        if (validationInput.getElement().getType().equals(TitledGroup.class.getSimpleName())) {
+            for (Element child : validationInput.getElement().getChildren()) {
+                if (hasValue(validationInput, child.getId())) {
+                    return validValidationResult;
+                }
+            }
+        } else if (hasValue(validationInput, this.id)) {
+            return validValidationResult;
+        }
+        return getInvalidValidationResult(validationInput);
+    }
+
+    private boolean hasValue(final ValidationInput validationInput, final String id) {
         String value;
         if (id != null) {
             value = validationInput.getValueByKey(id);
         } else {
             value = validationInput.getValue();
         }
-        if (StringUtils.isBlank(value)) {
-            return getInvalidValidationResult(validationInput);
-        }
-        return validValidationResult;
+        return !StringUtils.isBlank(value);
     }
 }
