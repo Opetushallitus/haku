5e71cf4218a18dfda0cf78e74d3de75caba10350
jteuho <juho.teuho@gofore.com>
2014-06-13 10:06:09 +0300

    listByOids myös postina

--- haku-app/src/main/java/fi/vm/sade/haku/oppija/hakemus/resource/ApplicationResource.java
+++ haku-app/src/main/java/fi/vm/sade/haku/oppija/hakemus/resource/ApplicationResource.java
@@ -119,6 +119,26 @@ public class ApplicationResource {
         return result;
     }
 
+    @POST
+    @Path("list")
+    @Produces(MediaType.APPLICATION_JSON + CHARSET_UTF_8)
+    @Consumes("application/json")
+    @PreAuthorize("hasAnyRole('ROLE_APP_HAKEMUS_READ_UPDATE', 'ROLE_APP_HAKEMUS_READ', 'ROLE_APP_HAKEMUS_CRUD', 'ROLE_APP_HAKEMUS_OPO')")
+    public List<Application> getApplicationsByOidsPost(final List<String> oids) {
+
+        List<Application> result = new ArrayList<Application>();
+        for (String oid : oids) {
+            LOGGER.debug("Getting application by oid : {}", oid);
+            try {
+                Application app = applicationService.getApplicationByOid(oid);
+                result.add(app);
+            } catch (ResourceNotFoundException e) {
+                throw new JSONException(Response.Status.NOT_FOUND, "Could not find requested application with oid: " + oid, e);
+            }
+        }
+        return result;
+    }
+
     @GET
     @Path("listfull")
     @Produces(MediaType.APPLICATION_JSON + CHARSET_UTF_8)
