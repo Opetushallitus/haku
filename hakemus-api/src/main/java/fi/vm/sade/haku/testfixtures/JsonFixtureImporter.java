5d4452a23f217cb996b27004b11837572a479a73
Juha Paananen <juha.paananen@gmail.com>
2014-07-02 15:24:55 +0300

    Add ProjectRootFinder

--- hakemus-api/src/main/java/fi/vm/sade/haku/testfixtures/JsonFixtureImporter.java
+++ hakemus-api/src/main/java/fi/vm/sade/haku/testfixtures/JsonFixtureImporter.java
@@ -11,7 +11,7 @@ import com.mongodb.util.JSON;
 
 public class JsonFixtureImporter {
     public static void importJsonFixtures(MongoTemplate template) throws IOException {
-        File fixturesRoot = new File("testfixtures");
+        File fixturesRoot = new File(ProjectRootFinder.findProjectRoot() + "/" + "testfixtures");
         for (File collectionDirectory : fixturesRoot.listFiles()) {
             if (collectionDirectory.isDirectory()) {
                 String collection = collectionDirectory.getName();
