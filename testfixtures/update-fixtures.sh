2ab101f7ec374bb3effbcc4878569598f495e60b
Juha Paananen <juha.paananen@gmail.com>
2014-07-02 15:15:03 +0300

    require mongo 2.4, because export created by newer mongo isn't compatible with the Java driver

--- testfixtures/update-fixtures.sh
+++ testfixtures/update-fixtures.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+mongoversion=`mongoexport --version`
+
+if [[ $mongoversion =~ 2\.4 ]]; then
+  echo Updating fixtures...
+else
+  echo Mongoexport 2.4 series required
+  exit 1
+fi
+
 mkdir -p application||0
 mkdir -p applicationSystem||0
 mkdir -p themequestion||0
@@ -9,3 +18,5 @@ mongoexport "$@" -d hakulomake -c applicationSystem --query '{_id:"1.2.246.562.5
 mongoexport "$@" -d hakulomake -c themequestion --query '{applicationSystemId: "1.2.246.562.5.2014022711042555034240"}' --out themequestion/2014022711042555034240.json
 
 mongoexport "$@" -d hakulomake -c application --query '{oid: "1.2.246.562.11.00000877107"}' | node process-application.js > application/00000877107.json
+
+echo done.
