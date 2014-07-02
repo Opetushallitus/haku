9553e06927e9323b6854e21cd95500cf413f51f9
Juha Paananen <juha.paananen@gmail.com>
2014-07-02 15:06:43 +0300

    remove encrypted hetu when importing

--- testfixtures/process-application.js
+++ testfixtures/process-application.js
@@ -4,7 +4,13 @@ function changePersonOid(obj) {
   obj.personOid = "1.2.246.562.24.14229104472";
 }
 
+function removeEncryptedHetu(obj) {
+  delete obj.answers.henkilotiedot.Henkilotunnus
+  delete obj.answers.henkilotiedot.Henkilotunnus_digest
+}
+
 process(function(obj, stdout) {
   changePersonOid(obj);
+  removeEncryptedHetu(obj);
   return obj;
 });
