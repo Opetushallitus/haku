5d6bedb93cfc7f7a14460d0a690862c89e30ea81
Juha Paananen <juha.paananen@gmail.com>
2014-07-02 13:45:10 +0300

    wip on dao integration test

---
+++ hakemus-api/src/test/java/fi/vm/sade/haku/oppija/hakemus/it/ApplicationDaoIT.java
@@ -0,0 +1,44 @@
+package fi.vm.sade.haku.oppija.hakemus.it;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+import fi.vm.sade.haku.oppija.hakemus.domain.Application;
+import fi.vm.sade.haku.oppija.hakemus.it.dao.impl.ApplicationDAOMongoImpl;
+
+public class ApplicationDaoIT {
+    @Test
+    public void fetchApplication() throws IOException {
+        final AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext();
+        appContext.getEnvironment().setActiveProfiles("it");
+        appContext.register(TestConfiguration.class);
+        appContext.refresh();
+        final ApplicationDAOMongoImpl dao = appContext.getBean(ApplicationDAOMongoImpl.class);
+        final List<Application> applications = dao.find(new Application());
+        assertEquals(0, applications.size());
+    }
+}
+
+@Configuration
+@ComponentScan(basePackages = {"fi.vm.sade.haku"})
+@PropertySource(value = {"config/it/haku.properties", "config/it/ext.properties", "haku-test.properties"})
+@Profile("it")
+@ImportResource("/META-INF/spring/logger-mock-context.xml")
+class TestConfiguration {
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer getPropertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
