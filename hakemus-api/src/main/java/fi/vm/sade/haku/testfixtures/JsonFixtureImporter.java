535506d970f9de306b4bc315d02e0df55870e83c
Juha Paananen <juha.paananen@gmail.com>
2014-07-04 10:27:08 +0300

    load fixtures using classloader instead of filesystem

--- hakemus-api/src/main/java/fi/vm/sade/haku/testfixtures/JsonFixtureImporter.java
+++ hakemus-api/src/main/java/fi/vm/sade/haku/testfixtures/JsonFixtureImporter.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 
 import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import com.mongodb.DBObject;
@@ -11,21 +13,17 @@ import com.mongodb.util.JSON;
 
 public class JsonFixtureImporter {
     public static void importJsonFixtures(MongoTemplate template) throws IOException {
-        File fixturesRoot = new File(ProjectRootFinder.findProjectRoot() + "/hakemus-api/src/test/resources/mongofixtures");
-        for (File collectionDirectory : fixturesRoot.listFiles()) {
-            if (collectionDirectory.isDirectory()) {
-                String collection = collectionDirectory.getName();
-                for (File jsonFile: collectionDirectory.listFiles()) {
-                    final String jsonString = IOUtils.toString(jsonFile.toURI());
-                    try {
-                        final DBObject dbObject = (DBObject) JSON.parse(jsonString);
-                        template.getCollection(collection).insert(dbObject);
-                    } catch (Exception e) {
-                        System.err.println("Dumping JSON:");
-                        System.err.println(jsonString);
-                        throw new RuntimeException("Error importing JSON from " + jsonFile.getPath(), e);
-                    }
-                }
+        final Resource[] resources = new PathMatchingResourcePatternResolver().getResources("mongofixtures/**/*.json");
+        for (Resource resource: resources) {
+            String collection = resource.getFile().getParentFile().getName();
+            final String jsonString = IOUtils.toString(resource.getURI());
+            try {
+                final DBObject dbObject = (DBObject) JSON.parse(jsonString);
+                template.getCollection(collection).insert(dbObject);
+            } catch (Exception e) {
+                System.err.println("Dumping JSON:");
+                System.err.println(jsonString);
+                throw new RuntimeException("Error importing JSON from " + resource.getFile().getPath(), e);
             }
         }
     }
