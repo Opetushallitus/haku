9f02415e9705a1e5c0e316ee3425ff52dbfc69e2
Lauri Vanhala <lauri.vanhala@reaktor.fi>
2014-07-08 16:53:31 +0300

    check additional questions in validation IT

--- hakemus-api/src/test/java/fi/vm/sade/haku/oppija/hakemus/it/ValidationIT.java
+++ hakemus-api/src/test/java/fi/vm/sade/haku/oppija/hakemus/it/ValidationIT.java
@@ -4,27 +4,43 @@ import static org.junit.Assert.assertEquals;
 import fi.vm.sade.haku.oppija.hakemus.domain.Application;
 import fi.vm.sade.haku.oppija.hakemus.it.dao.impl.ApplicationDAOMongoImpl;
 import fi.vm.sade.haku.oppija.lomake.domain.ApplicationSystem;
-import fi.vm.sade.haku.oppija.lomake.domain.elements.Element;
+import fi.vm.sade.haku.oppija.lomake.domain.I18nText;
 import fi.vm.sade.haku.oppija.lomake.service.ApplicationSystemService;
 import fi.vm.sade.haku.oppija.lomake.service.impl.ApplicationSystemServiceImpl;
-import fi.vm.sade.haku.oppija.lomake.util.ElementTree;
 import fi.vm.sade.haku.oppija.lomake.validation.ElementTreeValidator;
 import fi.vm.sade.haku.oppija.lomake.validation.ValidationInput;
 import fi.vm.sade.haku.oppija.lomake.validation.ValidationResult;
 import org.junit.Test;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ValidationIT extends IntegrationTestSupport {
+    final ApplicationSystemService ass = appContext.getBean(ApplicationSystemServiceImpl.class);
+    final ApplicationDAOMongoImpl dao = appContext.getBean(ApplicationDAOMongoImpl.class);
+    final ElementTreeValidator validator = appContext.getBean(ElementTreeValidator.class);
+    final String applicationSystemId = "1.2.246.562.5.2014022711042555034240";
+
     @Test
     public void validateFixture() {
-        final ApplicationSystemService ass = appContext.getBean(ApplicationSystemServiceImpl.class);
-        final ElementTreeValidator validator = appContext.getBean(ElementTreeValidator.class);
-        final ApplicationDAOMongoImpl dao = appContext.getBean(ApplicationDAOMongoImpl.class);
-
-        ApplicationSystem applicationSystemFixture = ass.getApplicationSystem("1.2.246.562.5.2014022711042555034240");
-        Element phase = ElementTree.getFirstChild(applicationSystemFixture.getForm());
+        ApplicationSystem applicationSystemFixture = ass.getApplicationSystem(applicationSystemId);
         Application applicationFixture = dao.find(new Application()).get(0);
-        ValidationResult vr = validator.validate(new ValidationInput(phase, applicationFixture.getVastauksetMerged(), applicationFixture.getOid(), applicationSystemFixture.getId()));
-        assertEquals(vr.getErrorMessages().size(), 2);
+        assertEquals(validateApplication(applicationSystemFixture, applicationFixture).size(), 3);
+    }
+
+    @Test
+    public void validateUpdatedFixture() {
+        ApplicationSystem applicationSystemFixture = ass.getApplicationSystem(applicationSystemId);
+        Application applicationFixture = dao.find(new Application()).get(0);
+        Map<String, String> vastaukset = new HashMap<String, String>();
+        vastaukset.put("preference1-Koulutus-id", "1.2.246.562.20.30500448839");
+        vastaukset.put("preference1-Opetuspiste-id", "1.2.246.562.10.35241670047");
+        applicationFixture.addVaiheenVastaukset("hakutoiveet", vastaukset);
+        assertEquals(validateApplication(applicationSystemFixture, applicationFixture).size(), 7);
+    }
+
+    private Map<String, I18nText> validateApplication(ApplicationSystem applicationSystemFixture, Application applicationFixture) {
+        ValidationResult vr = validator.validate(new ValidationInput(applicationSystemFixture.getForm(), applicationFixture.getVastauksetMerged(), applicationFixture.getOid(), applicationSystemFixture.getId()));
+        return vr.getErrorMessages();
     }
 }
