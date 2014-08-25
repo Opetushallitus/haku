b139daab3c20877d06555ff84b273569be324465
jteuho <juho.teuho@gofore.com>
2014-08-25 18:55:05 +0300

    Testien fixailua

--- hakemus-api/src/test/java/fi/vm/sade/haku/oppija/lomake/validation/validators/YearValidatorTest.java
+++ hakemus-api/src/test/java/fi/vm/sade/haku/oppija/lomake/validation/validators/YearValidatorTest.java
@@ -9,23 +9,35 @@ import fi.vm.sade.haku.oppija.lomake.validation.ValidationResult;
 import fi.vm.sade.haku.oppija.lomake.validation.Validator;
 import fi.vm.sade.haku.virkailija.lomakkeenhallinta.hakulomakepohja.FormParameters;
 import fi.vm.sade.haku.virkailija.lomakkeenhallinta.util.ElementUtil;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 
 public class YearValidatorTest {
 
     public static final String FIELD_NAME = "fieldname";
     public static final I18nText TEXT = ElementUtil.createI18NAsIs("text");
     public static final Element question = new TextQuestion(FIELD_NAME, TEXT);
-    public static final FormParameters formParameters = new FormParameters(null, null, null, null, null);
 
     public static final Map<String, String> year1984 = ImmutableMap.of(FIELD_NAME, "1984");
     public static final Map<String, String> yearEmpty = ImmutableMap.of(FIELD_NAME, "");
     public static final Map<String, String> yearNaN = ImmutableMap.of(FIELD_NAME, "one");
+
+    public FormParameters formParameters;
+
+    @Before
+    public void setUp() {
+        formParameters = Mockito.mock(FormParameters.class);
+        when(formParameters.getI18nText(any(String.class))).thenReturn(new I18nText(new HashMap<String, String>()));
+    }
 
     @Test
     public void testYearValidator1984() throws Exception {
