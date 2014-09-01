cb420ecccbd77279ee0ff676d2a3c77c7dceffb0
jteuho <juho.teuho@gofore.com>
2014-09-01 09:42:41 +0300

    getHigherEdAttachmentAOIds takaisin ApplicationUtiliin

--- haku-app/src/main/java/fi/vm/sade/haku/oppija/ui/common/AttachmentUtil.java
+++ haku-app/src/main/java/fi/vm/sade/haku/oppija/ui/common/AttachmentUtil.java
@@ -8,11 +8,13 @@ import fi.vm.sade.haku.oppija.hakemus.domain.util.ApplicationUtil;
 import fi.vm.sade.haku.oppija.lomake.util.StringUtil;
 import fi.vm.sade.haku.virkailija.koulutusinformaatio.KoulutusinformaatioService;
 import fi.vm.sade.haku.virkailija.lomakkeenhallinta.util.ElementUtil;
-import fi.vm.sade.haku.virkailija.lomakkeenhallinta.util.OppijaConstants;
 import fi.vm.sade.koulutusinformaatio.domain.dto.*;
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AttachmentUtil {
 
@@ -82,7 +84,7 @@ public class AttachmentUtil {
     private static List<ApplicationAttachment> addHigherEdAttachments(List<ApplicationAttachment> attachments,
                                                                       Application application,
                                                                       KoulutusinformaatioService koulutusinformaatioService) {
-        Map<String, List<String>> higherEdAttachmentAOIds = getHigherEdAttachmentAOIds(application);
+        Map<String, List<String>> higherEdAttachmentAOIds = ApplicationUtil.getHigherEdAttachmentAOIds(application);
         Map<String, List<fi.vm.sade.koulutusinformaatio.domain.dto.ApplicationOptionDTO>> higherEdAttachments =
                 new HashMap<String, List<ApplicationOptionDTO>>();
         for (Map.Entry<String, List<String>> entry : higherEdAttachmentAOIds.entrySet()) {
@@ -169,125 +171,4 @@ public class AttachmentUtil {
         return false;
     }
 
-
-    public static Map<String, List<String>> getHigherEdAttachmentAOIds(Application application) {
-
-        Map<String, List<String>> attachments = new LinkedHashMap<String, List<String>>();
-
-        List<String> universityAOs = getUniversityAOs(application);
-        List<String> amkAOs = getAmkAOs(application);
-        List<String> aspaAmkAOs = getAspaAmkAOs(application);
-
-        List<String> universityAndAspaAmkAOs = new ArrayList<String>();
-        universityAndAspaAmkAOs.addAll(universityAOs);
-        universityAndAspaAmkAOs.addAll(aspaAmkAOs);
-
-        List<String> allAOs = new ArrayList<String>();
-        allAOs.addAll(universityAOs);
-        allAOs.addAll(amkAOs);
-        if (yoNeeded(application) && !universityAOs.isEmpty()) {
-            attachments.put("yo", universityAOs);
-        }
-        if (hasBaseEducation(application, "pohjakoulutus_am") && !universityAOs.isEmpty()) {
-            attachments.put("am", universityAOs);
-        }
-        if (hasBaseEducation(application, "pohjakoulutus_amt") && !universityAndAspaAmkAOs.isEmpty()) {
-            attachments.put("amt", universityAndAspaAmkAOs);
-        }
-        if (hasBaseEducation(application, "pohjakoulutus_kk") && !allAOs.isEmpty()) {
-            attachments.put("kk", allAOs);
-        }
-        if (hasBaseEducation(application, "pohjakoulutus_ulk") && !universityAndAspaAmkAOs.isEmpty()) {
-            attachments.put("ulk", universityAndAspaAmkAOs);
-        }
-        if (hasBaseEducation(application, "pohjakoulutus_avoin") && !allAOs.isEmpty()) {
-            attachments.put("avoin", allAOs);
-        }
-        if (hasBaseEducation(application, "pohjakoulutus_muu") && !allAOs.isEmpty()) {
-            attachments.put("muu", allAOs);
-        }
-        return attachments;
-    }
-
-    private static List<String> getAspaAmkAOs(Application application) {
-        Set<String> aspaAos = new HashSet<String>();
-        Map<String, String> answers = application.getPhaseAnswers(OppijaConstants.PHASE_APPLICATION_OPTIONS);
-        List<String> aos = new ArrayList<String>();
-        int i = 1;
-        while (true) {
-            String key = String.format(OppijaConstants.PREFERENCE_ID, i);
-            if (!answers.containsKey(key)) {
-                break;
-            }
-            String liiteKey = "preference" + i + "-amkLiite";
-            if (answers.containsKey(liiteKey) && !aos.contains(key)) {
-                String groupsStr = answers.get("preference" + i + "-Koulutus-id-attachmentgroups");
-                if (StringUtils.isBlank(groupsStr)) {
-                    aos.add(answers.get(key));
-                } else {
-                    for (String group : groupsStr.split(",")) {
-                        if (!aspaAos.contains(group)) {
-                            aspaAos.add(group);
-                            aos.add(answers.get(key));
-                        }
-                    }
-                }
-            }
-            i++;
-        }
-        return aos;
-    }
-
-    private static List<String> getAmkAOs(Application application) {
-        return getAosForType(application, "amkLiite");
-    }
-
-    private static List<String> getUniversityAOs(Application application) {
-        return getAosForType(application, "yoLiite");
-    }
-
-    private static List<String> getAosForType(Application application, String liiteKeyBase) {
-        Map<String, String> answers = application.getPhaseAnswers(OppijaConstants.PHASE_APPLICATION_OPTIONS);
-        List<String> providers = new ArrayList<String>();
-        List<String> aos = new ArrayList<String>();
-        int i = 1;
-        while (true) {
-            String key = String.format(OppijaConstants.PREFERENCE_ID, i);
-            if (!answers.containsKey(key)) {
-                break;
-            }
-            String liiteKey = "preference" + i + "-" + liiteKeyBase;
-            String provider = answers.get("preference" + i + "-Opetuspiste-id");
-            if (answers.containsKey(liiteKey)
-                    && !aos.contains(answers.get(key))
-                    && !providers.contains(provider)) {
-                aos.add(answers.get(key));
-                providers.add(provider);
-            }
-            i++;
-        }
-        return aos;
-    }
-
-    private static boolean hasBaseEducation(Application application, String field) {
-        Map<String, String> answers = application.getVastauksetMerged();
-        return Boolean.parseBoolean(answers.get(field));
-    }
-
-    private static boolean yoNeeded(Application application) {
-        if (!hasBaseEducation(application, "pohjakoulutus_yo")) {
-            return false;
-        }
-        Map<String, String> answers = application.getVastauksetMerged();
-        int suoritusvuosi = Integer.parseInt(answers.get("pohjakoulutus_yo_vuosi"));
-        if (suoritusvuosi < 1990) {
-            return true;
-        }
-        String tutkinto = answers.get("pohjakoulutus_yo_tutkinto");
-        if ("fi".equals(tutkinto)) {
-            return false;
-        }
-        return true;
-
-    }
 }
