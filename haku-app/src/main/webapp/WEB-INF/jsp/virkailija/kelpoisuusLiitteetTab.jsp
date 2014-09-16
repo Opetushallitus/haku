42c3cb9215bff81625c54c4981643dfb46e289f8
tomalatv <toni.latvakoski@gofore.com>
2014-09-16 15:53:23 +0300

    HAK-242 kelpoisuus ja liitteet välilehden v3

--- haku-app/src/main/webapp/WEB-INF/jsp/virkailija/kelpoisuusLiitteetTab.jsp
+++ haku-app/src/main/webapp/WEB-INF/jsp/virkailija/kelpoisuusLiitteetTab.jsp
@@ -20,9 +20,9 @@
             <b>${ao.index}.hakutoive</b>
             <br>
             <br>
-            <b id="kesken-${ao.index}"  style="background-color: #000000; color: #ffffff; border-radius: 5px; padding: 5px; display: none">Kesken</b>
-            <b id="hylatty-${ao.index}" style="background-color: #a9a9a9; color: #ffffff; border-radius: 5px; padding: 5px; display: none">Hylätty</b>
-            <b id="valmis-${ao.index}" style="background-color: #feba00; color: #000000; border-radius: 5px; padding: 5px; display: none">Valmis</b>
+            <b id="kesken-${ao.index}"  style="background-color: #000000; color: #ffffff; border-radius: 5px; padding: 5px; display: none;" >Kesken</b>
+            <b id="hylatty-${ao.index}" style="background-color: #333333; color: #ffffff; border-radius: 5px; padding: 5px; display: none;" >Hylätty</b>
+            <b id="valmis-${ao.index}" style="background-color: #feba00; color: #000000; border-radius: 5px; padding: 5px; display: none;" >Valmis</b>
             <br>
             <br>
             <b id="kaikkiliitteet-${ao.index}" style="background-color: #feba00; color: #000000; border-radius: 5px; padding: 5px; display: none">Kaikki liitteet saapuneet</b>
@@ -37,146 +37,20 @@
             </div>
             <div class="grid16-16 inline-block">
                 <br>
-                <button class="button small primary" id="ls" >Tallenna</button>
-                <%--<button class="button small" id="tt" onclick="ttt('${ao.index}')" >Tiedot tarkistettu</button>--%>
-                <button class="button small" id="ls" onclick="kaikkiLiitteetSaapuneet('${ao.index}')" >Kaikki liitteet saapuneet</button>
-
+                <button class="button small primary" >Tallenna</button>
+                <button class="button small" onclick="kaikkiLiitteetSaapuneet('${ao.index}')" >Kaikki liitteet saapuneet</button>
             </div>
 
             <div class="grid16-16 inline-block">
                 <br>
                 <table class="virkailija-table-2" id="liitteet-table-${ao.index}">
-                    <tr>
-                        <td>
-                            Hakukelpoisuus
-                        </td>
-                        <td>
-                                <select class="width-12-11" id="hakukelpoisuus-select" onchange="hakuKelpoisuus(${ao.index})">
-                                <option value="1">Kelpoisuus tarkistamatta</option>
-                                <option value="2">Hakukelpoinen</option>
-                                <option value="3">Ei hakukelpoinen</option>
-                                <option value="4">Puuttelinen</option>
-                            </select>
-
-                        </td>
-                        <td>
-                            <select class="width-12-11" id="hakukelpoisuus-tietolahde" disabled onchange="tietoLahde(${ao.index})">
-                                <option value="" default selected disabled>valitse tarkistettu tietolähde</option>
-                                <option value="1">Oppilaitoksen toimittava tieto</option>
-                                <option value="2">Alkuperäinen todistus</option>
-                                <option value="3">Virallinen oikeaksi todistettu kopio</option>
-                                <option value="4">Oikeaksi todistettu kopio</option>
-                                <option value="5">Kopio</option>
-                                <option value="6">Rekisteri</option>
-                            </select>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Hylkäämisen peruste</td>
-                        <td colspan="2">
-                            <textarea id="hylkaamisenperuste" rows="4" class="width-12-11" disabled onblur="hylkaamisenSyy(${ao.index})"></textarea>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><b>Liitteiden määrä 2 kpl</b></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <input type="checkbox" id="liitesaapunutCB1"> Liite saapunut liitexyz
-                        </td>
-                        <td>
-                            <select class="width-12-11" id="liiteselect1" disabled>
-                                <option value="" default selected disabled>valitse</option>
-                                <option value="1">Saapunut</option>
-                                <option value="2">Saapunut myöhässä</option>
-                                <option value="3">Odottaa täydennystä</option>
-                                <option value="4">Puutteellinen</option>
-                                <option value="5">Ei tarkistettu</option>
-                                <option value="6">Ei saapunut</option>
-                                    <%--<option value="1">hyväksytty (rekisteri) </option>
-                                    <option value="2">hyväksytty </option>
-                                    <option value="3">hylätty</option>
-                                    <option value="4">Pyydetty täydennystä</option>--%>
-                            </select>
-                        </td>
-                        <td>
-                                <%--<select class="width-12-11" id="liiteselectsyy1" disabled>
-                                    <option value="" default selected>hylkäämisen syy</option>
-                                    <option value="">Puuttuva pakollinen suoritus tai arvosana</option>
-                                    <option value="">Riittämätön työkokemus</option>
-                                    <option value="">Riittämätön kieli taito</option>
-                                    <option value="">Muita ?</option>
-                                </select>--%>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <input type="checkbox" id="liitesaapunutCB2"> Liite saapunut liitexyz
-                        </td>
-                        <td>
-                            <select class="width-12-11" id="liiteselect2" disabled>
-                                <option value="1">Saapunut</option>
-                                <option value="2">Saapunut myöhässä</option>
-                                <option value="3">Odottaa täydennystä</option>
-                                <option value="4">Puutteellinen</option>
-                                <option value="5">Ei tarkistettu</option>
-                                <option value="6">Ei saapunut</option>
-                                    <%--<option value="" default selected>valitse</option>
-                                    <option value="">hyväksytty (rekisteri) </option>
-                                    <option value="">hyväksytty </option>
-                                    <option value="">hylätty</option>
-                                    <option value="">Pyydetty täydennystä</option>--%>
-                            </select>
-                        </td>
-                        <td>
-                                <%--<select class="width-12-11" id="liiteselectsyy2" disabled>
-                                    <option value="" default selected>hylkäämisen syy</option>
-                                    <option value="">Puuttuva pakollinen suoritus tai arvosana</option>
-                                    <option value="">Riittämätön työkokemus</option>
-                                    <option value="">Riittämätön kieli taito</option>
-                                    <option value="">Muita ?</option>
-                                </select>--%>
-                        </td>
-                    </tr>
-
-                    <%--<tr>
-                        <td>
-                                &lt;%&ndash;<input type="checkbox">&ndash;%&gt; Kaikki liitteet saapuneet
-                        </td>
-                        <td>
-                            <select class="width-12-11">
-                                <option value="" default selected>valiste</option>
-                                <option value="">Kyllä</option>
-                                <option value="">Ei</option>
-                            </select>
-                        </td>
-                        <td>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                                &lt;%&ndash;<input type="checkbox"> &ndash;%&gt;Tiedot tarkistettu
-                        </td>
-                        <td>
-                            <select class="width-12-11">
-                                <option value="" default selected>valitse</option>
-                                <option value="">Kyllä</option>
-                                <option value="">Ei</option>
-                            </select>
-                        </td>
-                        <td>
-                        </td>
-                    </tr>--%>
                 </table>
             </div>
 
             <div class="grid16-16 inline-block">
                 <br>
                 <button class="button small primary" id="ls" >Tallenna</button>
-                <%--<button class="button small" id="tt" onclick="ttt('${ao.index}')" >Tiedot tarkistettu</button>--%>
-                <button class="button small" id="ls" onclick="lss('${ao.index}')" >Kaikki liitteet saapuneet</button>
-
+                <button class="button small" id="ls" onclick="kaikkiLiitteetSaapuneet('${ao.index}')" >Kaikki liitteet saapuneet</button>
             </div>
 
         </div>
@@ -190,15 +64,24 @@
             indx: '<c:out value="${ao.index}"/>',
             hakukelpoisuus: 1,
             tietolahde: '',
-            hylkaamisperuste: ''/*,
-            liiteet: [
-                1: {
+            hylkaamisperuste: '',
+            liitteet: [
+                {
+                   id: 1,
+                   nimi: 'Tässä liite 1',
                    tila: 6
                 },
-                2: {
+                {
+                    id: 2,
+                    nimi: 'Tässä liite 2',
+                    tila: 6
+                },
+                {
+                    id: 3,
+                    nimi: 'Tässä liite 3',
                     tila: 6
                 }
-            ]*/
+            ]
         };
         hakutoiveet.push(kelpoisuus_liitteet);
     </script>
--- haku-app/src/main/webapp/resources/javascript/virkailija/kelpoisuusLiitteet.js
+++ haku-app/src/main/webapp/resources/javascript/virkailija/kelpoisuusLiitteet.js
@@ -1,6 +1,78 @@
 $(document).ready(function () {
+    //TODO: poista tämä
     console.log('Kelpoisuus liitteet --->', hakutoiveet);
+    /**
+     * luodaan kelpoisuus ja liitteet välihdelle taustajärjestelmästä saadun
+     * datan perusteella lomake hakukelpoisuuden ja liitteiden tilojen tallentamiseksi
+     */
+    populateForm = function () {
+        //TODO: poista tämä
+        console.log('populoidaan hakutoive lomake');
+        for (var hktindx in hakutoiveet) {
 
+            var liitteet = hakutoiveet[hktindx].liitteet,
+                ind = parseInt(hktindx) +1;
+
+            var $form =
+                "<tr>"
+                    + "<td> Hakukelpoisuus </td>"
+                    + "<td>"
+                    + "<select class=\"width-12-11\" id=\"hakukelpoisuus-select\" onchange=\"hakuKelpoisuus(" + ind + ")\">"
+                    + "<option value=\"1\">Kelpoisuus tarkistamatta</option>"
+                    + "<option value=\"2\">Hakukelpoinen</option>"
+                    + "<option value=\"3\">Ei hakukelpoinen</option>"
+                    + "<option value=\"4\">Puuttelinen</option>"
+                    + "</select>"
+                    + "</td>"
+                    + "<td>"
+                    + "<select class=\"width-12-11\" id=\"hakukelpoisuus-tietolahde\" disabled onchange=\"tietoLahde(" + ind + ")\">"
+                    + "<option value=\"\" default selected disabled>valitse tarkistettu tietolähde</option>"
+                    + "<option value=\"1\">Oppilaitoksen toimittava tieto</option>"
+                    + "<option value=\"2\">Alkuperäinen todistus</option>"
+                    + "<option value=\"3\">Virallinen oikeaksi todistettu kopio</option>"
+                    + "<option value=\"4\">Oikeaksi todistettu kopio</option>"
+                    + "<option value=\"5\">Kopio</option>"
+                    + "<option value=\"6\">Rekisteri</option>"
+                    + "</select>"
+                    + "</td>"
+                    + "</tr>"
+
+                    + "<tr>"
+                    + "<td>Hylkäämisen peruste</td>"
+                    + "<td colspan=\"2\">"
+                    + "<textarea id=\"hylkaamisenperuste\" rows=\"4\" class=\"width-12-11\" disabled onblur=\"hylkaamisenSyy(" + ind + ")\"></textarea>"
+                    + "</td>"
+                    + "</tr>"
+
+                    + "<tr>"
+                    + "<td id=\"liitteidenmaara\" style=\"font-weight: bold\">Liitteiden määrä " + hakutoiveet[hktindx].liitteet.length + " kpl </td>"
+                    + "<td></td>"
+                    + "</tr>";
+
+            for (var trs in liitteet){
+                $form += "<tr class=\"liitteesaapuneet-" + ind +"\" id=\"liitesaapunut-tr-" +ind+ "-" + trs +"\" >"
+                    + "<td><input type=\"checkbox\" \" onchange=\"validateKaikkiLiitteetSaapuneet(" + ind + "," + trs + ")\" > "
+                    + "Liite saapunut: "+ liitteet[trs].nimi + "</td> "
+                    + "<td>"
+                    + "<select class=\"width-12-11\" id=\"liiteselect-" +ind+ "-" + trs + "\" disabled onchange=\"liitteenTila("+ ind +","+ trs +")\">"
+                    + "<option value=\"1\">Saapunut</option>"
+                    + "<option value=\"2\">Saapunut myöhässä</option>"
+                    + "<option value=\"3\">Odottaa täydennystä</option>"
+                    + "<option value=\"4\">Puutteellinen</option>"
+                    + "<option value=\"5\">Ei tarkistettu</option>"
+                    + "<option value=\"6\" default selected disabled >Ei saapunut</option>"
+                    + "<option value=\"7\">Tarkistettu</option>"
+                    + "</select>"
+                    + "</td>"
+                    + "</tr>";
+            }
+            $('#liitteet-table-' + ind).append($form);
+        }
+    }();
+    /**
+     * Asetetaan hakukelpoisuus käyttöliittymästä
+     * @param indx hakutoiveen index numero
+     */
     this.hakuKelpoisuus =  function (indx) {
         hakutoiveet[indx-1].hakukelpoisuus = $('#liitteet-table-' + indx + ' #hakukelpoisuus-select').val();
         if (hakutoiveet[indx-1].hakukelpoisuus === '2') {
@@ -21,100 +93,125 @@ $(document).ready(function () {
             $('#liitteet-table-' + indx + ' #hylkaamisenperuste').val('');
             $('#liitteet-table-' + indx + ' #hakukelpoisuus-tietolahde').val('');
         }
+        tarkistaHakutoiveValmis(indx);
     };
-
+    /**
+     * Asetetaan hakukelpoisuuteen liittyvän tarkastuksen tietolähde
+     * @param indx hakutoiveen index numero
+     */
     this.tietoLahde = function (indx) {
         hakutoiveet[indx-1].tietolahde = $('#liitteet-table-' + indx + ' #hakukelpoisuus-tietolahde').val();
+        tarkistaHakutoiveValmis(indx);
     };
-
+    /**
+     * Asetetaan hakukelpoisuuteen liittyvä hylkäämisen syy
+     * @param indx hakutoiveen index numero
+     */
     this.hylkaamisenSyy = function (indx) {
         hakutoiveet[indx-1].hylkaamisperuste = $('#liitteet-table-' + indx + ' #hylkaamisenperuste').val();
+        tarkistaHakutoiveValmis(indx);
     };
-
-    $('#liitesaapunutCB1').change(function () {
-        if ($(this).is(':checked')) {
-            $('#liiteselect1').removeAttr('disabled');
-        } else {
-            $('#liiteselect1').attr('disabled', 'true');
-        }
-    });
-    $('#liitesaapunutCB2').change(function () {
-        if ($(this).is(':checked')) {
-            $('#liiteselect2').removeAttr('disabled');
-        } else {
-            $('#liiteselect2').attr('disabled', 'true');
-        }
-    });
-
-    $('#liiteselect1').change(function () {
-        if ($('#liiteselect1').val() === '3') {
-            $('#liiteselectsyy1').removeAttr('disabled');
-        } else {
-            $('#liiteselectsyy1').attr('disabled', 'true');
-        }
-    });
-    $('#liiteselect2').change(function () {
-        if ($('#liiteselect2').val() === '3') {
-            $('#liiteselectsyy2').removeAttr('disabled');
-        } else {
-            $('#liiteselectsyy2').attr('disabled', 'true');
-        }
-    });
-
-    $('#tt').click(function () {
-        $('#tiedotarkistettu').css('display', '');
-    });
-    var toggle1 = true;
-    $('#ls').click(function () {
-        if (toggle1) {
-            $('#liitesaapunutCB1').prop('checked', true);
-            $('#liitesaapunutCB2').prop('checked', true);
-            $('#kaikkiliitteet').css('display', '');
-            $('#liiteselect1').removeAttr('disabled');
-            $('#liiteselect2').removeAttr('disabled');
-            toggle1 = false;
-        } else {
-            $('#liitesaapunutCB1').prop('checked', false);
-            $('#liitesaapunutCB2').prop('checked', false);
-            $('#kaikkiliitteet').css('display', 'none');
-            $('#liiteselect1').attr('disabled', 'true');
-            $('#liiteselect2').attr('disabled', 'true');
-            toggle1 = true;
-        }
-
-
-    });
-
-    for(var hakut in hakutoiveet) {
-//        console.log('&&&& ', hakutoiveet[hakut].form_Id);
-//         console.log($('#form-kelpliit-'+hakutoiveet[hakut].form_Id).find('#tiedotarkistettu').text());
-        $('#form-kelpliit-'+ hakutoiveet[hakut].form_Id +' #tiedotarkistettu').attr('id', 'tiedotarkistettu-'+hakutoiveet[hakut].form_Id);
-        $('#form-kelpliit-'+ hakutoiveet[hakut].form_Id +' #kaikkiliitteet').attr('id', 'kaikkiliitteet-'+hakutoiveet[hakut].form_Id);
-//        console.log('*** ',$('#tiedotarkistettu-'+hakutoiveet[hakut].form_Id));
-    }
-    var toggle2 = true;
-    this.ttt = function (indx) {
-        console.log($('#tiedotarkistettu-'+ indx), indx);
-        if(toggle2){
-            $('#tiedotarkistettu-'+ indx).css('display', '');
-            toggle2 = false;
-        } else {
-            $('#tiedotarkistettu-'+ indx).css('display', 'none');
-            toggle2 = true;
-        }
-
-    };
-
+    /**
+     * Asetetaan kaikki liitteet saapuneet tilaan käyttöliittymästä
+     * @param indx hakutoiveen index numero
+     */
     this.kaikkiLiitteetSaapuneet = function (indx) {
-        console.log($('#kaikkiliitteet-'+ indx), indx);
-        if(toggle2){
-            $('#kaikkiliitteet-'+ indx).css('display', '');
-            toggle2 = false;
-        } else {
-            $('#kaikkiliitteet-'+ indx).css('display', 'none');
-            toggle2 = true;
+        var hakutoive = parseInt(indx) - 1;
+        for(var i in hakutoiveet[hakutoive].liitteet){
+            if (hakutoiveet[hakutoive].liitteet[i].tila === 6) {
+                $('#liiteselect-' + indx +'-' + i).removeAttr('disabled');
+                $('#liiteselect-' + indx +'-' + i).val(1);
+                hakutoiveet[hakutoive].liitteet[i].tila = 1;
+            }
         }
-
+        $('#kaikkiliitteet-'+ indx).css('display', '');
+        $('#liitteet-table-' + indx +' *:checkbox').prop('checked', true);
+        tarkistaHakutoiveValmis(indx);
     };
+    /**
+     * Tarkistaa hakutoiveeseen liittyvien liitepyyntöjen
+     * saapumisen tilan näyttääkseen indikaation siitä käyttöliittymässä
+     * @param hakutoive hakutoiveen index numero
+     */
+    kaikkiLiitteetSaapuneet = function (hakutoive) {
+        var ind = hakutoive + 1;
+        for(var i in hakutoiveet[hakutoive].liitteet){
+            console.log('€€ ', hakutoiveet[hakutoive].liitteet[i].tila);
+            if(hakutoiveet[hakutoive].liitteet[i].tila === 6) {
+                $('#kaikkiliitteet-' + ind).css('display', 'none');
+                return;
+            } else {
+                $('#kaikkiliitteet-' + ind).css('display', '');
+            };
+        }
+    }
+    /**
+     * Tarkistaa hakutoiveeseen liityvien liitepyyntöjen
+     * tarkistuksen tilan, jos kaikki on tarkistettu palautta true
+     * @param hakutoive hakutoiveen index numero
+     * @returns {boolean} true/false
+     */
+    kaikkiLiitteetTarkistettu = function (hakutoive) {
+        console.log('kaikkiLiitteetTarkistettu() -->');
+        for (var j in hakutoiveet[hakutoive].liitteet) {
+            console.log(hakutoiveet[hakutoive].liitteet[j].tila);
+            if(hakutoiveet[hakutoive].liitteet[j].tila !== '7') {
+                return false;
+            }
+        }
+        return true;
+    };
+    /**
+     * Asettaa kaikki hakutoiveessa olevat liitepyynnöt
+     * käyttöliittymällä saapuneet tilaan
+     * @param indx hakutoiveen index numero
+     * @param trs hakutoiveen liitteen index numero
+     */
+    this.validateKaikkiLiitteetSaapuneet = function (indx, trs) {
+        var hakutoive = parseInt(indx) - 1;
+        if ($('#liitesaapunut-tr-' +indx + '-' + trs + ' [type=checkbox]').prop('checked')) {
+            $('#liiteselect-' +indx + '-' + trs).removeAttr('disabled');
+            $('#liiteselect-' +indx + '-' + trs).val(1);
+            hakutoiveet[hakutoive].liitteet[trs].tila = 1;
+            kaikkiLiitteetSaapuneet(hakutoive);
+        } else {
+            $('#liiteselect-' +indx + '-' + trs).attr('disabled', 'true');
+            $('#liiteselect-' +indx + '-' + trs).val(6);
+            hakutoiveet[hakutoive].liitteet[trs].tila = 6;
+            kaikkiLiitteetSaapuneet(hakutoive);
+        }
+        tarkistaHakutoiveValmis(indx);
+    };
+    /**
+     * tarkistaa hakutoiveen valmiuden ja näyttää
+     * siitä indikaation käyttöliittymällä
+     * @param indx hakutoiveen index numero
+     */
+    tarkistaHakutoiveValmis = function (indx) {
+        if (hakutoiveet[indx - 1].hakukelpoisuus ===  '3') {
+            $('#hylatty-' +indx).css('display', '');
+            $('#valmis-' +indx).css('display', 'none');
+            $('#kesken-' +indx).css('display', 'none');
+        } else {
+            $('#hylatty-' +indx).css('display', 'none');
+            $('#valmis-' +indx).css('display', 'none');
+            $('#kesken-' +indx).css('display', 'none');
+            if(hakutoiveet[indx - 1].hakukelpoisuus ===  '2' && kaikkiLiitteetTarkistettu(indx-1) && hakutoiveet[indx-1].tietolahde !== '') {
+                $('#valmis-' +indx).css('display', '');
+            } else {
+                $('#kesken-' +indx).css('display', '');
+            }
+        }
+    };
+    /**
+     * asettetaan liitteen tilan UI:sta
+     * sitä vaihdettaessa pudotusvalikosta
+     * @param indx hakutoiveen index numero
+     * @param trs hakutoiveen liitteen index numero
+     */
+    this.liitteenTila = function (indx, trs) {
+        hakutoiveet[indx-1].liitteet[trs].tila = $('#liiteselect-' + indx +'-' + trs).val();
+        tarkistaHakutoiveValmis(indx);
+    }
 
 });
