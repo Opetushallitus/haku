73e50ba8143c19a6721d29c621398d12f691b852
Tuomas Hakkarainen <tuomas.hakkarainen@reaktor.fi>
2014-09-29 16:17:57 +0300

    Refactor to use weighted cache

--- hakemus-api/src/main/java/fi/vm/sade/haku/oppija/lomake/service/impl/ApplicationSystemServiceImpl.java
+++ hakemus-api/src/main/java/fi/vm/sade/haku/oppija/lomake/service/impl/ApplicationSystemServiceImpl.java
@@ -1,5 +1,13 @@
 package fi.vm.sade.haku.oppija.lomake.service.impl;
 
+import com.google.common.base.Function;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.cache.Weigher;
+import com.google.common.collect.Lists;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListenableFutureTask;
 import fi.vm.sade.haku.oppija.lomake.domain.ApplicationSystem;
 import fi.vm.sade.haku.oppija.lomake.exception.ApplicationSystemNotFound;
 import fi.vm.sade.haku.oppija.lomake.service.ApplicationSystemService;
@@ -8,53 +16,83 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.lang.ref.SoftReference;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class ApplicationSystemServiceImpl implements ApplicationSystemService {
-
-    final Map<String, SoftReference<ApplicationSystem>> applicationSystems = new ConcurrentHashMap<String, SoftReference<ApplicationSystem>>();
-
-    final ApplicationSystemRepository applicationSystemRepository;
-
-    final boolean cacheApplicationSystems;
+    private final ApplicationSystemRepository applicationSystemRepository;
+    private final LoadingCache<String, ApplicationSystem> cache;
+    private final Boolean cacheApplicationSystems;
 
     @Autowired
     public ApplicationSystemServiceImpl(final ApplicationSystemRepository applicationSystemRepository,
                                         @Value("${application.system.cache:true}") final boolean cacheApplicationSystems) {
         this.applicationSystemRepository = applicationSystemRepository;
         this.cacheApplicationSystems = cacheApplicationSystems;
+        this.cache = CacheBuilder.newBuilder()
+                .maximumWeight(10)
+                .refreshAfterWrite(1, TimeUnit.HOURS)
+                .weigher(new Weigher<String, ApplicationSystem>() {
+                    @Override
+                    public int weigh(String key, ApplicationSystem value) {
+                        if (value.isActive()) {
+                            return 1;
+                        } else {
+                            return 2;
+                        }
+                    }
+                }).build(new CacheLoader<String, ApplicationSystem>() {
+                    @Override
+                    public ApplicationSystem load(String key) throws Exception {
+                        return findById(key);
+                    }
+
+                    @Override
+                    public ListenableFuture<ApplicationSystem> reload(final String key, ApplicationSystem oldValue) throws Exception {
+                        return ListenableFutureTask.create(new Callable<ApplicationSystem>() {
+                            @Override
+                            public ApplicationSystem call() throws Exception {
+                                return findById(key);
+                            }
+                        });
+                    }
+                });
+    }
+
+    private ApplicationSystem findById(String key) {
+        final ApplicationSystem applicationSystem = applicationSystemRepository.findById(key);
+        if (applicationSystem != null) {
+            return applicationSystem;
+        } else {
+            throw new ApplicationSystemNotFound(key);
+        }
     }
 
     @Override
     public ApplicationSystem getApplicationSystem(final String id) {
-        if (applicationSystems.containsKey(id)) {
-            ApplicationSystem as =  applicationSystems.get(id).get();
-            if (null != as)
-                return as;
+        if(!cacheApplicationSystems) {
+            return findById(id);
         }
-        ApplicationSystem applicationSystem = applicationSystemRepository.findById(id);
-        if (applicationSystem != null) {
-            if (this.cacheApplicationSystems) {
-                this.applicationSystems.put(applicationSystem.getId(), new SoftReference<ApplicationSystem>(applicationSystem));
-            }
-            return applicationSystem;
+        try {
+            return cache.get(id);
+        } catch (ApplicationSystemNotFound e) {
+            throw new ApplicationSystemNotFound(id);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
         }
-        throw new ApplicationSystemNotFound(id);
     }
 
     @Override
-    public ApplicationSystem getApplicationSystem(final String id, final String... includeFields) {
-        return this.applicationSystemRepository.findById(id, includeFields);
+    public ApplicationSystem getApplicationSystem(String id, String... includeFields) {
+        return applicationSystemRepository.findById(id, includeFields);
     }
 
     @Override
-    public ApplicationSystem getActiveApplicationSystem(final String id) {
-        ApplicationSystem applicationSystem = this.getApplicationSystem(id);
+    public ApplicationSystem getActiveApplicationSystem(String id) {
+        ApplicationSystem applicationSystem = getApplicationSystem(id);
         if (applicationSystem.isActive()) {
             return applicationSystem;
         }
@@ -62,24 +100,26 @@ public class ApplicationSystemServiceImpl implements ApplicationSystemService {
     }
 
     @Override
-    public void save(final ApplicationSystem applicationSystem) {
-        this.applicationSystemRepository.save(applicationSystem);
-        this.applicationSystems.put(applicationSystem.getId(), new SoftReference<ApplicationSystem>(applicationSystem));
+    public void save(ApplicationSystem applicationSystem) {
+        applicationSystemRepository.save(applicationSystem);
+        if(cacheApplicationSystems) {
+            cache.put(applicationSystem.getId(), applicationSystem);
+        }
     }
 
     @Override
     public List<ApplicationSystem> getAllApplicationSystems(String... includeFields) {
-        return this.applicationSystemRepository.findAll(includeFields);
+        return applicationSystemRepository.findAll(includeFields);
     }
 
     @Override
     public List<String> findByYearAndSemester(String asSemester, String asYear) {
-        List<ApplicationSystem> ass = this.applicationSystemRepository.findBySemesterAndYear(asSemester, asYear, "id", "name");
-        List<String> asIds = new ArrayList<String>(ass.size() + 1);
-        for (ApplicationSystem as : ass) {
-            asIds.add(as.getId());
-        }
-        asIds.add(null);
-        return asIds;
+        List<ApplicationSystem> ass = applicationSystemRepository.findBySemesterAndYear(asSemester, asYear, "id", "name");
+        return Lists.transform(ass, new Function<ApplicationSystem, String>() {
+            @Override
+            public String apply(ApplicationSystem as) {
+                return as.getId();
+            }
+        });
     }
 }
