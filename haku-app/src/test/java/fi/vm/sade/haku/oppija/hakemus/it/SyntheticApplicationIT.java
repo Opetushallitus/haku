6eb112e33cf3b3f5767658c841c6a25acd8ab497
Eetu Blomqvist <eetu.blomqvist@reaktor.fi>
2014-12-09 13:47:28 +0200

    Refactor tests

--- haku-app/src/test/java/fi/vm/sade/haku/oppija/hakemus/it/SyntheticApplicationIT.java
+++ haku-app/src/test/java/fi/vm/sade/haku/oppija/hakemus/it/SyntheticApplicationIT.java
@@ -12,11 +12,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.ws.rs.core.Response;
-
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:spring/test-context.xml")
@@ -28,28 +27,38 @@ public class SyntheticApplicationIT {
 
     @Test
     public void testInvalidInput() {
-        SyntheticApplication input = new SyntheticApplication("1", "2", "3", ImmutableList.of(new SyntheticApplication.Hakemus(null, "Etu", "Suku", "foobar", "barfoo")));
-        Response response = applicationResource.putSyntheticApplication(input);
+        Response response = put("1", "1", null);
         assertEquals(400, response.getStatus());
     }
 
     @Test
     public void testCreateAndUpdate() {
-        SyntheticApplication firstInput = new SyntheticApplication("1", "2", "3", ImmutableList.of(new SyntheticApplication.Hakemus("123", "Etu", "Suku", "foobar", "barfoo")));
-        Response resp1 = applicationResource.putSyntheticApplication(firstInput);
+        String hakukohde1 = "1";
+        String hakukohde2 = "2";
+
+        Response resp1 = put(hakukohde1, "1", "010101-123N");
         assertEquals(200, resp1.getStatus());
         List<Application> apps1 = (List<Application>) resp1.getEntity();
         assertEquals(1, apps1.size());
 
-        SyntheticApplication second = new SyntheticApplication("2", "2", "4", ImmutableList.of(new SyntheticApplication.Hakemus("123", "Etu", "Suku", "foobar", "barfoo")));
-        Response resp2 = applicationResource.putSyntheticApplication(second);
+        Response resp2 = put(hakukohde2, "2", "010101-123N");
         assertEquals(200, resp1.getStatus());
         List<Application> apps2 = (List<Application>) resp2.getEntity();
         assertEquals(1, apps2.size());
 
         Application app = apps2.get(0);
         Map<String, String> hakutoiveet = app.getPhaseAnswers("hakutoiveet");
-        assertEquals("1", hakutoiveet.get("preference1-koulutus-id"));
-        assertEquals("2", hakutoiveet.get("preference2-koulutus-id"));
+        assertEquals(hakukohde1, hakutoiveet.get("preference1-koulutus-id"));
+        assertEquals(hakukohde2, hakutoiveet.get("preference2-koulutus-id"));
+    }
+
+    private Response put(String hakukohdeOid, String tarjoajaOid, String hetu) {
+        SyntheticApplication firstInput = new SyntheticApplication(
+                hakukohdeOid,
+                "1.2.3",
+                tarjoajaOid,
+                ImmutableList.of(new SyntheticApplication.Hakemus(hetu, "Etu", "Suku", "foobar", "barfoo"))
+        );
+        return applicationResource.putSyntheticApplication(firstInput);
     }
 }
