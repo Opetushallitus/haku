ff4b1d120a4775ab58c1f76db188e1ceeb0f4c59
Juha Paananen <juha.paananen@gmail.com>
2015-02-17 15:50:54 +0200

    start embedded mongo when running tomcat7:run -Pintegration-test

--- hakemus-api/src/main/java/fi/vm/sade/haku/oppija/configuration/MongoServer.java
+++ hakemus-api/src/main/java/fi/vm/sade/haku/oppija/configuration/MongoServer.java
@@ -9,6 +9,8 @@ import de.flapdoodle.embed.mongo.config.Net;
 import de.flapdoodle.embed.mongo.distribution.Version;
 import de.flapdoodle.embed.process.runtime.Network;
 import org.apache.commons.lang.math.NumberUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
@@ -16,31 +18,48 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PreDestroy;
 import java.io.IOException;
+import java.net.Socket;
 
 @Service
 @Profile("it")
 public class MongoServer {
-    private final MongodExecutable mongodExecutable;
-    private final MongodProcess mongod;
+    private Logger logger = LoggerFactory.getLogger(MongoServer.class);
+    private MongodExecutable mongodExecutable;
+    private MongodProcess mongod;
 
     @Autowired
     public MongoServer(@Value("${mongodb.port:27018}") final String port) throws IOException {
-        IMongodConfig mongodConfig = new MongodConfigBuilder()
+        if (isFreeLocalPort(Integer.parseInt(port))) {
+            logger.info("Starting embedded mongo on port " + port);
+            IMongodConfig mongodConfig = new MongodConfigBuilder()
                 .version(Version.Main.PRODUCTION)
                 .net(new Net(NumberUtils.toInt(port), Network.localhostIsIPv6()))
                 .build();
-        MongodStarter runtime = MongodStarter.getDefaultInstance();
+            MongodStarter runtime = MongodStarter.getDefaultInstance();
 
-        mongodExecutable = runtime.prepare(mongodConfig);
-        mongod = mongodExecutable.start();
-
+            mongodExecutable = runtime.prepare(mongodConfig);
+            mongod = mongodExecutable.start();
+        } else {
+            logger.info("Not starting embedded mongo: seems to be running on port " + port);
+        }
     }
 
     @PreDestroy
     public void destroy() {
-        mongod.stop();
-        mongodExecutable.stop();
+        if (mongod != null) {
+            mongod.stop();
+            mongodExecutable.stop();
+        }
     }
 
-
+    public final static boolean isFreeLocalPort(int port) {
+        Socket socket = null;
+        try {
+            socket = new Socket("127.0.0.1", port);
+            socket.close();
+        } catch (IOException e) {
+            return true;
+        }
+        return false;
+    }
 }
