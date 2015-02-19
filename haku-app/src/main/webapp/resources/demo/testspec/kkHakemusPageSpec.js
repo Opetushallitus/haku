f46eeef81c99a711f3c8e4c2b539a40f03ca27e6
Teijo Laine <teijo.laine@reaktor.fi>
2015-02-19 09:31:13 +0200

    Apply better spec abstraction to kkHakemusPageSpec

--- haku-app/src/main/webapp/resources/demo/testspec/kkHakemusPageSpec.js
+++ haku-app/src/main/webapp/resources/demo/testspec/kkHakemusPageSpec.js
@@ -5,55 +5,56 @@ describe('KK-hakemus', function () {
         return S('td:has(a[name=' + name + '])').next().html()
     }
 
-    function start() {
-        return logout().then(function() {
+    beforeEach(seqDone(
+        logout,
+        function() {
             return openPage("/haku-app/lomakkeenhallinta/1.2.246.562.29.173465377510", function() {
                 return S("form#form-henkilotiedot").first().is(':visible')
-            })()})
-            .then(openPage("/haku-app/virkailija/hakemus", function() {
-                return testFrame().document.getElementById('loginForm') !== null;
-            }))
-            .then(function() {
-                function input(name) {
-                    return testFrame().document.getElementsByName(name)[0];
-                }
-                input("j_username").value = "officer";
-                input("j_password").value = "officer";
-                input("login").click();
-            })
-            .then(click(virkailija.createApplicationButton))
-            .then(input(virkailija.selectHaku, "1.2.246.562.29.173465377510"))
-            .then(click(virkailija.submitConfirm))
-    }
-
-    beforeEach(start);
+            })()
+        },
+        openPage("/haku-app/virkailija/hakemus", function() {
+            return testFrame().document.getElementById('loginForm') !== null;
+        }),
+        function() {
+            function input(name) {
+                return testFrame().document.getElementsByName(name)[0];
+            }
+            input("j_username").value = "officer";
+            input("j_password").value = "officer";
+            input("login").click();
+        },
+        click(virkailija.createApplicationButton),
+        input(virkailija.selectHaku, "1.2.246.562.29.173465377510"),
+        click(virkailija.submitConfirm)
+    ));
 
     describe("Muokkaa koulutustaustaa -toiminto", function() {
-        beforeEach(function(done) {
-            Q.fcall(click(virkailija.editKoulutusTaustaButton, virkailija.addAmmatillinenCheckbox))
-                .then(all(
-                    input(virkailija.ammatillinenSuoritusVuosi, "2000"),
-                    input(virkailija.ammatillinenTutkintonimike, "Hitsaajan perustutkinto"),
-                    input(virkailija.ammatillinenTutkinnonLaajuus, "120"),
-                    input(virkailija.ammatillinenOppilaitos, "Ammattikoulu X")))
-                .then(click(
-                    virkailija.kkTutkintoSuoritettu(false),
-                    virkailija.addAvoinCheckbox))
-                .then(all(
-                    input(virkailija.avoinAla(), "Avoin ala 1"),
-                    input(virkailija.avoinKokonaisuus(), "Avoin kokonaisuus 1"),
-                    input(virkailija.avoinLaajuus(), "Avoin laajuus 1"),
-                    input(virkailija.avoinKorkeakoulu(), "Avoin Korkeakoulu 1")))
-                .then(click(virkailija.addSecondAvoinLink))
-                .then(all(
-                    input(virkailija.avoinAla(2), "Avoin ala 2"),
-                    input(virkailija.avoinKokonaisuus(2), "Avoin kokonaisuus 2"),
-                    input(virkailija.avoinLaajuus(2), "Avoin laajuus 2"),
-                    input(virkailija.avoinKorkeakoulu(2), "Avoin Korkeakoulu 2")))
-                .then(click(virkailija.saveButton))
-                .then(exists(virkailija.ammatilliset))
-                .then(done, done);
-        });
+        beforeEach(seqDone(
+            click(
+                virkailija.editKoulutusTaustaButton,
+                virkailija.addAmmatillinenCheckbox),
+            input(
+                virkailija.ammatillinenSuoritusVuosi, "2000",
+                virkailija.ammatillinenTutkintonimike, "Hitsaajan perustutkinto",
+                virkailija.ammatillinenTutkinnonLaajuus, "120",
+                virkailija.ammatillinenOppilaitos, "Ammattikoulu X"),
+            click(
+                virkailija.kkTutkintoSuoritettu(false),
+                virkailija.addAvoinCheckbox),
+            input(
+                virkailija.avoinAla(), "Avoin ala 1",
+                virkailija.avoinKokonaisuus(), "Avoin kokonaisuus 1",
+                virkailija.avoinLaajuus(), "Avoin laajuus 1",
+                virkailija.avoinKorkeakoulu(), "Avoin Korkeakoulu 1"),
+            click(virkailija.addSecondAvoinLink),
+            input(
+                virkailija.avoinAla(2), "Avoin ala 2",
+                virkailija.avoinKokonaisuus(2), "Avoin kokonaisuus 2",
+                virkailija.avoinLaajuus(2), "Avoin laajuus 2",
+                virkailija.avoinKorkeakoulu(2), "Avoin Korkeakoulu 2"),
+            click(virkailija.saveButton),
+            exists(virkailija.ammatilliset)
+        ));
 
         it('mahdollistaa uuden ammatillisen pohjakoulutuksen lisäämisen kaksi kertaa peräkkäin', function (done) {
             expect(answerForQuestion('pohjakoulutus_am_vuosi')).to.equal('2000');
