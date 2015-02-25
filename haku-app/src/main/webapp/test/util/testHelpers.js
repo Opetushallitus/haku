eadf2bd85bf74cf20e4b5c72dfedbe3fe50e0e9d
Teijo Laine <teijo.laine@reaktor.fi>
2015-02-25 09:46:08 +0200

    Simplify promise usage in test helpers

--- haku-app/src/main/webapp/test/util/testHelpers.js
+++ haku-app/src/main/webapp/test/util/testHelpers.js
@@ -177,25 +177,18 @@ function getJson(url) {
 }
 
 function frameJquery() {
-    return wait.until(function() {
-        return testFrame() && testFrame().jQuery;
-    })().then(function() {
-        return testFrame().jQuery;
-    });
+    return seq(
+        wait.until(function() { return testFrame() && testFrame().jQuery; }),
+        function() { return testFrame().jQuery; });
 }
 
 // Ensure that evaluation of given promise leads into a new page being loaded
 function waitPageLoad(promise) {
-    return function() {
-        return Q.fcall(function() { testFrame().window.SPEC_PAGE_CHANGE_PENDING = true; })
-            .then(promise)
-            .then(wait.until(function() {
-                return testFrame().window.SPEC_PAGE_CHANGE_PENDING === undefined;
-            }))
-            .then(wait.until(function() {
-                return (testFrame().document.readyState === 'complete');
-            }));
-    }
+    return seq(
+        function() { testFrame().window.SPEC_PAGE_CHANGE_PENDING = true; },
+        promise,
+        wait.until(function() { return testFrame().window.SPEC_PAGE_CHANGE_PENDING === undefined; }),
+        wait.until(function() { return (testFrame().document.readyState === 'complete'); }));
 }
 
 function pageChange(promise) {
@@ -228,8 +221,9 @@ function postAsForm(path, params) {
 }
 
 function get(url) {
-    return function() {
-        return frameJquery().then(function($) {
+    return seq(
+        frameJquery(),
+        function($) {
             var deferred = Q.defer();
             $.get(url, function(data, status) {
                 if (status === 'success') {
@@ -239,14 +233,14 @@ function get(url) {
                 }
             });
             return deferred.promise
-        })
-    }
+        });
 }
 
 function post(url, data, contentType) {
     contentType = contentType || 'application/x-www-form-urlencoded';
-    return function() {
-        return frameJquery().then(function($) {
+    return seq(
+        frameJquery(),
+        function($) {
             var deferred = Q.defer();
             $.ajax({
                 type: "POST",
@@ -259,8 +253,7 @@ function post(url, data, contentType) {
                 deferred.reject("post got status " + status)
             });
             return deferred.promise
-        })
-    }
+        });
 }
 
 function testFrame() {
@@ -274,18 +267,18 @@ function openPage(path, predicate) {
             return testFrame().jQuery
         }
     }
-    return function () {
-        var newTestFrame = $('<iframe/>').attr({src: path, width: 1024, height: 800, id: "testframe"});
-        $("#testframe").replaceWith(newTestFrame);
-        return wait.until(function () {
-            return predicate()
-        })().then(function () {
+    return seq(
+        function() {
+            var newTestFrame = $('<iframe/>').attr({src: path, width: 1024, height: 800, id: "testframe"});
+            $("#testframe").replaceWith(newTestFrame);
+        },
+        wait.until(predicate),
+        function () {
             window.uiError = null;
             testFrame().onerror = function (err) {
                 window.uiError = err;
             }; // Hack: force mocha to fail on unhandled exceptions
-        })
-    }
+        });
 }
 
 function logout() {
@@ -391,11 +384,9 @@ function initSelectors(elements) {
 }
 
 function input1(fn, value) {
-    return function() {
-        return visible(fn)().then(function() {
-            return fn().val(value).change().blur();
-        })
-    }
+    return seq(
+        visible(fn),
+        function() { return fn().val(value).change().blur(); });
 }
 
 function input(/* fn, value, fn, value, ... */) {
@@ -411,16 +402,16 @@ function input(/* fn, value, fn, value, ... */) {
 }
 
 function select(fn, value) {
-    return function() {
-        return visible(fn)()
-            .then(wait.until(function() {
-                var matches = fn().find('option[value="' + value + '"]').length;
-                if (matches > 1) {
-                    throw new Error('Value "' + value + '" matches ' + matches + ' <option>s from <select> ' + fn().selector)
-                }
-                return matches === 1;
-            })).then(input(fn, value))
-    }
+    return seq(
+        visible(fn),
+        wait.until(function() {
+            var matches = fn().find('option[value="' + value + '"]').length;
+            if (matches > 1) {
+                throw new Error('Value "' + value + '" matches ' + matches + ' <option>s from <select> ' + fn().selector)
+            }
+            return matches === 1;
+        }),
+        input(fn, value));
 }
 
 function readTable($tableElement, allowWrongDimensions) {
@@ -483,12 +474,10 @@ function click(/* ...promises */) {
     var fns = arguments;
     return function() {
         var clickSequence = Object.keys(fns).map(function(i) {
-            return function() {
-                var fn = fns[i];
-                return visible(fn)().then(function() {
-                    fn().click();
-                })
-            }
+            var fn = fns[i];
+            return seq(
+                visible(fn),
+                function() { fn().click(); });
         });
         return clickSequence.reduce(Q.when, Q());
     }
@@ -501,9 +490,9 @@ function visibleText(fn, text) {
 }
 
 function hasClass(fn, className) {
-    return visible(fn)().then(function() {
-        return fn().hasClass(className);
-    });
+    return seq(
+        visible(fn),
+        function() { return fn().hasClass(className); });
 }
 
 function headingVisible(heading) {
@@ -513,9 +502,10 @@ function headingVisible(heading) {
 }
 
 function eventIsBound(fn, event) {
-    return Q.fcall(visible(fn))
-        .then(frameJquery)
-        .then(wait.until(function($) {
+    return seq(
+        visible(fn),
+        frameJquery(),
+        wait.until(function($) {
             var events = $._data(fn()[0], 'events');
             return events && events[event].length > 0;
         }));
@@ -526,14 +516,12 @@ function autocomplete(fn, partialText, suggestionChoiceText) {
         return S("a.ui-corner-all:visible:contains(" + suggestionChoiceText + ")");
     };
 
-    return function() {
-        return eventIsBound(fn, 'keydown')
-            .then(function() {
-                fn().val(partialText).trigger("keydown");
-            })
-            .then(visible(pickFn))
-            .then(function() { return pickFn().mouseover() })
-            .then(hasClass(pickFn, 'ui-state-hover'))
-            .then(click(pickFn))
-    };
+    return seq(eventIsBound(fn, 'keydown'),
+        function() {
+            fn().val(partialText).trigger("keydown");
+        },
+        visible(pickFn),
+        function() { return pickFn().mouseover() },
+        hasClass(pickFn, 'ui-state-hover'),
+        click(pickFn));
 }
