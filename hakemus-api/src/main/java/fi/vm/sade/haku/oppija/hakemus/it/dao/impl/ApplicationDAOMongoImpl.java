8c4bf322504b742a67fa733a284f7cf463d9d3d3
Rauli Sjöholm <rauli.sjoholm@gofore.com>
2015-03-06 14:32:04 +0200

    Get also incomple application for the identification post processing

--- hakemus-api/src/main/java/fi/vm/sade/haku/oppija/hakemus/it/dao/impl/ApplicationDAOMongoImpl.java
+++ hakemus-api/src/main/java/fi/vm/sade/haku/oppija/hakemus/it/dao/impl/ApplicationDAOMongoImpl.java
@@ -547,7 +547,11 @@ public class ApplicationDAOMongoImpl extends AbstractDAOMongoImpl<Application> i
         query.put(FIELD_APPLICATION_OID, new BasicDBObject(EXISTS, true));
         query.put(FIELD_PERSON_OID, new BasicDBObject(EXISTS, true));
         query.put(FIELD_STUDENT_OID, new BasicDBObject(EXISTS, false));
-        query.put(FIELD_APPLICATION_STATE, Application.State.ACTIVE.toString());
+        query.put(FIELD_APPLICATION_STATE,
+          new BasicDBObject(IN,
+            Lists.newArrayList(
+              Application.State.ACTIVE.name(),
+              Application.State.INCOMPLETE.name())));
         query.put(FIELD_STUDENT_IDENTIFICATION_DONE, false);
 
         DBObject sortBy = new BasicDBObject(FIELD_LAST_AUTOMATED_PROCESSING_TIME, 1);
