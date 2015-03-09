b00b0ce8a6079eb5046ecdd8ad08ac7ac5dc58aa
Jyri Kytömäki <jyri.kytomaki@reaktor.fi>
2015-03-09 17:26:07 +0200

    Move generic stuff from GroupConfigurationValidation to helpers

--- haku-app/src/main/webapp/test/util/partials.js
+++ haku-app/src/main/webapp/test/util/partials.js
@@ -1,3 +1,34 @@
+var oulunYliopisto = "Oulun yliopisto, Humanistinen tiedekunta";
+var helsinginYliopisto = "Helsingin yliopisto, Humanistinen tiedekunta";
+var novia = "Yrkeshögskolan Novia, Raasepori";
+var jarvenpaanDiakoniaAMk = "Diakonia-ammattikorkeakoulu, Järvenpään toimipiste"
+
+var afrikkaKoulutus = "Afrikan ja Lähi-idän tutkimus, humanististen tieteiden kandidaatti ja filosofian maisteri";
+var aasiaKoulutus = "Aasian tutkimus, humanististen tieteiden kandidaatti ja filosofian maisteri";
+var raaseporiKoulutus = "Agrolog (YH)/Miljöplanerare (YH)/Skogsbruksingenjör (YH), dagstudier";
+var ouluKoulutus = "Aate- ja oppihistoria, humanististen tieteiden kandidaatti ja filosofian maisteri";
+var diakoniaKoulutus = "Sosionomi (AMK), monimuotototeutus"
+
+function raasepori(n) {
+    return valitseKoulutus(n, novia, raaseporiKoulutus);
+}
+
+function afrikka(n) {
+    return valitseKoulutus(n, helsinginYliopisto, afrikkaKoulutus);
+}
+
+function aasia(n) {
+    return valitseKoulutus(n, helsinginYliopisto, aasiaKoulutus);
+}
+
+function oulu(n) {
+    return valitseKoulutus(n, oulunYliopisto, ouluKoulutus)
+}
+
+function diakonia(n) {
+    return valitseKoulutus(n, jarvenpaanDiakoniaAMk, diakoniaKoulutus)
+}
+
 function henkilotiedotTestikaes() {
     return seq(
         input(
