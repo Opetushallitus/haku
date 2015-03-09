3c243bbfb732ca29d72ccb8b78268e9cec856271
Jyri Kytömäki <jyri.kytomaki@reaktor.fi>
2015-03-09 13:19:26 +0200

    Add deadline and use haku domain Address class in HigherEdBaseEducationAttachmentInfo

--- hakemus-api/src/main/java/fi/vm/sade/haku/oppija/hakemus/domain/HigherEdBaseEducationAttachmentInfo.java
+++ hakemus-api/src/main/java/fi/vm/sade/haku/oppija/hakemus/domain/HigherEdBaseEducationAttachmentInfo.java
@@ -2,26 +2,27 @@ package fi.vm.sade.haku.oppija.hakemus.domain;
 
 import fi.vm.sade.haku.oppija.lomake.domain.I18nText;
 import fi.vm.sade.haku.oppija.lomake.util.StringUtil;
-import fi.vm.sade.koulutusinformaatio.domain.dto.AddressDTO;
+
+import java.util.Date;
 
 public class HigherEdBaseEducationAttachmentInfo {
 
     public final I18nText attachmentName;
-    public final String recipientName;
-    public final AddressDTO addressDTO;
+    public final Address address;
     public final OriginatorType originatorType;
     public final String originatorId;
+    public final Date deadline;
 
     public enum OriginatorType {
         applicationOption,
         group
     }
 
-    public HigherEdBaseEducationAttachmentInfo(I18nText attachmentName, String recipientName, AddressDTO addressDTO, OriginatorType originatorType, String originatorId) {
+    public HigherEdBaseEducationAttachmentInfo(I18nText attachmentName, Address address, OriginatorType originatorType, String originatorId, Date deadline) {
         this.attachmentName = attachmentName;
-        this.recipientName = StringUtil.safeToString(recipientName);
-        this.addressDTO = addressDTO;
+        this.address = address;
         this.originatorType = originatorType;
         this.originatorId = originatorId;
+        this.deadline = deadline;
     }
 }
