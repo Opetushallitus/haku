b8c3cdcd9897af3887ebb7921f4177a6e32f4d88
Rauli Sjöholm <rauli.sjoholm@gofore.com>
2015-03-27 13:10:32 +0200

    Add indexes to the (local) AuditLogRepository

--- hakemus-api/src/main/java/fi/vm/sade/haku/oppija/repository/AuditLogRepository.java
+++ hakemus-api/src/main/java/fi/vm/sade/haku/oppija/repository/AuditLogRepository.java
@@ -1,12 +1,16 @@
 package fi.vm.sade.haku.oppija.repository;
 
+import com.mongodb.BasicDBObject;
 import fi.vm.sade.log.model.Tapahtuma;
 import org.apache.commons.lang.builder.ReflectionToStringBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
 
 @Repository
 public class AuditLogRepository {
@@ -16,6 +20,9 @@ public class AuditLogRepository {
     private final MongoOperations mongoOperations;
 
     private static final String collectionName = "auditlog";
+
+    @Value("${mongodb.ensureIndex:true}")
+    private boolean ensureIndex;
 
     @Autowired
     public AuditLogRepository(final MongoOperations mongoOperations) {
@@ -30,6 +37,13 @@ public class AuditLogRepository {
         }
     }
 
+    @PostConstruct
+    private void initIndexes(){
+        if (!ensureIndex)
+            return;
+        mongoOperations.getCollection(collectionName).ensureIndex(new BasicDBObject("_target",1), "index_target");
+        mongoOperations.getCollection(collectionName).ensureIndex(new BasicDBObject("_targetType",1).append("_target",1), "index_targetType");
+    }
 
     public class TapahtumaToStringWrapper{
 
