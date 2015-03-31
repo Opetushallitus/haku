6ca2b739c903275e1a68baf2968b63c5acf5a849
Rauli Sjöholm <rauli.sjoholm@gofore.com>
2015-03-31 12:42:42 +0300

    Moved some constants away from MongoImpl

--- hakemus-api/src/main/java/fi/vm/sade/haku/oppija/hakemus/it/dao/impl/ApplicationDAOMongoImpl.java
+++ hakemus-api/src/main/java/fi/vm/sade/haku/oppija/hakemus/it/dao/impl/ApplicationDAOMongoImpl.java
@@ -50,6 +50,8 @@ import java.util.regex.Pattern;
 import static com.mongodb.QueryOperators.IN;
 import static java.lang.String.format;
 import static org.apache.commons.lang.StringUtils.*;
+import static fi.vm.sade.haku.oppija.hakemus.it.dao.impl.ApplicationDAOMongoConstants.*;
+
 
 /**
  * @author Hannu Lyytikainen
@@ -58,92 +60,6 @@ import static org.apache.commons.lang.StringUtils.*;
 public class ApplicationDAOMongoImpl extends AbstractDAOMongoImpl<Application> implements ApplicationDAO {
 
     private static final Logger LOG = LoggerFactory.getLogger(ApplicationDAOMongoImpl.class);
-
-    private static final String INDEX_APPLICATION_OID = "index_oid";
-    private static final String INDEX_APPLICATION_SYSTEM_ID = "index_as_oid";
-    private static final String INDEX_SSN_DIGEST_SEARCH = "index_Henkilotunnus_digest_search";
-    private static final String INDEX_SSN_DIGEST = "index_Henkilotunnus_digest";
-    private static final String INDEX_DATE_OF_BIRTH = "index_syntymaaika";
-    private static final String INDEX_PERSON_OID = "index_personOid";
-    private static final String INDEX_STUDENT_OID = "index_studentOid";
-    private static final String INDEX_POSTPROCESS = "index_postprocess";
-    private static final String INDEX_STUDENT_IDENTIFICATION_DONE = "index_studentIdentificationDone";
-    private static final String INDEX_SENDING_SCHOOL = "index_lahtokoulu";
-    private static final String INDEX_SENDING_CLASS = "index_lahtoluokka";
-    private static final String INDEX_ALL_ORGANIZAIONS = "index_allOrganizations";
-    private static final String INDEX_SEARCH_NAMES = "index_searchNames";
-    private static final String INDEX_FULL_NAME = "index_full_name";
-    private static final String INDEX_MODEL_VERSION = "index_model_version";
-    private static final String INDEX_ASID_SENDING_SCHOOL_AND_FULL_NAME = "index_asid_sending_school_and_full_name";
-    private static final String INDEX_ASID_AND_SENDING_SCHOOL = "index_asid_and_sending_school";
-
-    private static final String INDEX_STATE_ASID_FN = "index_state_asid_fn";
-    private static final String INDEX_STATE_FN = "index_state_fn";
-    private static final String INDEX_STATE_ASID_AO_OID = "index_state_asid_ao_org_oid";
-    private static final String INDEX_STATE_AO_OID = "index_state_ao_oid";
-    private static final String INDEX_ASID_AO_OID = "index_asid_ao_oid";
-    private static final String INDEX_AO_OID = "index_ao_oid";
-    private static final String INDEX_STATE_ASID_ORG_OID = "index_state_asid_org_oid";
-    private static final String INDEX_STATE_ORG_OID = "index_state_org_oid";
-    private static final String INDEX_ASID_ORG_OID = "index_asid_org_oid";
-    private static final String INDEX_ORG_OID = "index_org_oid";
-
-    //Reference fields
-    private static final String FIELD_APPLICATION_SYSTEM_ID = "applicationSystemId";
-    private static final String FIELD_APPLICATION_OID = "oid";
-    private static final String FIELD_PERSON_OID = "personOid";
-    private static final String FIELD_STUDENT_OID = "studentOid";
-
-    // Application answer meta fields
-    private static final String PREFIX_AUTHORIZATION_META = "authorizationMeta";
-    private static final String META_FIELD_OPO_ALLOWED = "authorizationMeta.opoAllowed";
-    private static final String META_LOP_PARENTS_T = "authorizationMeta.aoOrganizations.%d";
-    private static final String META_SENDING_SCHOOL_PARENTS = "authorizationMeta.sendingSchool";
-    private static final String META_ALL_ORGANIZATIONS = "authorizationMeta.allAoOrganizations";
-
-    // TODO ApplicationPreferenceMeta
-    private static final String PREFIX_PREFERENCE_META = PREFIX_AUTHORIZATION_META + ".applicationPreferences";
-    private static final String META_FIELD_ORDINAL = PREFIX_PREFERENCE_META +".ordinal";
-    private static final String PREFIX_PREFERENCE_DATA_META = PREFIX_PREFERENCE_META + ".preferenceData";
-    private static final String FIELD_AO_T = "answers.hakutoiveet.preference%d-Koulutus-id";
-    private static final String META_FIELD_AO = PREFIX_PREFERENCE_DATA_META +".Koulutus-id";
-
-    private static final String FIELD_AO_KOULUTUS_ID_T = "answers.hakutoiveet.preference%d-Koulutus-id-aoIdentifier";
-    private static final String META_FIELD_AO_KOULUTUS_ID = PREFIX_PREFERENCE_DATA_META +".Koulutus-id-aoIdentifier";
-
-    private static final String FIELD_LOP_T = "answers.hakutoiveet.preference%d-Opetuspiste-id";
-    private static final String META_FIELD_LOP = PREFIX_PREFERENCE_DATA_META+ ".Opetuspiste-id";
-
-    private static final String FIELD_DISCRETIONARY_T = "answers.hakutoiveet.preference%d-discretionary";
-    private static final String META_FIELD_DISCRETIONARY = PREFIX_PREFERENCE_DATA_META+ ".discretionary";
-    private static final String FIELD_AO_GROUPS_T = "answers.hakutoiveet.preference%d-Koulutus-id-ao-groups";
-    private static final String META_FIELD_AO_GROUPS = PREFIX_PREFERENCE_DATA_META+ ".Koulutus-id-ao-groups";
-
-    //TODO Meta
-    private static final String FIELD_HIGHER_ED_BASE_ED_T = "answers.koulutustausta.pohjakoulutus_%s";
-
-    // Application Answers
-    private static final String FIELD_SENDING_SCHOOL = "answers.koulutustausta.lahtokoulu";
-    private static final String FIELD_SENDING_CLASS = "answers.koulutustausta.lahtoluokka";
-    private static final String FIELD_CLASS_LEVEL = "answers.koulutustausta.luokkataso";
-
-    private static final String FIELD_SSN = "answers.henkilotiedot.Henkilotunnus";
-    private static final String FIELD_SSN_DIGEST = "answers.henkilotiedot.Henkilotunnus_digest";
-    private static final String FIELD_DATE_OF_BIRTH = "answers.henkilotiedot.syntymaaika";
-
-    // Processing information fields
-    private static final String FIELD_APPLICATION_STATE = "state";
-    private static final String FIELD_RECEIVED = "received";
-    private static final String FIELD_UPDATED = "updated";
-
-    //Technical fields
-    private static final String FIELD_SEARCH_NAMES = "searchNames";
-    private static final String FIELD_FULL_NAME = "fullName";
-    private static final String FIELD_MODEL_VERSION = "modelVersion";
-    private static final String FIELD_APPLICATION_VERSION = "version";
-    private static final String FIELD_STUDENT_IDENTIFICATION_DONE = "studentIdentificationDone";
-    private static final String FIELD_REDO_POSTPROCESS = "redoPostProcess";
-    private static final String FIELD_LAST_AUTOMATED_PROCESSING_TIME = "lastAutomatedProcessingTime";
 
     private static final String REGEX_LINE_BEGIN = "^";
 
