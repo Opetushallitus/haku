1a61c8c066808a297e5b35bad7b655fa4474bd6f
Tapio Tiili <tapio.tiili@reaktor.fi>
2015-04-22 13:28:28 +0300

    Add testcase for ApplicationDAOMongoQueryBuilder query building code. Case tested is OVT-7698.

---
+++ haku-app/src/test/java/fi/vm/sade/haku/oppija/hakemus/it/dao/ApplicationSearchMongoIT.java
@@ -0,0 +1,110 @@
+package fi.vm.sade.haku.oppija.hakemus.it.dao;
+
+import com.google.common.collect.Lists;
+import com.mongodb.DBObject;
+import com.mongodb.util.JSON;
+import fi.vm.sade.haku.oppija.common.dao.AbstractDAOTest;
+import fi.vm.sade.haku.oppija.hakemus.domain.Application;
+import fi.vm.sade.haku.oppija.hakemus.domain.dto.ApplicationSearchResultDTO;
+import fi.vm.sade.haku.oppija.hakemus.domain.dto.ApplicationSearchResultItemDTO;
+import fi.vm.sade.haku.oppija.hakemus.service.ApplicationService;
+import org.apache.commons.io.IOUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+
+import static java.lang.ClassLoader.getSystemResourceAsStream;
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring/tomcat-container-context.xml")
+@ActiveProfiles(profiles = {"it"})
+public class ApplicationSearchMongoIT extends AbstractDAOTest {
+    public static final String SIRKUSALAN_PERUSTUKTINTO_PK_ID = "1.2.246.562.14.2014021310042661282381";
+    public static final String SIRKUSALAN_PERUSTUKTINTO_PK_AOID = "398";
+    public static final String TOINEN_ASTE_KEVAT_2014 = "1.2.246.562.5.2013080813081926341927";
+
+    public static final String KOULUTUSKESKUS_SALPAUS_LAHTI_STALBERINKATU = "1.2.246.562.10.43328767116";
+    public static final String KOULUTUSKESKUS_SALPAUS = "1.2.246.562.10.81934895871";
+    public static final String PAIJAT_HAMEEN_KOUTUTUSKONSERNI = "1.2.246.562.10.594252633210";
+    public static final String ROOT_ORGANIZATION = "1.2.246.562.10.00000000001";
+    public static final String SIRKUSLAINEN_HETU = "KkXOzRby23Ww4tlmQFb/ynWM+4Z7oQOGOv5aO2rdpLk=";
+
+    @Autowired
+    @Qualifier("applicationDAOMongoImpl")
+    private ApplicationDAO applicationDAO;
+
+    @Autowired
+    @Qualifier("applicationServiceImpl")
+    private ApplicationService applicationService;
+
+    private ApplicationFilterParameters koulutuskeskusSalpausOrganization;
+
+    @Before
+    public void setUp() throws Exception {
+        String content = IOUtils.toString(getSystemResourceAsStream("harkinnanvarainen_salpaus_ei_sirkus.json"), "UTF-8");
+
+        ArrayList<String> org = Lists.newArrayList(KOULUTUSKESKUS_SALPAUS);
+        koulutuskeskusSalpausOrganization = new ApplicationFilterParameters(5, org, org, org, null, null);
+
+        DBObject applicationFromJson = (DBObject) JSON.parse(content);
+        mongoTemplate.getCollection(getCollectionName()).insert(applicationFromJson);
+
+        Application savedApplication = applicationDAO.find(new Application((String) applicationFromJson.get("oid"))).get(0);
+        applicationDAO.save(cloneAndChangeSalpausKohdeToSalpausSirkus(savedApplication));
+    }
+
+    public Application cloneAndChangeSalpausKohdeToSalpausSirkus(Application application) throws IOException {
+        Application applicationToSalpausSirkus = application.clone().setOid("1.2.246.562.11.00000840423");
+
+        Map<String, String> hakutoiveet = applicationToSalpausSirkus.getAnswers().get("hakutoiveet");
+        hakutoiveet.put("preference2-Koulutus-id-aoIdentifier", SIRKUSALAN_PERUSTUKTINTO_PK_AOID);
+        hakutoiveet.put("preference2-Koulutus-id", SIRKUSALAN_PERUSTUKTINTO_PK_ID);
+        hakutoiveet.put("preference2-Koulutus", "Sirkusalan perustutkinto, pk");
+        hakutoiveet.put("preference2-Koulutus-id-educationcode", "koulutus_321902");
+        hakutoiveet.put("preference2-Opetuspiste-id", KOULUTUSKESKUS_SALPAUS_LAHTI_STALBERINKATU);
+        hakutoiveet.put("preference2-Opetuspiste-id-parents", KOULUTUSKESKUS_SALPAUS_LAHTI_STALBERINKATU + ","+
+                                                              KOULUTUSKESKUS_SALPAUS + "," +
+                                                              PAIJAT_HAMEEN_KOUTUTUSKONSERNI + "," +
+                                                              ROOT_ORGANIZATION);
+        hakutoiveet.put("preference2-discretionary", "true");
+        applicationToSalpausSirkus.addVaiheenVastaukset("hakutoiveet", hakutoiveet);
+
+        Map<String, String> henkilotiedot = applicationToSalpausSirkus.getAnswers().get("henkilotiedot");
+        henkilotiedot.put("Henkilotunnus", SIRKUSLAINEN_HETU);
+        henkilotiedot.put("Henkilotunnus_plain", "221158-956V");
+        applicationToSalpausSirkus.addVaiheenVastaukset("henkilotiedot", henkilotiedot);
+
+        return applicationService.updateAuthorizationMeta(applicationToSalpausSirkus);
+    }
+
+
+    @Test
+    public void testFindSameOrganisationWithDifferentApplicationOptionDiscretionaryOnlySearchOVT7698() throws Exception {
+        ApplicationQueryParameters applicationQueryParameters  = new ApplicationQueryParametersBuilder()
+                                                                            .setSearchTerms("")
+                                                                            .setAsId(TOINEN_ASTE_KEVAT_2014)
+                                                                            .setAoId(SIRKUSALAN_PERUSTUKTINTO_PK_AOID)
+                                                                            .setLopOid(PAIJAT_HAMEEN_KOUTUTUSKONSERNI)
+                                                                            .setDiscretionaryOnly(true).build();
+
+        ApplicationSearchResultDTO result = applicationDAO.findAllQueried(applicationQueryParameters, koulutuskeskusSalpausOrganization);
+        assertEquals(1, result.getTotalCount());
+        ApplicationSearchResultItemDTO application = result.getResults().get(0);
+        assertEquals(SIRKUSLAINEN_HETU.toUpperCase(), application.getSsn());
+    }
+    @Override
+    protected String getCollectionName() {
+        return "application";
+    }
+}
