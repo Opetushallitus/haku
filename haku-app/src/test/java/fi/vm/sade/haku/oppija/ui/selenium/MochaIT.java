034f3f8520d1adf4964427ed99be75d550e7cea3
Jyri Kytömäki <jyri.kytomaki@reaktor.com>
2015-05-06 11:08:54 +0300

    Pretty print mocha results

--- haku-app/src/test/java/fi/vm/sade/haku/oppija/ui/selenium/MochaIT.java
+++ haku-app/src/test/java/fi/vm/sade/haku/oppija/ui/selenium/MochaIT.java
@@ -8,6 +8,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+
 public class MochaIT extends AbstractSeleniumBase {
 
     protected WebDriver driver;
@@ -29,18 +33,52 @@ public class MochaIT extends AbstractSeleniumBase {
                         return executor.executeScript("return runResults.allDone");
                     }
                 });
-        Long failed  = (Long) js.executeScript("return runResults.failed");
-        Long passed  = (Long) js.executeScript("return runResults.passed");
-        Long pending = (Long) js.executeScript("return runResults.pending");
         Object result = js.executeScript("return runResults");
         System.out.println("\n\n--------------------------------Mocha testing results start-------------------------------------------------------------");
-        System.out.println("Summary, Failed: " + failed + " Passed: " + passed + " Pending: " + pending);
-        System.out.println("Details:");
-        System.out.println(result);
+        System.out.println(getPrettyPrintResult("", result));
         System.out.println("--------------------------------Mocha testing results end---------------------------------------------------------------\n\n");
 
+        Long failed = (Long) js.executeScript("return runResults.failed");
         if (failed > 0) {
-            throw new RuntimeException("Mocha tests failed, see log for details!");
+            throw new RuntimeException(failed + " Mocha tests failed, see log for details!");
         }
+    }
+
+    private String getPrettyPrintResult(String prefix, Object result) {
+        if(result instanceof Map) {
+            return getPrettyPrintResult(prefix, (Map) result);
+        }
+        if(result instanceof Collection) {
+            return getPrettyPrintResult(prefix, (Collection)result);
+        }
+        return prefix + result;
+    }
+
+    private String getPrettyPrintResult(String prefix, Map result) {
+        StringBuilder rString = new StringBuilder();
+        Iterator<?> keys = result.keySet().iterator();
+        while(keys.hasNext()) {
+            Object key = keys.next();
+            rString.append(prefix);
+            rString.append(key);
+            rString.append(":\n");
+            rString.append(getPrettyPrintResult(prefix + "  ", result.get(key)));
+            if(keys.hasNext()) {
+                rString.append("\n");
+            }
+        }
+        return rString.toString();
+    }
+
+    private String getPrettyPrintResult(String prefix, Collection result) {
+        StringBuilder rString = new StringBuilder();
+        Iterator<?> iterator = result.iterator();
+        while(iterator.hasNext()) {
+            rString.append(getPrettyPrintResult(prefix, iterator.next()));
+            if(iterator.hasNext()) {
+                rString.append("\n");
+            }
+        }
+        return rString.toString();
     }
 }
