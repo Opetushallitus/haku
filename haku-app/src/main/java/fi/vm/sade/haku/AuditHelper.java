e0383c559380ff4dc0ac4fdcfa4000efc86bedc1
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2015-08-14 13:18:08 +0300

    audit-log configuration and dependency

---
+++ haku-app/src/main/java/fi/vm/sade/haku/AuditHelper.java
@@ -0,0 +1,31 @@
+package fi.vm.sade.haku;
+
+import fi.vm.sade.auditlog.ApplicationType;
+import fi.vm.sade.auditlog.Audit;
+import fi.vm.sade.auditlog.LogMessage;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.security.Principal;
+import java.util.Optional;
+
+/**
+ * @author Jussi Jartamo
+ */
+public class AuditHelper {
+    public static final Audit AUDIT = new Audit("haku-app", ApplicationType.VIRKAILIJA);
+
+    public static LogMessage.LogMessageBuilder builder() {
+        return LogMessage.builder().id(getUsernameFromSession());
+    }
+    private static String getUsernameFromSession() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        if(context != null) {
+            Principal p = (Principal) context.getAuthentication();
+            if(p != null) {
+                return p.getName();
+            }
+        }
+        return "Anonymous user";
+    }
+}
