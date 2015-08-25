d011c46422704a7d6f95648104272637a0e63e5f
Jani Rahkola <jani.rahkola@reaktor.com>
2015-08-25 09:35:37 +0300

    Migration script for base education countries

---
+++ haku-app/scripts/ulkomaisten_pohjakoulutusten_suoritusmaat.js
@@ -0,0 +1,40 @@
+var ulkomainenYo = function() {
+    var dropdownKey = "answers.koulutustausta.pohjakoulutus_yo_ulkomainen_maa";
+    var otherKey = "answers.koulutustausta.pohjakoulutus_yo_ulkomainen_maa_muu";
+    var q = {};
+    var u = {};
+    q[dropdownKey] = "XXX";
+    u[otherKey] = "Tuntematon";
+    db.application.update(q, {$set: u}, {multi: true});
+};
+
+var ulkomainenKK = function() {
+    for (var i = 0; i < 7; i++) {
+        var postfix = i === 0 ? "" : i;
+        var dropdownKey = "answers.koulutustausta.pohjakoulutus_kk_ulk_maa" + postfix;
+        var otherKey = "answers.koulutustausta.pohjakoulutus_kk_ulk_maa_muu" + postfix;
+        var q = {};
+        var u = {};
+        q[dropdownKey] = "XXX";
+        u[otherKey] = "Tuntematon";
+        db.application.update(q, {$set: u}, {multi: true});
+    }
+};
+
+var ulkomainen = function() {
+    for (var i = 0; i < 7; i++) {
+        var postfix = i === 0 ? "" : i;
+        var oldTextKey = "answers.koulutustausta.pohjakoulutus_ulk_suoritusmaa" + postfix;
+        var otherKey = "answers.koulutustausta.pohjakoulutus_ulk_suoritusmaa_muu" + postfix;
+        var q = {};
+        var u = {};
+        q[oldTextKey] = {$exists: true};
+        u[oldTextKey] = otherKey;
+        db.application.update(q, {$rename: u}, {multi: true});
+        q = {};
+        u = {};
+        q[otherKey] = {$exists: true};
+        u[oldTextKey] = "XXX";
+        db.application.update(q, {$set: u}, {multi: true});
+    }
+};
