5a286e7f702f5d0e757702ec5a4e1722f86aedcd
Teijo Laine <teijo.laine@reaktor.fi>
2015-11-11 13:22:49 +0200

    Async request and response handling

--- hakemus-api/src/test/java/fi/vm/sade/haku/http/RestClientTest.java
+++ hakemus-api/src/test/java/fi/vm/sade/haku/http/RestClientTest.java
@@ -4,8 +4,6 @@ import com.google.api.client.util.Key;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
