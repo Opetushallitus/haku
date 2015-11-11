b442fe22e6dcd9b356987c397de4e17fe6cd2ff6
Teijo Laine <teijo.laine@reaktor.fi>
2015-11-11 11:25:04 +0200

    New simplified REST client

---
+++ hakemus-api/src/test/java/fi/vm/sade/haku/http/RestClientTest.java
@@ -0,0 +1,38 @@
+package fi.vm.sade.haku.http;
+
+import com.google.api.client.util.Key;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+
+import static org.junit.Assert.assertNotNull;
+
+public class RestClientTest {
+    public static class Output {
+        @Key
+        public String koodiUri;
+    }
+    @Test
+    public void fooTest() throws ExecutionException, InterruptedException {
+        Future<String> f = new FutureTask<>(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return "foo";
+            }
+        });
+        System.out.println(f.get());
+    }
+    @Test
+    public void getTest() throws ExecutionException, InterruptedException, IOException {
+        String url = "https://testi.virkailija.opintopolku.fi/koodisto-service/rest/codeelement/valtioryhmat_2/1";
+        Future<Output> response = RestClient.get(url, Output.class);
+        Output output = response.get();
+        assertNotNull(output);
+    }
+}
