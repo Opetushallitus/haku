73235073bfbd6fffc081a22524b3ad99719b92a2
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2015-11-17 11:30:33 +0200

    Refactor hakumaksu test to handle the rest exemptions

--- hakemus-api/src/test/java/fi/vm/sade/haku/oppija/hakemus/HakumaksuTest.java
+++ hakemus-api/src/test/java/fi/vm/sade/haku/oppija/hakemus/HakumaksuTest.java
@@ -2,6 +2,7 @@ package fi.vm.sade.haku.oppija.hakemus;
 
 
 import fi.vm.sade.haku.oppija.hakemus.service.HakumaksuService;
+import fi.vm.sade.haku.virkailija.lomakkeenhallinta.util.Types;
 import org.junit.Test;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 import static fi.vm.sade.haku.oppija.hakemus.TestApplicationData.*;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 public class HakumaksuTest {
@@ -23,7 +25,13 @@ public class HakumaksuTest {
         for (Map.Entry<Hakukelpoisuusvaatimus, List<Pohjakoulutus>> entry : exemptions.entrySet()) {
             for (Pohjakoulutus pohjakoulutus : entry.getValue()) {
                 try {
-                    service.paymentRequirements(getApplication(entry.getKey(), pohjakoulutus));
+                    final Map<Types.ApplicationOptionOid, List<HakumaksuService.Eligibility>> requirements =
+                            service.paymentRequirements(getApplication(entry.getKey(), pohjakoulutus));
+
+                    assertTrue(
+                            "paymentRequirements were not empty for " + entry.getKey() + ", " + pohjakoulutus.name() + ": " + requirements.toString(),
+                            !requirements.isEmpty() && requirements.entrySet().iterator().next().getValue().isEmpty()
+                    );
                 } catch (ExecutionException e) {
                     fail(e.getMessage());
                 }
