d012d740dfca1f7e2ff4478e42f73c37712467ce
Pasi Kallioniemi <pasi.kallioniemi@gofore.com>
2015-11-25 15:13:00 +0200

    Title element doesnt need to be required

--- hakemus-api/src/main/java/fi/vm/sade/haku/virkailija/lomakkeenhallinta/hakulomakepohja/phase/osaaminen/OsaaminenPhase.java
+++ hakemus-api/src/main/java/fi/vm/sade/haku/virkailija/lomakkeenhallinta/hakulomakepohja/phase/osaaminen/OsaaminenPhase.java
@@ -109,7 +109,7 @@ public class OsaaminenPhase {
                 kysytaankoLukionKeskiarvo.addChild(lukioKeskiarvo.build());
             }
 
-            ElementBuilder yoKysymysRyhma = TitledGroup("osaaminen.kansainvalinenyo.arvosanat").required();
+            ElementBuilder yoKysymysRyhma = TitledGroup("osaaminen.kansainvalinenyo.arvosanat");
             addYoAsteikkoQuestion(OppijaConstants.ELEMENT_ID_OSAAMINEN_YOARVOSANAT_PARAS_KIELI, yoKysymysRyhma, formParameters);
             addYoAsteikkoQuestion(OppijaConstants.ELEMENT_ID_OSAAMINEN_YOARVOSANAT_AIDINKIELI, yoKysymysRyhma, formParameters);
             addYoAsteikkoQuestion(OppijaConstants.ELEMENT_ID_OSAAMINEN_YOARVOSANAT_MATEMATIIKKA, yoKysymysRyhma, formParameters);
