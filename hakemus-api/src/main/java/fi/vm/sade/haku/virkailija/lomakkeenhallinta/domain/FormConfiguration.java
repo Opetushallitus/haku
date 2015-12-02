04d236c1edb1823a691931a53ef5749406959fd7
Jani Rahkola <jani.rahkola@reaktor.com>
2015-12-02 16:18:29 +0200

    Feature flag for AMK ope muut tutkinnot kysymys

--- hakemus-api/src/main/java/fi/vm/sade/haku/virkailija/lomakkeenhallinta/domain/FormConfiguration.java
+++ hakemus-api/src/main/java/fi/vm/sade/haku/virkailija/lomakkeenhallinta/domain/FormConfiguration.java
@@ -45,7 +45,8 @@ public class FormConfiguration {
         gradeAverageLukio,
         gradeAverageYoAmmatillinen,
         gradeAverageAmmatillinen,
-        additionalInfoForPreviousDegree
+        additionalInfoForPreviousDegree,
+        amkOpeMuutTutkinnotKysymys
     }
 
     // FormConfiguration oid
