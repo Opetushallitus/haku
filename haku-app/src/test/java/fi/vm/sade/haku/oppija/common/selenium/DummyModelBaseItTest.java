193578bf627c9777fbe7f0165e79aea76a914d62
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2015-12-14 14:50:46 +0200

    Extend wait timeout for integration tests

--- haku-app/src/test/java/fi/vm/sade/haku/oppija/common/selenium/DummyModelBaseItTest.java
+++ haku-app/src/test/java/fi/vm/sade/haku/oppija/common/selenium/DummyModelBaseItTest.java
@@ -49,7 +49,7 @@ public abstract class DummyModelBaseItTest extends AbstractSeleniumBase {
     }
 
     protected void expectPhase(String expected) {
-        new WebDriverWait(seleniumContainer.getDriver(), 30)
+        new WebDriverWait(seleniumContainer.getDriver(), 60)
                 .until(ExpectedConditions.presenceOfElementLocated(By.id("nav-" + expected)));
         WebElement form = findBy(By.id("nav-" + expected));
         assertEquals("current", form.getAttribute("class"));
