85852327e33f3ffb332711a16b388b41f6377917
Tomi Katajanheimo <tomi.katajanheimo@gmail.com>
2015-12-18 10:03:29 +0200

    Refactor HAK153IT

--- haku-app/src/test/java/fi/vm/sade/haku/oppija/lomake/it/HAK153IT.java
+++ haku-app/src/test/java/fi/vm/sade/haku/oppija/lomake/it/HAK153IT.java
@@ -35,11 +35,11 @@ public class HAK153IT extends DummyModelBaseItTest {
         nextPhase(OppijaConstants.PHASE_APPLICATION_OPTIONS);
         typeWithoutTab("preference1-Opetuspiste", "Esp");
         clickLinkByText("FAKTIA, Espoo op");
-        findByAndAjaxClick(By.xpath("//*[@data-sora='true']"));
-        findByXPath("//a[@href='#' and @data-po-show='sora-popup' and @class='popup-link']");
-        findByAndAjaxClick(By.xpath("//*[@data-sora='false']"));
-        boolean soraNotFound = seleniumContainer.getDriver().findElements(By.xpath("//a[@href='#' and @data-po-show='sora-popup' and @class='popup-link']")).isEmpty();
-        findByAndAjaxClick(By.xpath("//*[@data-athlete='true']"));
+        findByAndAjaxClick(By.cssSelector("#preference1-Koulutus option[value='Kaivosalan perustutkinto, pk']"));
+        findBy(By.cssSelector("a.popup-link[href='#'][data-po-show='sora-popup']"));
+        findByAndAjaxClick(By.cssSelector("#preference1-Koulutus option[value='Musiikkiteknologian koulutusohjelma, pk (Musiikkialan perustutkinto)']"));
+        boolean soraNotFound = seleniumContainer.getDriver().findElements(By.cssSelector("a.popup-link[href='#'][data-po-show='sora-popup']")).isEmpty();
+        findByAndAjaxClick(By.cssSelector("#preference1-Koulutus option[value='Kaivosalan perustutkinto, pk']"));
         clickByNameAndValue("preference1_urheilijan_ammatillisen_koulutuksen_lisakysymys", "false");
         clickByNameAndValue("preference1_urheilijan_ammatillisen_koulutuksen_lisakysymys", "true");
         assertTrue(soraNotFound);
