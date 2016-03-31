d89dffdf1d68d0fd3cf52f8ca4aa7fdffce29197
Alexis Troberg <alexis.troberg@gofore.com>
2016-03-31 15:36:49 +0300

    Define roles in separate class to make code readable

--- haku-app/src/main/java/fi/vm/sade/haku/oppija/hakemus/resource/ApplicationResource.java
+++ haku-app/src/main/java/fi/vm/sade/haku/oppija/hakemus/resource/ApplicationResource.java
@@ -57,8 +57,8 @@ import java.util.*;
 
 import static fi.vm.sade.haku.AuditHelper.AUDIT;
 import static fi.vm.sade.haku.AuditHelper.builder;
+import static fi.vm.sade.haku.oppija.AuthorizationRoles.ALLOWED_FOR_ADMIN;
 import static fi.vm.sade.haku.oppija.lomake.util.StringUtil.nameOrEmpty;
-import static fi.vm.sade.haku.virkailija.lomakkeenhallinta.util.OppijaConstants.ROOT_ORGANIZATION_OID;
 import static org.apache.commons.lang3.StringUtils.isNotEmpty;
 
 /**
@@ -249,7 +249,7 @@ public class ApplicationResource {
     @POST
     @Path("/byPersonOid")
     @Produces(MediaType.APPLICATION_JSON + CHARSET_UTF_8)
-    @PreAuthorize("hasAnyRole('ROLE_APP_HAKEMUS_CRUD_" + ROOT_ORGANIZATION_OID + "')")
+    @PreAuthorize(ALLOWED_FOR_ADMIN)
     @ApiOperation(value="Hakemusten haku henkilönumeroiden perusteella", response = Application.class, responseContainer = "Map")
     public Map<String, Collection<Map<String, Object>>> findApplicationsByPersonOid(Set<String> personOids) {
         return applicationService.findApplicationsByPersonOid(personOids);
