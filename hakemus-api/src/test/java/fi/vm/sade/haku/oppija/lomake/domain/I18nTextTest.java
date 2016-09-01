797bcae025f14ab239aedb1a0a7b834081a57758
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2016-09-01 16:58:58 +0300

    test json is ok

--- hakemus-api/src/test/java/fi/vm/sade/haku/oppija/lomake/domain/I18nTextTest.java
+++ hakemus-api/src/test/java/fi/vm/sade/haku/oppija/lomake/domain/I18nTextTest.java
@@ -1,15 +1,28 @@
 package fi.vm.sade.haku.oppija.lomake.domain;
 
 import com.google.common.collect.ImmutableMap;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.Map;
 
 import static com.google.common.collect.ImmutableMap.*;
 
 public class I18nTextTest {
 
+    @Test
+    public void testJsonContainsDefaultValuesAsWell() throws IOException {
+        final String expectedEnglish = "English";
+        Map<String,String> translation = of(
+                "en", expectedEnglish);
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(new I18nText(translation));
+        Assert.assertTrue(
+                "{\"translations\":{\"fi\":\"English\",\"sv\":\"English\",\"en\":\"English\"}}".length() <=json.length());
+
+    }
     @Test
     public void testWithSpecialWhiteSpace() {
         String someWhitespace = "\u0020";
