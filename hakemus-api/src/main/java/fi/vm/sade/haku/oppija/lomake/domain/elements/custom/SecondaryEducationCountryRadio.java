b4758e7b429c957893335d5a16aed9ffe833fec1
Jaakko Salokanta <jaakko.salokanta@cybercom.com>
2016-09-07 13:13:02 +0300

    BUG-1105: Issues with SecondaryEducationCountryRadio
    The radio now returns the option children correctly. This allows
    merging valintadata because the merge uses element children id's when
    deciding which fields to pick from the valintadata.

--- hakemus-api/src/main/java/fi/vm/sade/haku/oppija/lomake/domain/elements/custom/SecondaryEducationCountryRadio.java
+++ hakemus-api/src/main/java/fi/vm/sade/haku/oppija/lomake/domain/elements/custom/SecondaryEducationCountryRadio.java
@@ -1,13 +1,25 @@
 package fi.vm.sade.haku.oppija.lomake.domain.elements.custom;
 
 import fi.vm.sade.haku.oppija.lomake.domain.I18nText;
+import fi.vm.sade.haku.oppija.lomake.domain.elements.Element;
 import fi.vm.sade.haku.oppija.lomake.domain.elements.questions.Option;
 import fi.vm.sade.haku.oppija.lomake.domain.elements.questions.OptionQuestion;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SecondaryEducationCountryRadio extends OptionQuestion {
     public SecondaryEducationCountryRadio (final String id, final I18nText i18nText, final List<Option> options) {
         super(id, i18nText, options, null, false);
+    }
+
+    @Override
+    public List<Element> getAllChildren() {
+        List<Element> allChildren = new ArrayList<>();
+        for (Element child : this.getChildren()) {
+            allChildren.add(child);
+            allChildren.addAll(child.getAllChildren());
+        }
+        return allChildren;
     }
 }
