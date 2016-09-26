0ef68cfece25bbd16f27a83c5860e9fbc1e10292
jussi.jartamo@gofore.com <jussi.jartamo@gofore.com>
2016-09-26 12:46:03 +0300

    load kelpoisuus ja liitteet lazily after save

--- haku-app/src/main/webapp/resources/javascript/virkailija/tabs.js
+++ haku-app/src/main/webapp/resources/javascript/virkailija/tabs.js
@@ -1,4 +1,40 @@
 
+var loadKelpoisuusJaLiitteetContent = function() {
+    var url = $('#kelpoisuusLiitteetContent').data("url");
+    var isLoading = $('#kelpoisuusLiitteetContent').data("loading");
+    if(isLoading) {
+        var load = function reload () {
+            $('#kelpoisuusLiitteetContent').load(url, function( response, status, xhr ) {
+                $('#kelpoisuusLiitteetContentLoaderIcon').hide();
+                if ( status == "error" ) {
+                    $('#kelpoisuusLiitteetContent')
+                    var retry = $("<a href='#'>Välilehden lataus epäonnistui. Yritä välilehden hakua uudelleen.</a>").click(function(e) {
+                        $('#kelpoisuusLiitteetContent').html("");
+                        $('#kelpoisuusLiitteetContentLoaderIcon').show();
+                        reload();
+                    });
+                    $('#kelpoisuusLiitteetContent').html("");
+                    $('#kelpoisuusLiitteetContent').append(retry);
+                } else {
+                    kjal.populateForm();
+                    /**
+                     * kelpoisuus ja liitteet välilehden tallennuksen jälkeen
+                     * asetataan kelpoisuus ja liitteet välilehti takaisin aktiiviseksi
+                     */
+                    if (window.location.href.split('#')[1] === 'liitteetkelpoisuusTab' ) {
+                        var navY = window.location.href.split('#')[2];
+                        window.location.href = window.location.href.split('#')[0]+'#';
+                        $('#kelpoisuusliitteetTab').click();
+                        window.setTimeout(function (){scrollTo(0,navY);},10);
+                    }
+                }
+            });
+        }
+        load();
+    }
+    $('#kelpoisuusLiitteetContent').data("loading", true);
+}
+
 $(document).ready(function() {
 
     $('div.tabs .tab').click(function() {
@@ -34,37 +70,6 @@ $(document).ready(function() {
         $('#valintaContent').data("url", null);
     });
     $('#kelpoisuusliitteetTab').click(function() {
-        var url = $('#kelpoisuusLiitteetContent').data("url");
-        if(url) {
-            var load = function reload () {
-                $('#kelpoisuusLiitteetContent').load(url, function( response, status, xhr ) {
-                    $('#kelpoisuusLiitteetContentLoaderIcon').hide();
-                    if ( status == "error" ) {
-                        $('#kelpoisuusLiitteetContent')
-                        var retry = $("<a href='#'>Välilehden lataus epäonnistui. Yritä välilehden hakua uudelleen.</a>").click(function(e) {
-                            $('#kelpoisuusLiitteetContent').html("");
-                            $('#kelpoisuusLiitteetContentLoaderIcon').show();
-                            reload();
-                        });
-                        $('#kelpoisuusLiitteetContent').html("");
-                        $('#kelpoisuusLiitteetContent').append(retry);
-                    } else {
-                        kjal.populateForm();
-                        /**
-                         * kelpoisuus ja liitteet välilehden tallennuksen jälkeen
-                         * asetataan kelpoisuus ja liitteet välilehti takaisin aktiiviseksi
-                         */
-                        if (window.location.href.split('#')[1] === 'liitteetkelpoisuusTab' ) {
-                            var navY = window.location.href.split('#')[2];
-                            window.location.href = window.location.href.split('#')[0]+'#';
-                            $('#kelpoisuusliitteetTab').click();
-                            window.setTimeout(function (){scrollTo(0,navY);},10);
-                        }
-                    }
-                });
-            }
-            load();
-        }
-        $('#kelpoisuusLiitteetContent').data("url", null);
+        loadKelpoisuusJaLiitteetContent();
     });
 });
