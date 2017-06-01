cf97c3d29d7616d110e77ccabb5719926d382cd5
Tuukka Palomäki <tuukka.palomaki@gofore.com>
2017-06-01 14:31:58 +0300

    OK-136 lisatty osallistumistietoja

--- hakemus-api/src/main/java/fi/vm/sade/haku/virkailija/valinta/dto/Osallistuminen.java
+++ hakemus-api/src/main/java/fi/vm/sade/haku/virkailija/valinta/dto/Osallistuminen.java
@@ -1,5 +1,5 @@
 package fi.vm.sade.haku.virkailija.valinta.dto;
 
 public enum Osallistuminen {
-    OSALLISTUU, EI_OSALLISTU, VIRHE
+    OSALLISTUU, EI_OSALLISTU, VIRHE, MERKITSEMATTA, OSALLISTUI, EI_OSALLISTUNUT, EI_VAADITA
 }
