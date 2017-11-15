ca8808d8d06e031caf64613e5fd86501653dad71
August Kilponen <august.kilponen@gmail.com>
2017-11-15 13:55:06 +0200

    BUG-1576: Luetaan kieli myös lang-parametrista

--- haku-app/src/main/java/fi/vm/sade/haku/oppija/ui/LocaleFilter.java
+++ haku-app/src/main/java/fi/vm/sade/haku/oppija/ui/LocaleFilter.java
@@ -19,6 +19,7 @@ package fi.vm.sade.haku.oppija.ui;
 import com.sun.jersey.api.core.InjectParam;
 import com.sun.jersey.spi.container.ContainerRequest;
 import com.sun.jersey.spi.container.ContainerRequestFilter;
+import fi.vm.sade.haku.oppija.lomake.util.StringUtil;
 import fi.vm.sade.haku.virkailija.authentication.AuthenticationService;
 import fi.vm.sade.haku.virkailija.authentication.Person;
 import org.slf4j.Logger;
@@ -65,15 +66,17 @@ public class LocaleFilter implements ContainerRequestFilter {
     }
 
     private String getLanguage(ContainerRequest containerRequest) {
-        String host = containerRequest.getHeaderValue("Host");
+        String lang = containerRequest.getQueryParameters().getFirst(LANGUAGE_QUERY_PARAMETER_KEY);
 
-        String lang;
-        if (host != null && host.endsWith("studieinfo.fi")) {
-            lang = "sv";
-        } else if (host != null && host.endsWith("studyinfo.fi")) {
-            lang = "en";
-        } else {
-            lang = "fi";
+        if(lang == null) {
+            String host = containerRequest.getHeaderValue("Host");
+            if (host != null && host.endsWith("studieinfo.fi")) {
+                lang = "sv";
+            } else if (host != null && host.endsWith("studyinfo.fi")) {
+                lang = "en";
+            } else {
+                lang = "fi";
+            }
         }
 
         Person person = authenticationService.getCurrentHenkilo();
