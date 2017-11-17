7a79f729b681aa61950b4b1296bc150ecc19e109
Joni Mäkelä <joni.makela@gmail.com>
2017-11-17 10:31:02 +0200

    Fixed bug in user remote address resolving

--- haku-auditlogger/src/main/java/fi/vm/sade/haku/HakuAuditLogger.java
+++ haku-auditlogger/src/main/java/fi/vm/sade/haku/HakuAuditLogger.java
@@ -16,6 +16,7 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import javax.servlet.http.HttpServletRequest;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.function.Predicate;
 
 public class HakuAuditLogger extends Audit {
 
@@ -51,7 +52,7 @@ public class HakuAuditLogger extends Audit {
             HttpServletRequest req = sra.getRequest();
             String sessionId = req.getSession().getId();
             String useragent = req.getHeader("User-Agent");
-            String remoteAddr = req.getRemoteAddr();
+            String remoteAddr = getRemoteAddress(req);
             try {
                 address = InetAddress.getByName(remoteAddr);
             } catch (UnknownHostException e) {
@@ -63,5 +64,23 @@ public class HakuAuditLogger extends Audit {
             LOGGER.warn("Servlet request attributes not present, can not audit log all user details");
             return new User(currentPersonOid, InetAddress.getLoopbackAddress(), "", "");
         }
+    }
+
+    private String getRemoteAddress(HttpServletRequest httpServletRequest) {
+        String xRealIp = httpServletRequest.getHeader("X-Real-IP");
+        Predicate<String> isNotBlank = (String txt) -> txt != null && !txt.isEmpty();
+        if (isNotBlank.test(xRealIp)) {
+            return xRealIp;
+        }
+        String xForwardedFor = httpServletRequest.getHeader("X-Forwarded-For");
+        if (isNotBlank.test(xForwardedFor)) {
+            if (xForwardedFor.contains(",")) {
+                LOGGER.error("Could not find X-Real-IP header, but X-Forwarded-For contains multiple values: {}, " +
+                        "this can cause problems", xForwardedFor);
+            }
+            return xForwardedFor;
+        }
+        LOGGER.warn("X-Real-IP or X-Forwarded-For was not set. Are we not running behind a load balancer?");
+        return httpServletRequest.getRemoteAddr();
     }
 }
