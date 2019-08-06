cfb94745b0aba59d6b7793f2215b4b104a7234d6
Tatu Honkanen <tatu.honkanen@cybercom.com>
2019-08-06 14:00:08 +0300

    BUG-2028 : WIP - Removed field already defined in FieldValidator

--- hakemus-api/src/main/java/fi/vm/sade/haku/oppija/lomake/validation/validators/EqualFieldValidator.java
+++ hakemus-api/src/main/java/fi/vm/sade/haku/oppija/lomake/validation/validators/EqualFieldValidator.java
@@ -1,25 +1,16 @@
 package fi.vm.sade.haku.oppija.lomake.validation.validators;
 
-import com.google.common.base.Strings;
-import fi.vm.sade.haku.oppija.lomake.domain.I18nText;
-import fi.vm.sade.haku.oppija.lomake.domain.elements.Element;
-import fi.vm.sade.haku.virkailija.lomakkeenhallinta.i18n.I18nBundleService;
-import fi.vm.sade.haku.oppija.lomake.validation.ValidationResult;
-import fi.vm.sade.haku.oppija.lomake.validation.ValidationInput;
 import fi.vm.sade.haku.oppija.lomake.validation.FieldValidator;
-
+import fi.vm.sade.haku.oppija.lomake.validation.ValidationInput;
+import fi.vm.sade.haku.oppija.lomake.validation.ValidationResult;
 import org.apache.commons.lang.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 public class EqualFieldValidator extends FieldValidator {
 
     final String otherFieldName;
-    final String errorMessageKey;
 
     public EqualFieldValidator(final String otherFieldName, final String errorMessageKey) {
         super(errorMessageKey);
-        this.errorMessageKey = errorMessageKey;
         this.otherFieldName = otherFieldName;
     }
 
